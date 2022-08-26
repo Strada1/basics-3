7db49b79cd8510fae9532a41008d5fc73d111b39
koresh <ruslankoren1995@gmail.com>
2022-08-26 18:38:47 +0300

    Updated main.js

--- calctest/js/main.js
+++ calctest/js/main.js
@@ -2,22 +2,22 @@
 function calc(operation, a, b) {
     let result;
     switch (operation) {
-        case '+':
+        case 'add':
             result = a + b;
             break;
-        case '*':
+        case 'multi':
             result = a * b;
             break;
-        case '-':
+        case 'sub':
             result = a - b;
             break;
     }
     return result;
 }
 
-console.log(calc('+', 1, 2));
-console.log(calc('*', 1, 2));
-console.log(calc('-', 3, 2));
+console.log(calc('add', 1, 2));
+console.log(calc('multi', 1, 2));
+console.log(calc('sub', 3, 2));
 
 
 
