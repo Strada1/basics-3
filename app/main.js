dcaa082d72bac7ed21739f6a18ae129bd9a10379
ARSTSoftware <95930407+ARSTSoftware@users.noreply.github.com>
2023-01-23 22:01:06 +0700

    Добавил файл main js

---
+++ app/main.js
@@ -0,0 +1,16 @@
+let userName = 'Арс';
+let userAge = 31;
+let userHeight;
+console.log(userHeight);
+userHeight = 180;
+console.log(userHeight);
+console.log('меня зовут ' + userName + ' и мне ' + userAge );
+
+console.log(5 + 10);
+console.log(10 * 2);
+console.log(2 > 10);
+
+
+let a = 8;
+let b = a + ' Vertino';
+console.log(b);
