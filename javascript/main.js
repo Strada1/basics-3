f942223f1a7bed8bb95a53a02848f277df2dd7c5
Sakenuly <sakenuly2016@yandex.ru>
2023-01-28 09:12:29 +0600

    Измененная версия калькулятора из 12 задачи.

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+let operation = prompt('add,multi,subtract');
+let a = +prompt('Первая цифра');
+let b = +prompt('Вторая цифра');
+let result;
+
+  switch(operation) {
+    case 'add': 
+    result = a + b;
+    break;
+  
+    case 'multi':
+    result = a * b;
+     break;
+   
+    case 'subtract':
+    result = a - b;
+     break;
+  }
+
+alert(result);
+//версия только с switch
