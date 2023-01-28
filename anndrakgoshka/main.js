b475f3ffa684a453aacbaa7556721345da422e81
Drakgoshka <drakgoshka@gmail.com>
2023-01-28 09:55:20 +0300

    создан калькулятор

---
+++ anndrakgoshka/main.js
@@ -0,0 +1,18 @@
+
+function calc(a, b, operation) {
+	if (isNaN(a)|| isNaN(b)) {
+		return 'ошибка';
+	}
+	switch (operation) {
+		case 'add':
+			return a + b;
+		case 'substract' :
+			return a - b;
+		case 'multi' :
+			return
+	}
+пше 
+
+console.log(calc(5, 2, 'add'));
+console.log(calc(10, 2, 'substract'));
+console.log(calc('f', 3, 'multi'));
