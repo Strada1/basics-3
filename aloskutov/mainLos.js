5ab7eafbe050f19ee6029f4ef00417bb2f977447
andreu03 <andrey.loskutov.2003@mail.ru>
2023-01-28 09:55:08 +0300

    использование switch

---
+++ aloskutov/mainLos.js
@@ -0,0 +1,17 @@
+function calc(operator, a, b) {
+    switch(operator) {
+        case "add":
+            return a+b;
+            break;
+        case "multi":
+            return a*b;
+            break;
+        case "subtract":
+            return a-b;
+            break;
+    }
+}
+
+console.log(calc("add", 3, 2))
+console.log(calc("multi", 3, 2))
+console.log(calc("subtract", 3, 2))
