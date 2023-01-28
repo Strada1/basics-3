58eea6acf33578a73ca2af322571d6bbc8749cc0
RenaultSport13 <truejdmfan@gmail.com>
2023-01-28 23:00:08 +0500

    function calc

---
+++ main.js
@@ -0,0 +1,32 @@
+
+function calc( a, operation, b) {
+    
+    switch (operation) {
+        case '+':
+            return a + b;
+        
+        case '*':
+            return a * b;
+
+        case '-':
+            return a - b;
+    
+        default:
+            break;
+    }
+    // if (operation == '+') {
+    //     return a + b;
+    // }
+    // else if (operation == '*') {
+    //     return a * b;
+    // }
+    // else if (operation == '-') {
+    //     return a - b;
+    // }
+}
+let add = calc(7, '-', 2); // 5
+let multi = calc(6, '+', 6); // 12
+let subtract = calc(17, '/', 27); //459
+console.log(add);
+console.log(multi);
+console.log(subtract);
