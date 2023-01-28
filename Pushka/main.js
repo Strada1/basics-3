0eaaaf59b1ede768d5d0044df20b57d7e8f9c25a
Lindover <123489854+Lindover@users.noreply.github.com>
2023-01-28 19:35:22 +0300

    Update main.js

--- Pushka/main.js
+++ Pushka/main.js
@@ -1,27 +1,3 @@
-/*Создайте простую функцию калькулятор с именем сalc()
-С тремя параметрами a и b, а также operation
-Вызов сalc(‘add’, 1, 2) - возвращает 3
-Вызов сalc(‘multi’, 1, 2) - возвращает 2
-Вызов сalc(’subtract’, 3, 2) - возвращает 1
-Делить пока ничего не надо.*/
-
-
-
-/*function calc(operation, a, b){
-    if (operation === 'add'){
-        console.log(a + b)
-    } else if (operation === 'substract'){
-        console.log(a - b);
-    } else if(operation === 'multi'){
-        console.log(a * b);
-    }
-    }
-    
-    calc('add', 1, 2)
-    calc('substract', 5, 4)
-    calc('multi', 3, 4)*/
-
-
 function calc(operator = '', a = 5, b = 4){
     switch(operator){
         case 'add':
