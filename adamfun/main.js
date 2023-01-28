65f5813c7a7fc96f5d2195fea93db2729439cda8
ArteomForFun <Mr.Kabyto@mail.ru>
2023-01-28 14:58:24 +0300

    Second commit

---
+++ adamfun/main.js
@@ -0,0 +1,23 @@
+function calc(operation = '', a = 1, b = 1) {
+    switch(operation) {
+      case 'add':
+        return a + b;
+  
+      case 'multi':
+        return a * b;
+        
+      case 'exponentiation':
+        return a**b;
+  
+      case 'subtract':
+        return a - b;
+  
+      case 'division':
+      default:
+        return 'There is no such operation!';
+    }
+  }
+  const result = calc('exponentiation', 2, 2);
+
+  console.log(result);
+  
