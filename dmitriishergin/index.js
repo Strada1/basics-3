35890854be88f32cad0a4ff877be7a7e56896800
dmitrii-shergin <120621105+dmitrii-shergin@users.noreply.github.com>
2023-01-28 23:48:48 +0700

    add check isNaN

--- dmitriishergin/index.js
+++ dmitriishergin/index.js
@@ -1,18 +1,19 @@
-console.log(calc(5, 7, '*'))
+console.log(calc('sdgsdgsdg', 7, '*'))
 
 function calc(a, b, oper) {
     a = Number(a)
     b = Number(b)
+    let error = 'enter a valid value'
+
+    if (isNaN(a) || isNaN(b)) {
+        return error
+    }
 
     switch (oper) {
         case '+': return a + b
         case '-': return a - b
         case '*': return a * b
         case '/': return a / b
-        default: return null
+        default: return error
     }
 }
-
-
-
-
