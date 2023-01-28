b1bf5ce3dd3bf88dfea07ec1170127eb56c24c8d
TotSamiyMorzh <bibigonqwe@gmail.com>
2023-01-28 08:39:12 +0300

    Add if else calc function

--- battle_toasster/main.js
+++ battle_toasster/main.js
@@ -1,3 +1,19 @@
+//функция калькулятора на if else
+// function calc(a, operator, b) {
+//     if (operator === 'add') {
+//         return a + b;
+//     } else if (operator === 'multi') {
+//         return a * b;
+//     } else if (operator === 'sub') {
+//         return a - b;
+//     }
+// }
+
+//     let result = calc (1, 'multi', 2);
+//     console.log(result);
+
+
+//Функция калькулятора на switch/case
 function calc (a, operator, b) {
     switch (operator) {
         case 'add':
