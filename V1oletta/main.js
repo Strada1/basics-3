11bae7a2a23f97f3673b0a49d1f40ce0feb49fec
Vi <vetttaa@mail.ru>
2023-01-28 12:33:22 +0300

    fix: main deleted unneccery comment

--- V1oletta/main.js
+++ V1oletta/main.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// function createCalc(operation, a, b) {
-//     if (operation == 'add') {
-//         return a + b;
-//     }else if (operation == 'multi') {
-//         return a * b;
-//     }else if (operation == 'subtract') {
-//         return a - b;
-//     }
-// }
-
-// createCalc('add', 1, 2);
-
 function createCalc(operation, a, b) {
     switch (operation) {
         case 'add':
@@ -27,5 +15,6 @@ function createCalc(operation, a, b) {
             console.log('Operation not found');
     }
 }
+
 
 createCalc('add', 1, 2);
