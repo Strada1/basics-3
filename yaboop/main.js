c2ba576d5ada53ab2aefe3d616a3a1f7171acdef
yaboop <majezaxo@gmail.com>
2023-01-28 18:15:11 +0300

    comment

---
+++ yaboop/main.js
@@ -0,0 +1,34 @@
+function calc(operation,a,b) {
+    if (operation ==='add') {
+        console.log (a + b);
+    } else if (operation==='multi'){
+        console.log (a * b);
+    } else if (operation==='subtract'){
+        console.log (a - b);
+    }
+}
+
+calc('add', 1, 2);
+calc('multi', 1, 2);
+calc('subtract', 3, 2);
+
+
+
+
+function calc (operation,a,b) {
+    switch(operation) {
+    case 'add':
+        console.log (a + b);
+        break;
+    case 'multi':
+        console.log (a * b);
+        break;
+    case 'subtract':
+        console.log (a - b);
+        break;
+    }
+}
+
+calc('add', 1, 2);
+calc('multi', 1, 2);
+calc('subtract', 3, 2);
