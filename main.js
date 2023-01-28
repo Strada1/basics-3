f0208e3e48ab7579606aa3a388d22308f0bfeb1c
Тимур <timur-skvorcov-666@mail.ru>
2023-01-28 10:15:50 +0500

    функция switch

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+function calc(a, b, operation) {
+	// if (!(typeof (a) == 'number' && typeof (b) == 'number')) {
+	//  return 'Ошибочка!'
+	// }
 
+	// if (operation == 'add') {
+	//  return a + b
+	// } else if (operation == 'multi') {
+	//  return a * b
+	// } else if (operation == 'subtract') {
+	//  return a - b
+	// } else {
+	//  return 'Ошибочка!'
+	// }
+
+	if (!(typeof (a) == 'number' && typeof (b) == 'number')) {
+		return 'Ошибочка!'
+	}
+
+	switch (operation) {
+		case 'add':
+			return a + b;
+		case 'multi':
+			return a * b;
+		case 'subtract':
+			return a - b;
+		default:
+			return 'ошибочка'
+	}
+}
+
+const result = calc(5, 4, 'multi')
+
+console.log(result)
