3fddbd0c230cfe8e211818cbd1937dc24e890703
Maxim <maximguwop@icloud.com>
2023-01-29 11:29:42 +0300

    simple__calc__with__switch__and__prompt

--- max_liashevich/main.js
+++ max_liashevich/main.js
@@ -18,3 +18,48 @@ function calc(a, b, operation) {
 
 calc(8, 5, '*')
 
+
+// -------------SIMPLE__CALC__WITH__SWITCH__AND__PROMPT-----------------
+const operations = {
+  sum: '+',
+  min: '-',
+  umn: '*',
+  div: '/'
+};
+
+const firstNum = +prompt(`Enter first digit, please...`, ``);
+const secNum = +prompt(`Enter second digit, please...`,``)
+const operation = prompt(`What you wanna do with this?(+, -, *, /)`,``);
+
+let result;
+
+if(operation === operations.div && secNum === 0) {
+  alert(`division on zero??)) lol!`)
+} else {
+  switch (operation) {
+    case operations.sum:
+      result = (firstNum + secNum);
+      break;
+    case operations.min:
+      result = (firstNum - secNum);
+      break;
+    case operations.umn:
+      result = (firstNum * secNum);
+      break;
+    case operations.div:
+      result = (firstNum / secNum);
+      break;
+    default:
+      alert('wtf?');
+  }
+}
+
+alert(result);
+const review = prompt('do you like it?', ``);
+if(review === 'yes') {
+  alert('Thank you so much, dear friend!');
+} else {
+  alert('Okay...and fuck you!');
+}
+
+
