9c28a86797cadcf146a8e465bcd6dc264ab94a3b
Maxim <maximguwop@icloud.com>
2023-01-29 11:30:59 +0300

    fruit__choice__with__switch__and__prompt

--- max_liashevich/main.js
+++ max_liashevich/main.js
@@ -62,4 +62,29 @@ if(review === 'yes') {
   alert('Okay...and fuck you!');
 }
 
+// ----------fruit__choice__and__price__with__switch__and__prompt------------------
 
+const expr = prompt(`What kind of fruit you wanna?`,``);
+
+switch (expr) {
+  case "Oranges":
+    alert("Oranges are $0.59 a pound.");
+    break;
+  case "Apples":
+    alert("Apples are $0.32 a pound.");
+    break;
+  case "Bananas":
+    alert("Bananas are $0.48 a pound.");
+    break;
+  case "Cherries":
+    alert("Cherries are $3.00 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    alert("Mangoes and papayas are $2.79 a pound.");
+    break;
+  default:
+    alert("Sorry, we are out of " + expr + ".");
+}
+
+prompt("Is there anything else you'd like?",``);
