7ac6e5cc453028f1bb29e735b98e055cfbb12b05
AnnaPalehova <ann.konfetti@mail.ru>
2023-01-29 21:45:13 +0300

    добавила

---
+++ AnnPalexova/main.js
@@ -0,0 +1,15 @@
+function calc(a, b, operation) {
+    switch (operation) {
+    
+        case 'add':
+        return a + b;
+    
+        case 'multi':
+        return a - b;
+    
+        case 'subtract':
+        return a * b;  
+       }
+    }
+    console.log (calc(2,4,'add'));
+    
