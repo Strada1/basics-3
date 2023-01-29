857e9c6c45dd5984539cbe04d72de328f4eac3fb
KYKI <semkyki@gmail.com>
2023-01-29 19:26:47 +0300

    #13 Practice-3

--- per/main.js
+++ per/main.js
@@ -1,36 +1,51 @@
 const calc = (opperator, a, b) => {
-  if (typeof a == 'number' || typeof b == 'number') {
-    if (a !== 0 || b !== 0) {
-      let result = 0
-      switch (opperator) {
-        case 'add':
-        case '+':
-          result = a + b
-          break
-        case 'subtract':
-        case '-':
-          result = a - b
-          break
-        case '*':
-        case 'multi':
-          result = a * b
-          break
-        case 'delit':
-        case '/':
-          result = a / b
-          break
-        case 'procent':
-        case '%':
-          result = (a / b) * 100 + '%'
-        default:
-          'I not know this opperator'
+  if (
+    opperator === 'add' ||
+    opperator === '+' ||
+    opperator === '-' ||
+    opperator === 'subtract' ||
+    opperator === 'multi' ||
+    opperator === '*' ||
+    opperator === 'delit' ||
+    opperator === '/' ||
+    opperator === 'procent' ||
+    opperator === '%'
+  ) {
+    if (typeof a == 'number' && typeof b == 'number') {
+      if (a !== 0 && b !== 0) {
+        let result = 0
+        switch (opperator) {
+          case 'add':
+          case '+':
+            result = a + b
+            break
+          case 'subtract':
+          case '-':
+            result = a - b
+            break
+          case '*':
+          case 'multi':
+            result = a * b
+            break
+          case 'delit':
+          case '/':
+            result = a / b
+            break
+          case 'procent':
+          case '%':
+            result = (a / b) * 100 + '%'
+          default:
+            'I not know this opperator'
+        }
+        return result
+      } else {
+        return 'a or b = 0'
       }
-      return result
     } else {
-      return console.log('a or b = 0')
+      return ' a or b not number'
     }
   } else {
-    return console.log(' a or b not number')
+    return 'I not know this opperator'
   }
 }
 
@@ -43,3 +58,7 @@ console.log(calc('*', 597, 13)) // 7761
 console.log(calc('+', 597, 13)) // 610
 console.log(calc('-', 597, 13)) // 584
 console.log(calc('/', 597, 13)) // 45.92307692307692
+console.log(calc('%', 450, 67)) // 671.6417910447761%
+console.log(calc('-', 0, 24)) // a or b = 0
+console.log(calc('-', 34, 0)) // a or b = 0
+console.log(calc('&', 0, 24)) // I not know this opperator
