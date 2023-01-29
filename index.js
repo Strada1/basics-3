663c2b6c14b6c9f015396f9f933c6426bd8cd7d5
Vladimir Maevskiy <vmayevskiy3009@gmail.com>
2023-01-29 14:14:22 +0300

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function calc(operation, a, b) {
+    switch (operation) {
+        case "add":
+            console.log(a + b);
+            break;
+        case "multi":
+            console.log(a * b);
+            break;
+        case "subtract":
+            console.log(b - a);
+            break;    
+    }
+    
+}
