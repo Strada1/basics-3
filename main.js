02d8c29b72c4184d22007037c7b8f5e824a2b3f2
Petr Aksenov <p.aksenov@rubetek.ru>
2023-01-29 12:59:48 +0300

    push commit #2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function calc(a, b, operation) {
+function calc(a , b, operation) {
     switch (operation) {
       case "add":
-        return a + b;
+        return Number(a) + Number(b); // для сложения приводим тип нанных к number - по идее должен быть способ правильнее
       case "multi":
         return a * b;
       case "subtract":
@@ -15,15 +15,15 @@ function calc(a, b, operation) {
     return resultAdd + resultSubtract + resultMulti;
   };
   
-  let resultAdd = calc(1, 2, "add");
-  let resultMulti = calc(1, 2, "multi");
+  let resultAdd = calc("1", 2, "add");
+  let resultMulti = calc(1, "2", "multi");
   let resultSubtract = calc(3, 2, "subtract");
   
   let resultMissOperation = calc(1, 2, ""); // Если не ввели operation
   
-  console.log(resultAdd);
-  console.log(resultMulti);
-  console.log(resultSubtract);
+  console.log(resultAdd); // a + b
+  console.log(resultMulti); // a * b
+  console.log(resultSubtract); // a - b
   
   console.log(resultMissOperation); // покажет default
   
