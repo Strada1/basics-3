0f7a0438ddce63bd1e95773bbed4f72e0fa06cc3
Marina Frolova <mikiaef@gmail.com>
2023-01-29 10:55:59 +0300

    one string has changed

--- task13.js
+++ task13.js
@@ -1,23 +1,27 @@
 function calc(operation, a, b) {
-  switch (operation) {
-    case "add":
-    case "+":
-      return a + b;
+  if (isNaN(a) || isNaN(b)) {
+    console.log("You should write numbers");
+  } else {
+    switch (operation) {
+      case "add":
+      case "+":
+        return a + b;
 
-    case "multi":
-    case "*":
-      return a * b;
+      case "multi":
+      case "*":
+        return a * b;
 
-    case "subtract":
-    case "-":
-      return a - b;
+      case "subtract":
+      case "-":
+        return a - b;
 
-    default:
-      console.log("no result");
+      default:
+        console.log("no result");
+    }
   }
 }
-console.log(calc("add", 1, 7));
-console.log(calc("+", 1, 7));
+console.log(calc("add", "h", 7));
+console.log(calc("+", 3, 7));
 console.log(calc("*", 3, 5));
 console.log(calc("multi", 3, 5));
 console.log(calc("-", -1, 6));
