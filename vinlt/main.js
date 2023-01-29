baaf12c42d5b92be0915f2094a19caf2fe6103aa
кучанский cергей <vkuchanskyi@gmail.com>
2023-01-29 21:50:31 +0300

    switchTwo

--- vinlt/main.js
+++ vinlt/main.js
@@ -1,2 +1,16 @@
-console.log('123')
-console.log('VId')
+function culc(operation, a ,b) {
+	switch (operation) {
+		case 'add':
+			console.log(a + b)
+			break
+		case 'multi':
+			console.log(a * b)
+			break
+		case 'subtract':
+			console.log(a - b)
+			break
+	}
+}
+culc('add', 1, 2)
+culc('multi', 1, 2)
+culc('subtract', 3, 2)
