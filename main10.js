341be8290c7e18a298930001d989b194442a5ea9
ForeverBlade <tkp_r@mail.ru>
2023-01-29 12:33:12 +0300

    1 коммит

---
+++ main10.js
@@ -0,0 +1,13 @@
+function calc ( operation, a, b ) {
+    switch (operation){
+    case 'add': 
+        return a+b;
+    case 'multi': 
+        return a*b;
+    case 'subtract': 
+        return a - b;
+    }
+    }
+    console.log (calc('subtract', 89, 16)) // 73
+    console.log (calc('add', 15, 41)) // 56
+    console.log (calc('multi', 25, 4)) // 100
