bf001605ea02a2155a6060b53a7b7e036b586134
Alexander <alexander.lennikov@yandex.ru>
2023-01-29 22:42:19 +0300

    Добавил свои  файлы

---
+++ Alexander_L/main.js
@@ -0,0 +1,26 @@
+function calc(operation, a, b) {
+	switch (operation) {
+		case "add":
+			return a + b;
+
+		case "multi":
+			return a * b;
+
+		case "subtract":
+			return a - b;
+
+
+	}
+
+}
+
+const resultAdd = calc("add", 1, 2);
+console.log(resultAdd);
+
+const resultMulti = calc("multi", 1, 2);
+console.log(resultMulti);
+
+const resultSubtract = calc("subtract", 3, 2);
+console.log(resultSubtract);
+
+
