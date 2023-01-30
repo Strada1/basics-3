16d332cc08121f0a9c6060806c18c71cf0f92284
Mansur Khasanov <mansur@Mans.local>
2023-01-30 17:07:59 +0300

    Циклы 3 варианта

---
+++ thedexxter/main.js
@@ -0,0 +1,19 @@
+let i = 1;
+while (i < 20) {
+  console.log(i)
+  i = i + 1;
+}
+
+console.log('2 вариант')
+
+let j = 0;
+do {
+	j = j + 1;
+  console.log(j);
+} while (j < 19);
+
+console.log('3 вариант')
+
+for (let i = 1; i < 20; i++) {
+  console.log(i);
+}
