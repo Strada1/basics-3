8f30f7d81d5275f4a5bf313e6b5b936ecaec827a
Mansur Khasanov <mansur@MacBook-Pro-Mansur.local>
2023-01-30 16:41:52 +0300

    Добавление default значения

--- thedexxter/main.js
+++ thedexxter/main.js
@@ -9,8 +9,12 @@ function calc(operation, a, b) {
     case 'subtract':
       console.log(a-b);
         break;
+    default:
+      console.log('Неверное значение');
+      break; 
   }
 }
 calc('add', 1, 2); //3
 calc('multi', 1, 2); //2
 calc('subtract', 3, 2); //1
+calc('miss', 3, 2); // Неверное значение
