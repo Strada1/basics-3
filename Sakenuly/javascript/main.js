cd8dc3e410dd2d5def9fcfd11784f37119f31ad4
Sakenuly <sakenuly2016@yandex.ru>
2023-01-30 20:02:07 +0600

    V3 With function

--- Sakenuly/javascript/main.js
+++ Sakenuly/javascript/main.js
@@ -1,22 +1,29 @@
-let operation = prompt('add,multi,subtract');
-let a = +prompt('Первая цифра');
-let b = +prompt('Вторая цифра');
-let result;
-
-  switch(operation) {
-    case 'add': 
-    result = a + b;
-    break;
+function calc(){
+  const operation = prompt('add,multi,subtract');
+  const firstNumber = +prompt('Первая цифра');
+  const secondNumber = +prompt('Вторая цифра');
+  let result;
   
-    case 'multi':
-    result = a * b;
-     break;
-   
-    case 'subtract':
-    result = a - b;
-     break;
-  }
+  switch(operation) {
+  case 'add':
+     result = firstNumber + secondNumber; 
+    break;
+  case 'multi':
+      result = firstNumber * secondNumber;
+    break;
+  case 'subtract':
+      result = firstNumber - secondNumber;
+        break;
+}
+switch(result) {
+  case null:
+    case '':
+    case undefined:
+    return alert('Ошибка');
 
-alert(result);
+}
+alert(String(result)); 
 
-//версия только с switch
+}
+ 
+calc();
