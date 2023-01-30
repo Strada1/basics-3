7b0e5f7d34a30b93c4f50a14dbcb89c8732a1ae6
Salavat Khasanov <salavatkhasanov@MacBook-Air-Salavat.local>
2023-01-30 20:57:50 +0300

    Some changes

--- Khasanovjr/main.js
+++ Khasanovjr/main.js
@@ -1,6 +1,36 @@
-let userName = 'Art';
-let userAge = 31;
-console.log(userName + ' is ' + userAge + ' years old');
-let userHeight;
-console.log(userHeight);
-console.log(userHeight);
+/*function calc(operation, a, b) {
+    if (operation=='add') {
+        console.log(a + b);
+    }
+    else if (operation=='multi') {
+        console.log(a * b);
+    }
+    else if (operation=='subtract') {
+        console.log(a - b);
+    }
+    else {
+        console.log("Error")
+    }
+}
+*/
+
+function calc(operation, a, b) {
+    switch (operation) {
+        case "add":
+            console.log(a + b);
+            break;
+        case "multi":
+            console.log(a * b);
+            break;
+        case "subtract":
+            console.log(a - b);
+            break;
+        default:
+          console.log("Other");
+          break;
+      }
+}
+
+calc("add", 3, 2)
+calc("multi", 1, 2)
+calc("subtract", 3, 2)
