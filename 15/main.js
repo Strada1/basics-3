4600b9ab61d6dc0c981156418bc707017477525a
Daniil Pchelintsev <pchelincev.danya@bk.ru>
2023-01-30 23:50:07 +0300

    15pr

---
+++ 15/main.js
@@ -0,0 +1,21 @@
+
+
+// for (let i = 1; i < 20; i++){
+//     console.log(i)
+// }
+
+let i = 0 
+while (i < 20){
+    console.log(i)
+    i++ 
+}
+
+
+
+
+// let i = 0
+// for (; i < 3; ){
+//     console.log(i)
+//     i = i + 1
+// }
+
