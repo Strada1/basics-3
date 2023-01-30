b693f8f38d392f7953b6809e9292dcb2ac28ab38
Ruslan Rmv <ruslanrmv@gmail.com>
2023-01-30 23:01:19 +0300

    Trying FOR and WHILE with increment/decrement

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ for (let i = 1; i < 10; i++) {
 // 1 2 3 4 5 6 7 8 9
 
 
-// for (; i < 3;) {
-//   console.log(i);
-//   i = i + 1;
-// }
+for (; i < 3; i = i + 1) {
+  console.log(i); // Won't work
+}
 
 
 for (let i = 1; i < 10; i++) { 
@@ -15,3 +14,13 @@ for (let i = 1; i < 10; i++) {
   console.log(i);
 }
 // 1 3 5 7 9
+
+let i = 0;
+while (++i < 5) { // 1 2 3 4
+  console.log(i);
+}
+
+let i = 0;
+while (i++ < 5) {
+  console.log(i); // 1 2 3 4 5 
+} 
