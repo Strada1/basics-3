fabafd98eeff1a0f27eb3d0c53be59887d787ff4
AlenaIonshina <barinivaa218@gmail.com>
2023-01-30 13:46:16 +0300

    first commit

---
+++ AlenaIonshina/main.js
@@ -0,0 +1,19 @@
+//объявляем функцию culk
+function calc (operation, a, b){
+    switch (operation){
+        case 'add':
+            return a + b;
+            break;
+        case 'multi':
+            return a * b;
+            break;
+        case 'subtract':
+            return a - b;
+            break;  
+        default:
+            console.log('operation dont found')          
+    }
+}
+calc('add', 1, 2)
+calc('multi', 1, 2)
+calc('subtract', 3, 2)
