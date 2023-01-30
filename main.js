408144bc9a32121f6ea756f21c6569dc3deead0a
RenaultSport13 <truejdmfan@gmail.com>
2023-01-30 16:44:13 +0500

    Забыл удалить комментарии

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-// let a = 0;
-// while (++a < 5) {
-//   console.log(a); // от 1 до 4
-// }
-// console.log('\n');
+let a = 0;
+while (++a < 5) {
+  console.log(a); // от 1 до 4
+}
+console.log('\n');
 
-// let i = 0;
-// while (i++ < 5) {
-//   console.log(i); // от 1 до 5
-// }
-// console.log('\n');
+let i = 0;
+while (i++ < 5) {
+  console.log(i); // от 1 до 5
+}
+console.log('\n');
 
-// for (let q = 0; q < 5; q++) {
-//   console.log(q); // от 0 до 4 т.к.шаг выполняется после
-//                   // проверки условия и тела цикла
-// }
-// console.log('\n');
+for (let q = 0; q < 5; q++) {
+  console.log(q); // от 0 до 4 т.к.шаг выполняется после
+                  // проверки условия и тела цикла
+}
+console.log('\n');
 
-// for (let w = 0; w < 5; ++w) {
-//   console.log(w); // от 0 до 4 т.к.шаг выполняется после
-//                   // проверки условия и тела цикла
-// }
-// console.log('\n');
+for (let w = 0; w < 5; ++w) {
+  console.log(w); // от 0 до 4 т.к.шаг выполняется после
+                  // проверки условия и тела цикла
+}
+console.log('\n');
 
-// for (let p = 2; p <= 10; p++) {
-//  if (p % 2 == 0) {
-//   console.log(p);
-//  }
-// }
-// console.log('\n');
+for (let p = 2; p <= 10; p++) {
+ if (p % 2 == 0) {
+  console.log(p);
+ }
+}
+console.log('\n');
 
-// let z = 0;
-// while (z < 3) {
-//   console.log( `number ${z}!` );
-//   z++;
-// }
-// console.log('\n');
+let z = 0;
+while (z < 3) {
+  console.log( `number ${z}!` );
+  z++;
+}
+console.log('\n');
 
 let y = 15;
 
