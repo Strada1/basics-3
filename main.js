e31a4184ff85f28dcdb7b6bd7d5674c6e323912c
Ruslan Rmv <ruslanrmv@gmail.com>
2023-01-30 22:38:28 +0300

    Trying FOR and FOR-CONTINUE

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-let i = 1
-do {
-  console.log (i)
-  i = i + 1
-} while (i < 10)
+for (let i = 1; i < 10; i++) {
+  console.log(i)
+} 
+// 1 2 3 4 5 6 7 8 9
 
-// Result in console
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
+
+// for (; i < 3;) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+
+for (let i = 1; i < 10; i++) { 
+  if (i % 2 == 0) continue;
+  console.log(i);
+}
+// 1 3 5 7 9
