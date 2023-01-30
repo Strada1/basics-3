e709abf143a789abbf89de50acc820b9228abee8
Kvazarus <kvazarus13@yandex.ru>
2023-01-30 18:11:05 +0300

    #15 Циклы

--- Kvazarus/main.js
+++ Kvazarus/main.js
@@ -1,32 +1,21 @@
-function calc(operation, a, b) {
-    function add(a, b) {
-        return a + b;
-    }
-    function multi(a, b) {
-        return a * b;
-    }
-    function subtract(a, b) {
-        return a - b;
-    }
-
-    if (!a || !b) { return 'Error'; }
-
-    switch (operation) {
-        case '+':
-        case 'add':
-            return add(a, b);
-        case '*':
-        case 'multi':
-            return multi(a, b);
-        case '-':
-        case 'subtract':
-            return subtract(a, b);
-        default:
-            return 'Error';
-    }
+let i = 1
+/*
+while (i < 20) {
+    console.log(i)
+    i++;
 }
 
-let a = Number(prompt('Введите первое число'));
-let b = Number(prompt('Введите второе число'));
-operation = prompt('Введите операцию: add, multi, subtract');
-alert(calc(operation, a, b));
+i = 1
+do {
+    console.log(i);
+    i++;
+} while (i < 20);
+*/
+
+for (i; i < 20; i++) {
+    console.log(i);
+}
+
+for (let j = 1; j < 20; j++) {
+    console.log(j);
+}
