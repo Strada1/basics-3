d8a7b7f41e3efb42bba76409465e1edd4eb81e2d
Ruslan Rmv <ruslanrmv@gmail.com>
2023-01-30 21:42:11 +0300

    Trying function while

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-// Программа с множеством if
-function calc(operation, a, b) {
-
-    if (operation == "add") {
-     return a + b;
-    }
-
-    if (operation == "multi") {
-        return a * b;
-    }
-
-    if (operation == "substract") {
-        return a - b;
-    }
+let i = 1
+while (i < 10) {
+  console.log (i)
+  i = i + 1
 }
-
-let resultCalc = calc ("substract", 3, 2)
-
-console.log (resultCalc)
-
-// Программа calc со switch
-function calcSwitch(operation, a, b) {
-  switch (operation) {
-    case 'add':
-      return a + b;
-      break;
-
-    case 'multi':
-      return a * b;
-      break;
-
-    case 'substract':
-      return a - b;
-      break;
-  }
-
-}
-
-let resultCalcSwitch = calcSwitch('multi', 3, 2);
-
-console.log(resultCalcSwitch);
