703ed4209fdc2413d5dda1d81bda704814c3a311
Maxim Savelyev <maks@MacBook-Pro-Maks.local>
2023-01-30 21:24:42 +0300

    First commit

---
+++ maximsav/main.js
@@ -0,0 +1,16 @@
+
+function calc(operation, a, b) {
+    switch (operation) {
+    case 'add':
+       console.log(a +b);
+    case 'multi': 
+       console.log(a * b);
+    case 'subtract':
+       console.log(a - b);
+    }
+ }
+ calc(`add`, 1, 2); //3
+ calc(`multi`, 1, 2); //2
+ calc(`subtract`, 3, 2); //1
+
+ //changes
