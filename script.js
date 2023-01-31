43f54650c62afd6bd0e0be19b05db5574d017656
andrey ryabov <araibov@mail.ru>
2023-01-31 18:24:34 +0500

    first solution

---
+++ script.js
@@ -0,0 +1,18 @@
+let number = 0;
+
+switch (number) {
+    case 0:
+        console.log('число 0');
+        break;
+    case 1:
+        console.log('число 1');
+        break;
+
+    case 2:
+    case 3:
+        console.log('Число 2 или 3')
+        break;
+
+
+        
+}
