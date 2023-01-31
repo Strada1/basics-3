1c4bc44ee263b6be2624b24cd587bcf028b259ed
Ruslan Rmv <ruslanrmv@gmail.com>
2023-01-31 07:22:40 +0300

    Added objects with methods

--- main.js
+++ main.js
@@ -8,3 +8,32 @@ const address = {
 console.log (address) // all
 console.log (address.city) // New York
 console.log (address["city", "street"]) // New York Gagarina
+
+const contactBook = { 
+
+  address: {
+  "city": "New York",
+  "street": "Gagarina",
+  "houseNumber": 146,
+  "apartmentNumber": 66 
+  },
+
+  name: {
+    "firstName": "George",
+    "secondName": "Obalabama"
+  },
+
+  contacts: {
+    "phoneNumber": 17850032,
+    "telegram": "@geobalabama",
+    "twitter": "@suckmusk"
+  },
+
+  log () {
+    console.log(this.name)
+    console.log(this.contacts)
+  }
+
+};
+
+contactBook.log()
