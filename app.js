e6a18dd948efb5c83bf0446a9eaf0c2f219b7799
kihrynko <ki.hrynko@gmail.com>
2023-01-31 07:30:19 +0200

    added different methods

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ const telegramUserContacts = {
   'LordWood': 80002794575,
   'John': 80002814575,
   'Kirol': 80002894475,
+
+  checkValidation(value) {
+    if (typeof value !== 'string') {
+      return new Error('Incorrect type of data!');
+    }
+
+    if (value === '') {
+      return new Error('Empty string is not allowed!');
+    }
+  },
+  addNewContact(newContact, value) {
+    this[newContact] = value;
+  },
+  showContact(contact) {
+    if (this.checkValidation(contact)) {
+      return null;
+    }
+
+    if (!this[contact]) {
+      return null;
+    }
+
+    console.log(this[contact]);
+  },
 };
 
 telegramUserContacts['John'] = 80002894335;
 telegramUserContacts['Poll'] = 80002894311;
 delete telegramUserContacts.Tom;
+
+telegramUserContacts.showContact('');
+telegramUserContacts.addNewContact('Derail', 80002774335);
