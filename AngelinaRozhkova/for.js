b7288c1782d51e9fcb103b5a4430296fa7a74ba9
AngelinaRozhkova <angerozhkova@gmail.com>
2023-01-31 19:28:58 +0300

    added method and new contact

--- AngelinaRozhkova/for.js
+++
@@ -1,9 +0,0 @@
-// for  (let i=1; i<20; i++) {
-//     console.log(i);
-// }
-for (let i = 0; i < 10; i++) {
-    // если true, пропустить оставшуюся часть тела цикла
-    if (i % 2 == 0) continue;
-    console.log(i); // 1, затем 3, 5, 7, 9
-}
-
