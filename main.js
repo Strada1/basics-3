cea96844c0a19b7ff9cec5850f29cfb0b8a19d75
Ruslan Rmv <ruslanrmv@gmail.com>
2023-01-31 07:21:10 +0300

    Added address object

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-for (let i = 1; i < 10; i++) {
-  console.log(i)
-} 
-// 1 2 3 4 5 6 7 8 9
+const address = {
+  "city": "New York",
+  "street": "Gagarina",
+  "houseNumber": 146,
+  "apartmentNumber": 66 
+};
 
-
-for (; i < 3; i = i + 1) {
-  console.log(i); // Won't work
-}
-
-
-for (let i = 1; i < 10; i++) { 
-  if (i % 2 == 0) continue;
-  console.log(i);
-}
-// 1 3 5 7 9
-
-let i = 0;
-while (++i < 5) { // 1 2 3 4
-  console.log(i);
-}
-
-let i = 0;
-while (i++ < 5) {
-  console.log(i); // 1 2 3 4 5 
-} 
+console.log (address) // all
+console.log (address.city) // New York
+console.log (address["city", "street"]) // New York Gagarina
