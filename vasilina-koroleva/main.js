d65668ba6e3a7c0b9abd0673471082ff688f189f
vasilina <vasilina.koroleva.1996@mail.ru>
2023-01-31 21:12:41 +0700

    #16 practice

--- vasilina-koroleva/main.js
+++ vasilina-koroleva/main.js
@@ -1,37 +1,28 @@
-// while
-let b = 1;
-while (b < 20) {
-    console.log(b);
-    b = b + 1;
+//objects
+
+const telegramContacts = {
+    'Danya': 89999999999,
+    'Polly': 83333333333,
+    'Oleg Petrovich': 89201902929
 }
 
-//do while
+console.log(telegramContacts);
+console.log(telegramContacts.Polly);
+console.log(telegramContacts['Oleg Petrovich']);
+delete telegramContacts['Danya'];
+telegramContacts['Krotik'] = 87776665544;
+console.log(telegramContacts);
 
-let c = 1;
-do {
-    console.log(c);
-    c = c + 1;
-} while (c < 20);
+//methods
 
-//for
-
-for (let i = 1; i < 4; i++) {
-    console.log(i);
-}
-
-for (let i = 1; i < 20; i++) {
-    console.log(i);
-}
-
-//break
-while (true) {
-    console.log('start');
-    break;
-    console.log('finish')
-}
-console.log('the end');
-//continue
-for (let i = 0; i < 10; i++) {
-    if (i % 2 == 0) continue;
-    console.log(i);
-}
+const phoneBook = {
+    list: {
+        'john': 123,
+        'jane': 456,
+        'jim': 789
+    },
+    log() {
+        console.log(this.list)
+    }
+};
+phoneBook.log();
