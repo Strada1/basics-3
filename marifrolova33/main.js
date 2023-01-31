e5e3d22ee738328ce94f65bc033e418ab0e39b64
Marina Frolova <mikiaef@gmail.com>
2023-01-31 22:31:23 +0300

    object with myCat

--- marifrolova33/main.js
+++ marifrolova33/main.js
@@ -1,42 +1,8 @@
-const phoneBook = {
-  "Mari Fr": 7911783,
-  "Lena More": 79091,
-  Misa: 792158,
-  Homebank: 100000,
+const myCat = {
+  age: 3,
+  name: "Misa",
+  "cat color": "grey",
+  owner: "Marina",
 };
-
-//вызываем сам обЪект phoneBook
-console.log(phoneBook);
-//получить доступ к Misa
-console.log(phoneBook.Misa);
-
-// строки в квадратных скобках
-console.log(phoneBook["Lena More"]);
-console.log(phoneBook["Mari Fr"]);
-
-//добавить запись
-phoneBook.Mother = 8937379;
-console.log(phoneBook);
-
-//обновить запись
-phoneBook["Lena More"] = "lpraud";
-console.log(phoneBook);
-
-//удалить запись
-delete phoneBook["Homebank"];
-console.log(phoneBook);
-
-// Методы
-const anotherPhoneBook = {
-  list: {
-    "Mari Fr": 7911783,
-    "Lena More": 79091,
-    Misa: 792158,
-    Homebank: 100000,
-  },
-  log() {
-    console.log(this.list); //ключевое слово - для вывода информации, которая есть в самом объекте
-  },
-};
-
-anotherPhoneBook.log();
+myCat["cat color"] = "grey and yellow";
+console.log("Hello, " + myCat.name + "!");
