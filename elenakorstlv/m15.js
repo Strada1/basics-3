3dcc66533ccc17334a37bc29d467fbf7f74aada1
Elena-Kr <korostellen@gmail.com>
2023-01-31 23:36:48 +0500

    while

---
+++ elenakorstlv/m15.js
@@ -0,0 +1,17 @@
+let i = 1;
+while (i < 20){
+    console.log(i)
+    i = i + 1;
+}
+
+let k = 1;
+do{
+    console.log(k);
+    k = k +1;
+}while(k < 20);
+
+/*
+for (инициализация; условие; шаг) {
+  // тело цикла
+}
+*/
