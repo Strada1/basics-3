f2ab6240605e11c7b0141c68d624ac575a15ac00
andrey ryabov <araibov@mail.ru>
2023-01-31 16:09:21 +0500

    первый

---
+++ exstp/script.js
@@ -0,0 +1,22 @@
+const user = {
+    
+    userData: {
+        'name': 'Andrey',
+        'surname': 'Ryabov',
+        'age': 19
+    },
+    
+    push() {
+        let key = prompt('Введите ключ : ');
+        let body = prompt('Введите значение :');
+        this.userData[key] = body;
+    },
+
+    log() {
+        console.log(this.userData)
+    }
+    
+}
+
+user.userData['name'] = 'max'
+console.log(user.userData['name'])
