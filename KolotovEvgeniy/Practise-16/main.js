6f351a17731a83e6fb59e17072c1edd21cd8944f
KolotovEvgeniy <kolotov.e@gmail.com>
2023-01-31 16:12:16 +0300

    New branch

---
+++ KolotovEvgeniy/Practise-16/main.js
@@ -0,0 +1,16 @@
+const phoneBook = {
+  list: {
+    "I": 111111,
+    "Wife": 222222,
+    "Son Tim": 333333,
+    "Mom": 444444,
+    "Girl-Lili": 555555,
+    "Girl-Lili (black)": 666666,
+    "Irina 69": 696969
+  },
+  log() {
+    console.log(this.list)
+  }
+};
+
+phoneBook.log();
