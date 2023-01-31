bfc83fb141e09c187aa87c7487bb1fbf5a41474a
Oika <seryy.gordeev.78@mail.ru>
2023-01-31 21:04:59 +0700

    I hope it's last test

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("Hello world")
+function calc(x, y, operation){
+    let r;
+    switch (operation) {
+        case "mult":
+            r= x*y;
+            break;
+        case "add":
+            r = x+y;
+            break
+        case "sub":
+            r = x - y;
+            break;
+        case "div":
+            r= x/y;
+            break;
+    }
+    return r;
+
+}
+
+console.log(calc(2,2,"mult"))
+
+console.log(calc(2,2,"div"))
+
+console.log(calc(2,2,"add"))
+
+console.log(calc(2,2,"sub"))
