4c268e2b6c4eb5b91219203a98d1faeba1b832e6
kihrynko <ki.hrynko@gmail.com>
2023-01-31 07:15:28 +0200

    created a directory with mine contacts from telegram

---
+++ app.js
@@ -0,0 +1,10 @@
+const telegramUserContacts = {
+  'Tom': 80002894575,
+  'LordWood': 80002794575,
+  'John': 80002814575,
+  'Kirol': 80002894475,
+};
+
+telegramUserContacts['John'] = 80002894335;
+telegramUserContacts['Poll'] = 80002894311;
+delete telegramUserContacts.Tom;
