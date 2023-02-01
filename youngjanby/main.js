e209f7cd546a893eb178aff202d87de3e0a3d9fd
youngjanby <janby3521@gmail.com>
2023-02-01 19:01:04 +0300

    Проверка ключа

--- youngjanby/main.js
+++ youngjanby/main.js
@@ -1,13 +1,18 @@
 const myPhoneBook = {
     list: {
-        'Number1': 11111111,
-        'Number2': 22222222,
-        'Number3': 33333333,
+        'Number1': 1111,
+        'Number2': 2222,
+        'Number3': 3333,
     },
     add(name, number) {
-        this.list[name] = number; //  number пишется не с большой буквы!! ты присваиваешь значение this.list[name] функции Number, а не параметру number из метода add.
+        this.list[name] = number;
     }
 };
 
-myPhoneBook.add('Number4', 4444444);  //    функция объявляется с круглыми скобками
-console.log(myPhoneBook.list['Number4'])    
+if(myPhoneBook.list.Number1) {
+    console.log('i m here!')
+}
+
+
+myPhoneBook.add('Number4', 4444);
+console.log(myPhoneBook.list['Number4'])
