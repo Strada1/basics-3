173aa1a5ce9e7b45dfac54509f060cb91a69284c
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-01 20:14:19 +0300

    Used operator IF for checking existing of value

--- main.js
+++ main.js
@@ -11,12 +11,32 @@ const contactBook = {
   },
 
   delete(name) {
-   delete this.list[name];
-  }
+    delete this.list[name];
+  },
 };
 
-const fullName = 'Ivan Meshkov'
+  if (contactBook.list['Ivan Meshkov']) {
+    console.log('Ivan is here');
+  }
 
-contactBook.delete(fullName);
+
+// contactBook.delete(fullName);
 console.log(contactBook.list['Ivan Meshkov'])
 
+// const phoneBook = {
+// 	list: {
+// 		"John": 123456789,
+// 		"Jane Doe": 987654321,
+// 		"Jim Smith": 111111111
+// 	}
+// };
+
+// if (phoneBook.list['Jane Doe']) { // свойство существует
+// 	console.log('Jane is here!'); // John is here!
+// }
+
+// if (phoneBook.list.Art) { // свойства не существует
+// 	console.log('Art is here!');
+// }
+
+// console.log(phoneBook.list['Jane Doe']); // undefined
