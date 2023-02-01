37447854f3945323a799ddf3da80a144cd53a03f
Stanicovsci <stanicovsci@gmail.com>
2023-02-01 20:23:23 +0300

    Refactoring

--- VorStaNic/day17.js
+++ VorStaNic/day17.js
@@ -13,3 +13,15 @@ const phoneBook = {
   phoneBook.add("Sarah Johnson", 5555);
   console.log(phoneBook.list['Sarah Johnson']);
   
+
+  const phoneBook = {
+    list: {
+      "John": 123456789,
+      "Jane Doe": 987654321,
+      "Jim Smith": 111111111
+    },
+  };
+  
+  for (const name in phoneBook.list) {
+    console.log(name + ' ' + phoneBook.list[name] ); // "John 123456789"
+  }
