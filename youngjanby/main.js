08d7bf7efe886086b0fb765f6e1b7537959e4d57
youngjanby <janby3521@gmail.com>
2023-02-01 20:02:32 +0300

    Пояснение по поводу ключей

--- youngjanby/main.js
+++ youngjanby/main.js
@@ -1,12 +1,20 @@
-const myPhoneBook = {
+const phoneBook = {
     list: {
         'Number1': 1111,
         'Number2': 2222,
         'Number3': 3333,
     },
-
-};
-
-for (const name in myPhoneBook.list) {
-console.log(name + ' - ' + myPhoneBook.list[name])
+    add(name, number) {
+        phoneBook.list[name] = number
+    }
 }
+
+phoneBook.add('Number4', 4444)
+for(const name in phoneBook.list) {
+    console.log(name + ' - ' + phoneBook.list[name])  // name - это ключ myPhoneBook.list[name]  - это означает, что ты просишь js в объекте myPhoneBook найти запись с ключом list, в значении которого объект, и у него по ключу [name] получить значение
+}
+
+
+
+
+
