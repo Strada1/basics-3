473abc1cc4d6d46e95c07017dfcc725d5d4567e0
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-01 19:01:34 +0300

    Corrected key value of Ivan -_-

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const contactBook = { 
 
   name: {
-    "Ivan Mehskov": 88992234578,
+    "Ivan Meshkov": 88992234578,
     "Dmitry Polivtsev": 88993746278,
     "Albert Akhtyamov": 88993746372
-  },
+  }
 
 };
 
-let fullName = 'Ivan Meshkov' // Переменнаая - пользователь вводит имя
+const fullName = 'Ivan Meshkov' // Переменнаая - пользователь вводит имя
 console.log (contactBook.name[fullName]) // Имя как ключ // получаем номер Ивана
+
+// const phoneBook = {
+// 	list: {
+// 		"John": 123456789,
+// 		"Jane Doe": 987654321,
+// 		"Jim Smith": 111111111
+// 	}
+// };
+
+// const fullName = 'Jane Doe'
+// console.log(phoneBook.list[fullName]); // 987654321
