84cf6506f2f45d48020e937c2147638801e18463
Kvazarus <kvazarus13@yandex.ru>
2023-02-01 18:42:33 +0300

    #17 Объекты: подробнее

--- Kvazarus/main.js
+++ Kvazarus/main.js
@@ -2,8 +2,14 @@ const teleg = {
     spam: '88005553535',
     "just number": '8536475610',
     infoOut() {
-        console.log(this.spam);
-    }
+        console.log(this);
+    },
+    add(name, value) {
+        this[name] = value;
+    },
+    removal(name) {
+        delete this[name];
+    },
 }
 
 console.log(teleg.spam, teleg["just number"]);
@@ -11,6 +17,18 @@ console.log(teleg.spam, teleg["just number"]);
 teleg["justNumber"] = teleg["just number"];
 delete teleg['just number'];
 teleg.spam = '+78005553535'
-console.log(teleg);
-
 teleg.infoOut();
+
+
+teleg.add("list", {});
+teleg.infoOut();
+teleg.list["spam"] = teleg.spam;
+teleg.removal("spam");
+teleg.infoOut();
+teleg.list['justNumber'] = teleg['justNumber'];
+teleg.removal('justNumber');
+teleg.infoOut();
+
+for (const name in teleg.list) {
+    console.log(name, teleg.list[name]);
+}
