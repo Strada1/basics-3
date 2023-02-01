043c75c0c929ece47a425526ade2e9f3faa6f80f
Artem <archiarchiv@gmail.com>
2023-02-01 20:44:27 +0300

    new lesson added

--- buben_95/objects.js
+++ buben_95/objects.js
@@ -1,4 +1,4 @@
-const tgList = {
+/* const tgList = {
     list: {
     "Immaculated": 88888888888,
     "Duskbringer": 77777777777,
@@ -21,4 +21,36 @@ const tgList = {
   console.log(tgList["Ks Barrrs"]);
   console.log(tgList["Kostynokit"]);
   
-  tgList.log();
+  tgList.log(); */
+
+  const tgContacts = {
+    list: {
+    "Immaculated": 88888888888,
+    "Duskbringer": 77777777777,
+    "Kostynokit": 66666666666,
+    "Bog Chervey": 5555555555
+    },
+    addContact(name, number) {
+      this.list[name] = number;
+    },
+    deleteContact(name){
+      delete this.list[name];
+    },
+  };
+    
+tgContacts.addContact("Ks Barrrs", 4444444444);
+tgContacts.deleteContact('Kostynokit');
+
+console.log(tgContacts);
+
+const phoneBook = {
+    list: {
+      "John": 123456789,
+      "Jane Doe": 987654321,
+      "Jim Smith": 111111111
+    },
+  };
+  
+  for (const name in phoneBook.list) {
+    console.log(`${name} - ${phoneBook.list[name]}`);
+  }
