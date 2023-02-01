e18ab512f2b6ccce26aa6bed49e09765725ee2f6
andrey ryabov <araibov@mail.ru>
2023-02-01 15:25:37 +0500

    улучшил вывод пользователей

--- #17ObjectsDetails/script.js
+++ #17ObjectsDetails/script.js
@@ -4,12 +4,23 @@ const phoneBook = {
         "Jane Doe": 987654321,
         "Jim Smith": 111111111
     },
+
     add(name, number) {
         this.list[name] = number;
     },
+
     del(name) {
         delete this.list[name];
     },
+
+    log(name) {
+        if (name in this.list) {
+            console.log(`${name} - ${this.list[name]}`);
+        } else {
+            console.log(`Пользователь "${name}" не найден`);
+        }
+    },
+
     logAll() {
         for (const name in this.list) {
             console.log(`${name} - ${this.list[name]}`)
@@ -17,6 +28,9 @@ const phoneBook = {
     }
 }
 
+
+
+
 phoneBook.add('Andrey', 12312312);
-phoneBook.del('John');
+phoneBook.log('Andrey');
 phoneBook.logAll();
