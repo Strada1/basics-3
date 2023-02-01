735ac47be47d406fa4ffb6dbde2f71cc6a3fc83a
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-01 19:42:52 +0300

    Adding contact method

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const contactBook = { 
 
-  name: {
+  list: {
     "Ivan Meshkov": 88992234578,
     "Dmitry Polivtsev": 88993746278,
     "Albert Akhtyamov": 88993746372
-  }
+  },
 
+  add(name, number) {
+    this.list[name] = number;
+  }
 };
 
-const fullName = 'Ivan Meshkov' // Переменнаая - пользователь вводит имя
-console.log (contactBook.name[fullName]) // Имя как ключ // получаем номер Ивана
+contactBook.add('Darya Lyzhina', 88993748293);
+console.log(contactBook.list['Darya Lyzhina'])
+
