c3803963012b6e4cb13aab6dbb5e2be9a5baf50b
sergio-kuk <kuksenko@rt-bpm.ru>
2023-02-02 20:43:41 +0300

    сделал циклы

--- sergio-kuk/main.js
+++ sergio-kuk/main.js
@@ -1,39 +1,15 @@
-
-function calc(operation, a, b) {
-    let result;
-    if (typeof a != 'number' || typeof b != 'number') {
-        console.log('Неверный тип аргумента. Должно быть два числа');
-        return;
-    }
-    switch (operation) {
-        case 'add': 
-            result = a + b;
-            break;
-        case 'multi': 
-            result = a * b;
-            break;
-        case 'subtract':
-            result = a - b;
-            break;
-        default:
-            console.log('Неизвестное действие');
-    }
-    return result;
+let i = 0;
+while (i < 19) {
+    console.log('итерация №', ++i);
 }
 
-let r;
-r = calc('add', 1, 2);      // 3
-console.log(r);
+i = 1;
+do {
+    console.log(i++);
+} while (i < 20)
 
-r = calc('multi', 1, 2);    // 2
-console.log(r);
-
-r = calc('subtract', 11, 2);    // 9
-console.log(r);
-
-r = calc('divide', 10, 2);
-console.log(r);
-
-r = calc('subtract', '11', 2);
-console.log(r);
+for (i = 1; i < 20; i++) {
+    if (i % 2 == 0) continue;
+    console.log('#',i);
+}
 
