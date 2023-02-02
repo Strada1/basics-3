98adda7cc79cf21ccabfae338004fb14011b40f3
vasilina <vasilina.koroleva.1996@mail.ru>
2023-02-02 23:08:31 +0700

    #18 day

---
+++ vasilina-koroleva/practice-18/main.js
@@ -0,0 +1,30 @@
+/*const telegramContacts = {
+    list: {
+        'Danya': 89999999999,
+        'Polly': 83333333333,
+        'Oleg Petrovich': 89201902929,
+    },
+    add(name, number) {
+        this.list[name] = number;
+    },
+    delete(name, number) {
+        delete this.list[name];
+    }
+}
+
+delete telegramContacts['Danya'];
+telegramContacts['Krotik'] = 87776665544;
+console.log(telegramContacts);
+telegramContacts.delete('Polly');
+telegramContacts.add('Mark', 123123123);*/
+
+let user = {
+    name: 'john',
+    age: 30
+};
+
+
+let clone = Object.assign({}, user)
+
+alert(clone);
+
