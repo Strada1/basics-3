2bc63a8357fdef0ae9c9d7d978804cddae2fd09c
Sakenuly <sakenuly2016@yandex.ru>
2023-02-03 21:02:03 +0600

    ToDo

--- Sakenuly/javascript/main.js
+++ Sakenuly/javascript/main.js
@@ -1,22 +1,49 @@
-let operation = prompt('add,multi,subtract');
-let a = +prompt('Первая цифра');
-let b = +prompt('Вторая цифра');
-let result;
-
-  switch(operation) {
-    case 'add': 
-    result = a + b;
-    break;
-  
-    case 'multi':
-    result = a * b;
-     break;
-   
-    case 'subtract':
-    result = a - b;
-     break;
+const list = {
+  "create a new practice task": "In progress",
+  "make cookie": "Done",
+  "Meme Time": "In progress",
+  "Write code": "In progress",
+  "Learn js": "Done",
   }
+  
+  function changeStatus(task, status) {
+  
+  return list[task] = status, console.log('Статус задачи "' + task + '" изменен'); 
+  }
+  function addTask(task,status) {
+      if (task in list) {
+       return console.log("Уже существует"); 
+      }
+      else {
+      return list[task] = status = "To Do", console.log('Задача добавлена')
+    }
+  }
+  function deleteTask(task) { 
+    if (task in list) {
+      return delete list[task], console.log('Задача "' + task + '" удалена');
+    }
+    else{
+      console.log('Задачи "' + task + '" нет в списке' );
+    }
+  }
+  let doneCount = 0;
 
-alert(result);
 
-//версия только с switch
+  function showList() {
+    for (rezultat in list) { 
+      console.log(rezultat+ '  --  ' + list[rezultat]);
+
+      if (list[rezultat] == "Done") {doneCount = doneCount + 1;
+      }
+     
+    }
+    if (doneCount == 0) {
+      console.log('Ты лентяй!');
+    }
+      else if (doneCount > 0) {
+        console.log('Выполненных задач ' + doneCount);
+        }
+    }
+
+  showList();
+
