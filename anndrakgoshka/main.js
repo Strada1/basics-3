cfdb065201deff83256de521775011986288cc7a
Drakgoshka <drakgoshka@gmail.com>
2023-02-03 18:09:44 +0300

    todo-2

--- anndrakgoshka/main.js
+++ anndrakgoshka/main.js
@@ -1,33 +1,73 @@
 const list = {
-	'create a new practice task': 'In Progress',
-	'make a bed': 'Done',
+	
+	
 	'write a post': 'To Do',
 }
+
 function changeStatus (task, stat) {
+	if (list[task] !== stat) {
 	list[task] = stat;
+	}
 };
 
-function addTask(task, stat) {
+function addTask(task) {
+	if (!task) {
+		return console.log('error')
+	}
+	for (const currentTask in list) {
+		if(currentTask === task) {
+			return console.log('error')
+		}
+	}	
+	
+	if(task in list) {
+		return list[task] 
+	}
 	list[task] = 'To Do';
+	// list[task] = 'To Do';
+	
 };
 
-function deleteTask(task) {
-delete list[task];
+function deleteTask(task) {	
+	delete list[task];
 };
 
 function showList() {
 	
+	console.log('To Do');
+	// for (const task in list) {
+	// 	if (list[task] === 'To Do') {
+	// 		console.log(task)
+	// 	} else if (!task[list]) {
+	// 		console.log('Nothing is To Do');
+	// 	}
+	// 	break;
+	// }
+	
+
+	console.log('In Progress')
 	for (const task in list) {
-		console.log(`${task}: ${list[task]}`);
+		if (list[task] === 'In Progress') {
+			console.log(task)
+		} 
+		else {
+			console.log('Nothing')
+		}
+		break;
 		
+	}
+
+	console.log('Done')
+	for (const task in list) {
+		if (list[task] === 'Done') {
+			console.log(task)
+		} else if (!task[list]) {
+			console.log('Nothing is Done');
+		}
+		break;
 	}
 }
 
-
-
-addTask('have a walk','To Do');
-deleteTask('have a walk');
-changeStatus('create a new practice', 'To Do');
-changeStatus('make a bed', 'To Do')
-changeStatus('write a post', 'In Progress');
+addTask('xbox')
+console.log(list)
 showList();
