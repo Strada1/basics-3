f2414b0bb76a32c9a7d77221ba110232d417dd77
RenaultSport13 <truejdmfan@gmail.com>
2023-02-03 17:17:04 +0500

    ToDo

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-const phoneBook = {
+const toDo = {
   list: {
-    'John': 123456789,
-    'Jane Doe': 987654321,
-    'Jim Smith': 123459876
+  'Have a breakfast' : 'To do',
+  'Take a walk with the dog' : 'To do',
+  'Create a new practice task' : 'In progress',
+  'To cook dinner' : 'To do'
   },
+  addTask(task) {
+    this.list[task] = 'To do';
+
+  },
+  delTask(task) {
+    delete this.list[task];
+  },
+  changeStatus(task, status) {
+    this.list[task] = status;
+  },
+  showList() {
+    console.log(this.list);
+    for (task in toDo.list) {
+      if (toDo.list[task] == 'Done') {
+        break;
+      }
+      if (toDo.list[task] !== 'Done') {
+        console.log('Nothing is Done');
+        break;
+      }
+    }
+  }
 };
 
-for (const name in phoneBook.list) {
-console.log(`${name} - ${phoneBook.list[name]}`);
-}
+toDo.addTask('Go to sopping');
+toDo.delTask('To cook dinner');
+toDo.changeStatus('Take a walk with the dog', 'In progress');
+toDo.changeStatus('Have a breakfast', 'Done');
+toDo.showList();
