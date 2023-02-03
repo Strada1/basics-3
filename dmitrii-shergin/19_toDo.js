7a8a908d332990c226088de19efd778fade66c23
dmitrii-shergin <120621105+dmitrii-shergin@users.noreply.github.com>
2023-02-03 13:25:02 +0700

    added checks

--- dmitrii-shergin/19_toDo.js
+++ dmitrii-shergin/19_toDo.js
@@ -13,18 +13,51 @@ const list = {
 const toDoList = {
     list,
     statuses,
+
     changeStatus(task, status) {
-        this.list[task] = status
+        if (!(task && status)) {
+            console.log('enter the valid value')
+        }
+        else if (!(task in this.list)) {
+            console.log('there is no this task in list')
+        }
+        else if (!(status in this.statuses)) {
+            console.log('enter the valid status')
+        }
+        else {
+            this.list[task] = status
+        }
     },
+
     addTask(task) {
-        this.list[task] = TO_DO
+        if (task) {
+            if (!task.trim()) {
+                console.log('enter the valid value')
+            }
+            else if (task in this.list) {
+                console.log('this task has already been added')
+            }
+            else this.list[task] = TO_DO
+        }
+        else {
+            console.log('enter the valid value')
+        }
     },
+
     deleteTask(task) {
-        delete this.list[task]
+        if (task in this.list) {
+            delete this.list[task]
+        }
+        else {
+            console.log('there is no this task in list')
+        }
+
     },
+
     values() {
         return Object.values(this.list)
     },
+
     showList(status) {
         if (status) {
             console.log(`${status}:`)
@@ -65,26 +98,21 @@ toDoList.deleteTask('make a bed')
 toDoList.addTask('learn JS')
 toDoList.addTask('learn English')
 
-toDoList.showList(DONE)
-/*
-DONE:
-     -
-*/
-
+// log(showlist)
 toDoList.showList()
-/*
-IN PROGRESS
-     create a new practice task
-DONE
-     -
-TO DO
-     to go for a walk
-     learn JS
-     learn English
-*/
-
+toDoList.showList(DONE)
 toDoList.showList(IN_PROGRESS)
-/*
-IN PROGRESS:
-     create a new practice task
-*/
+
+// log(errors)
+toDoList.deleteTask()
+toDoList.deleteTask(' ')
+toDoList.deleteTask('asfasf')
+
+toDoList.addTask()
+toDoList.addTask('  ')
+toDoList.addTask('learn English')
+
+toDoList.changeStatus()
+toDoList.changeStatus('')
+toDoList.changeStatus('asasf')
+toDoList.changeStatus('learn English', 'asfs')
