0d462b9ec51f43d3a0e65bb56f299dc537594ec8
Leonid Niselovsky <leonisel@outlook.com>
2023-02-03 15:26:59 +0500

    init: start to-do project

--- leonisel/main.js
+++ leonisel/main.js
@@ -1,25 +1,69 @@
-// function calc(operation, a, b) {
-//   return operation == 'add' ? a + b :
-//   'multi' ? a * b :
-//   'subtract' ? a - b : 'oops';
-// }
-
-
-
-function calc(operation, a, b) {
-  switch(operation) {
-  case 'add':
-    return a + b;
-  case 'multi':
-    return a * b;
-  case 'subtract':
-    return a - b;
-  default:
-    return 'oooops dude ... something wrong';
-  }
+const STATUS = {
+  PROGRESS: "In Progress",
+  DONE: "Done",
+  TODO: "To Do"
 }
 
-console.log(calc('add', 3, 2));
+const list = {
+	"create a new practice task": STATUS.PROGRESS, 
+	"make a bed": STATUS.DONE,
+	"write a post": STATUS.TODO,
+}
+
+const changeStatus = (bussines, status) => {
+  list[bussines] = status;
+}
+
+const addTask = (newTask) => {
+  list[newTask] = STATUS.TODO;
+}
+
+const deleteTask = (task) => {
+  delete list[task];
+}
+
+const showList = () => {
+  let todoStr = '';
+  for(let key in list){
+    if(list[key] == STATUS.TODO) {
+      todoStr += `"${key}"\n    `; 
+    }
+  }
+  if(todoStr == ''){
+    todoStr = `    -`;
+  }
+  console.log(`Todo:\n    ${todoStr}`)
+
+  let progressStr = '';
+  for(let key in list){
+    if(list[key] == STATUS.PROGRESS) {
+      progressStr += `"${key}"\n    `;
       
+    }
+  }
+  if(progressStr == '') {
+    progressStr = '    -';
+  }
+  console.log(`In Progress:\n    ${progressStr}`)
+
+  let doneStr = '';
+  for(let key in list){
+    if(list[key] == STATUS.DONE) {
+      doneStr += `"${key}"\n    `;
+    }
+  }
+  if(doneStr == '') {
+    doneStr = `    -`;
+  }
+  console.log(`Done:\n    ${doneStr}`);
+}
+
+changeStatus("make a bed", STATUS.TODO);
+changeStatus("create a new practice task", STATUS.TODO);
+changeStatus("write a post", STATUS.PROGRESS);
+showList();
+
+
+
 
 
