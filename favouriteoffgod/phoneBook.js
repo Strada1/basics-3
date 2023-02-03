d3f3da91da42c9bfff478430e7db03ed878ad029
Tatiana Ershova <ershovati24@gmail.com>
2023-02-03 23:01:05 +0300

    phoneBook w/o methods

---
+++ favouriteoffgod/phoneBook.js
@@ -0,0 +1,26 @@
+const telegramContacts = {
+    "лучик света": "@sivveatna",
+    "НЕвынужденный друг": "@gsv_ylu",
+    "lisa": "@lihidden",
+    "Utochkin.": "@zoran22",
+    "таджана буджанова": "@tatiana2102",
+    "юлия": "@Vzd0r",
+    "Polina": "@pdbra",
+    "вальтер": "@bthrypp",
+    "като": "@kkkwqx",
+    "владislove": "@unbearable_void",
+    "настенька": "@Etica",
+    "кики": "@twigtwigtw",
+    "сабмиссив": "@klauss_vass",
+    "настин никита": "@nmihalyov",
+    "аленушка": "@olenabl",
+    "настя влада": "@cummio",
+    "Никита Арбузов": "@arbuztea"
+}
+
+telegramContacts["бариста из бравозза"] = "@FalseMontana";
+telegramContacts["андрей"] = "@zzzmnx";
+telegramContacts["катафалк"] = "@tobyretka";
+telegramContacts["леся"] = "@kolesiya";
+telegramContacts.Polina = "@pdbraa";
+delete telegramContacts.андрей;
