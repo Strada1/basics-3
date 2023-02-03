14a2ae54f614a0394f7bd4416d17614fc3f1da08
Drakgoshka <drakgoshka@gmail.com>
2023-02-03 11:24:49 +0300

    todo

--- anndrakgoshka/main.js
+++ anndrakgoshka/main.js
@@ -1,91 +1,26 @@
-const phonebook = {
-	'2 med': 9878154719,
-	'Natalia Kochetkova Dream': 9265579037,
-	'Karina': 9178267119,
-	'Sasha Atishev': 9101885571,
-	'Artem Street beat': 9110880460,
-	'Ruslan Street beat': 9277167255,
-	'Volodya Street beat': 9991721680,
-	'Bravik': 9991721680,
-	'Vary brovi': 9608117708,
-	'Vera': 9171674182,
-	'Gaydamakaainv': 9871600507,
-	'GB': 9272608061,
-	'Georgiy': 967255159,
-	'Irina Germanovna': 9178123491,
-	'Nastya Gradova': 9376537855,
-	'Guz': 9275068623, //скорректировать номер 89276058623
-	'Petr Dmitriev': 9372167659,
-	'Evgenij sber': 89277536026,
-	'Elena': 9276102214,
-	'Zaur': 9179148000,
-	'Tanya Zlobina': 9170127273,
-	'Igor': 9179411849,
-	'Ira': 9179486404,
-	'Vanya Karinin': 9879264216,
-	'Katya Smorodina': 9998276094,
-	'Katya': 9111482968,
-	'Street Lena': 9991724222,
-	'Lyuba': 9277594447,
-	'Bystrov A.M': 9277497039,
-	'Darya Mikhailovna': 9277040319,
-	'Nadyuha': 9099239655,
-	'Ekaterina nalogi': 9649808871,
-	'Dimas Nastin': 9277076523,
-};
+const todo = {
+ list: {
+	'create a new practice task': 'In Progress',
+	'make a bed': 'Done',
+	'write a post': 'To Do',
+},
+changeStatus (task, stat) {
+	this.list[tasl] = stat;
+},
+addTask(task) {
+	this.list[task] = 'To Do';
 
-phonebook.Nikolay = 9608268788; //исправить на 89608298788
-phonebook.Nyuta = 9150600269;
-phonebook['Mina psicholog'] = 927404452; //скорректировать на 9277704452
-phonebook['Lilya psicholog'] = 9277829443;
-phonebook.Ramil = 9272024062;
-phonebook['Irina rieltor'] = 9372053060;
-phonebook['Dzhamilya rieltor'] = 9178134518;
-phonebook['galina rieltor'] = 9171189388;
-phonebook.Ruzaliya = 9274015389;
-phonebook['Nastya ruk'] = 89379914036;
-phonebook['Nastya sber'] = 9171621109;
-phonebook['vika 15 sektor'] = 9024130619;
-
-delete phonebook['Artem Street beat'];
-delete phonebook['Mina psicholog'];
-delete phonebook['Irina rieltor'];
-delete phonebook['Dzhamilya rieltor'];
-delete phonebook['galina rieltor'];
-delete phonebook.Ruzaliya;
-delete phonebook['Nastya sber'];
-delete phonebook['vika 15 sektor'];
-delete phonebook['Darya Mikhailovna'];
-delete phonebook['Bystrov A.M'];
-delete phonebook['Bravik'];
-delete phonebook['Ekaterina nalogi'];
-delete phonebook['GB'];
-delete phonebook['Evgenij sber'];
-delete phonebook['Gaydamakaainv'];
-delete phonebook['Georgiy'];
-delete phonebook['Igor'];
-delete phonebook['Irina Germanovna'];
-delete phonebook['Katya Smorodina'];
-delete phonebook['Petr Dmitriev'];
-delete phonebook['Zaur'];
-delete phonebook['Volodya Street beat'];
-delete phonebook['Vary brovi'];
-delete phonebook['Street Lena'];
-delete phonebook['Ruslan Street beat'];
-delete phonebook['2 med'];
-delete phonebook.Nikolay;
-delete phonebook['Lilya psicholog'];
-
-phonebook['Natalia Kochetkova Dream'] = 11111111111;
-phonebook.Karina = 2222222222;
-phonebook['Sasha Atishev'] = 333333333;
-phonebook.Vera = 444444444;
-phonebook['Nastya Gradova'] = 55555555;
-phonebook.Guz = 666666666;
-phonebook.Elena = 777777777;
-phonebook['Tanya Zlobina'] = 888888888;
-phonebook.Ira = 99999999;
+},
+deleteTask() {
+delete this.list[task];
+},
+showList() {
+	for (const task in todo.list) {
+		console.log(`${stat}: \n ${task} \n`)
+	}
+}
+}
 
 
 
-console.log(phonebook);
+console.log(changeStatus(todo.list['write a post', 'Done']));
