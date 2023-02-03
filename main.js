d7b61dc00fc796f759e0ae3e0652e6bd8065385a
Robert <artemyoutubed@mail.ru>
2023-02-03 22:24:17 +0300

    #19 ПЕРВЫЙ ТОДО ЛИСТ ПРОСТОЙ ВАРИАНТ

--- main.js
+++ main.js
@@ -1,112 +1,84 @@
-const obj = {
-  list: {
-      "Андрей": 8952342243,
-      "Вася": 895235423,
-      "Маша": 892463463
-  },
-  add(name, number) {
-      this.list[name] = number
-  },
-  del(name) {
-      delete this.list[name]
-  }
+const list = {
+	"create a new practice task": "In Progress", 
+	"make a bed": "Done", // задача "заправить кровать" в статусе "Готово"
+	"write a post": "To Do",
 }
 
-
-obj.add("Артем", 89523552436)
-// console.log(obj)
-
-
-// obj.del("Андрей", 895235423)
-// console.log(obj)
-
-
-for(const a in obj.list) {
-  // console.log(a)
-  
-console.log(a, "-", obj.list[a])
-}
-
-
-// Задача на learn.js #1
-const user  = {}
-user["name"] = "John"
-user["surname"] = "Smith"
-user["name"] = "Pete"
-delete user["name"]
-// console.log(user)
-
-
-
-
-
-// Задача на learn.js #2
-let nullobj = {
-}
-function isEmpty(obj){
-  for(let a  in obj) {
-          return false
-  } 
-   return true
-}
-
-// console.log(isEmpty(nullobj)) 
-
-
-
-
-
-// Задача на learn.js #4
-
-let salaries = { 
-  John: 100,
-  Ann: 160,
-  Pete: 130
-}
-
-function sumObj(obj) {
-  let sum = 0;
-  for(let a in obj) {
-sum+= obj[a]
-
-  }
-  return sum
-}
-
-//   console.log(sumObj(salaries))
-
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu"
-};
-
-function multiplyNumeric(obj) {
-  for(let a in obj) {
-      if(typeof obj[a] ==  "number") {
-          obj[a] = obj[a] * 2
-      }
-  }
-}
-
-multiplyNumeric(menu);
-console.log(menu)
-
-
-
-
-
-
-const phoneBook = {
-    list: {
-      "John": 123456789,
-      "Jane Doe": 987654321,
-      "Jim Smith": 111111111
+const todoList = {
+    list,
+    addTask(task) {
+        this.list[task] = 'In Progress'
     },
-    add(name, number) { // добавили метод add с параметрами name и number
-      this.list[name] = number; // добавили number в свойство [name] свойства list
+    deleteTask(task) {
+        delete this.list[task]
+    },
+
+    changeStatus(task, status) {
+        this.list[task] = status
+    },
+    showList() {
+        a = 0;
+        b = 0;
+        c = 0;
+        for(key in this.list) {
+            if(this.list[key] === 'Done' && a == 0) {
+                a++
+                console.log(`\n${key} : \t${this.list[key]}`)
+            } else if(this.list[key] === 'Done' && a >= 1) {
+                console.log(`${key} : \t${this.list[key]}`)
+            }
+        }
+        for(key in this.list) {
+            if(this.list[key] === 'To Do' && b == 0) {
+                b++
+                console.log(`\n${key} : \t${this.list[key]}`)
+            } else if(this.list[key] === 'To Do' && b >= 1) {
+                console.log(`${key} : \t${this.list[key]}`)
+            }
+        }
+
+        for(key in this.list) {
+            if(this.list[key] === 'In Progress' && c == 0) {
+                c++
+                console.log(`\n${key} : \t${this.list[key]}`)
+            } else if(this.list[key] === 'In Progress' && c >= 1) {
+                console.log(`${key} : \t${this.list[key]}`)
+            }
+        }
+
+        if(a == 0) {
+            console.log(`\n\'Nothink is \'Done\'`)
+        }
+        if(b == 0) {
+            console.log(`\n\'Nothink is \'To Do\'`)
+        }
+
+        if(c == 0) {
+            console.log(`\n\'Nothink is \'In Progress\'`)
+        }
+
+
+
     }
-  };
-  
-  phoneBook.add("Sarah Johnson", 123123123);
-  console.log(phoneBook.list['Sarah Johnson']); // 123123123
+
+}
+
+
+todoList.addTask('first todo'); // добавляет новую задачу
+todoList.changeStatus("first todo", "To Do") // меняет статус задачи
+todoList.addTask('second todo'); // добавляет новую задачу
+todoList.changeStatus("second todo", "To Do") // меняет статус задачи
+
+todoList.addTask('first done'); // добавляет новую задачу
+todoList.changeStatus("first done", "Done") // меняет статус задачи
+todoList.addTask('second done'); // добавляет новую задачу
+todoList.changeStatus("second done", "Done") // меняет статус задачи
+
+todoList.addTask('first in proces'); // добавляет новую задачу
+todoList.addTask('second in proces'); // добавляет новую задачу
+
+
+
+
+
+todoList.showList(); // показывает список всех задач
