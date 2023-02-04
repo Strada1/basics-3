5306c1f8ad44ed6c0b3c7d465ae2b52fbcf06a6b
Egor <egorlobzov8@gmail.com>
2023-02-04 20:40:23 +0300

    переделал код как сказал Ярик, версия 3

--- ReZible/main.js
+++ ReZible/main.js
@@ -1,74 +1,65 @@
-function changeStatus(key, status){
-  if(!(key in list)){
-      return console.log("Такой задачи нет в списке");
-  }
-  if(!(status === "To Do" || status === "Done" || status === "In Progress")){
-      return console.log('Вы ввели неверный статус, доступные статусы: "To Do", "Done", "In Progress"');
-  }
-  const lastStatus = list[key];
-  list[key] = status;
-  console.log(`Статус для "${key}" был изменен с "${lastStatus}" на "${status}"`);
-}
-function addTask(key){
-  if(key in list){
-      console.log("Такая задачка уже есть");
-  }
-  if(key === "" || key === undefined){
-      console.log("Вы не ввели задачку");
-  }
-  list[key] = "To Do";
-  console.log(`Задача "${key}" была успешно добавлена в список`);
-}
-function deleteTask(key){
-  if(!(key in list)){
-      return console.log("Такой задачи нет в списке");
-  }
-  if(key === "" || key === undefined){
-      return console.log("Вы не ввели задачку для удаления");
-  }
-  delete list[key];
-  console.log(`Задача "${key}" была успешно удалена из списка`);
-}
-function showList(){
-  let text = "", 
-  todo = "Todo: \n" ,
-  progress = "Progress: \n",
-  done = "Done: \n",
-  todoFlag = true,
-  progressFlag = true,
-  doneFlag = true;
-  for(key in list){
-    switch(list[key]){
-      case "To Do":
-        todo += `\t ${key} \n`;
-        todoFlag = false;
-        break;
-      case "In Progress":
-        progress += `\t ${key} \n`;
-        progressFlag = false;
-        break;
-      case "Done":
-        done += `\t ${key} \n`;
-        doneFlag = false;
-        break;
-    }
-  }
-  if(todoFlag){
-    todo += '\t - \n';
-  }
-  if(progressFlag){
-    progress += '\t - \n';
-  }
-  if(doneFlag){
-    done += '\t - \n';
-  }
-  text = todo + progress + done;
-  console.log(text);
+const STATUS = {
+  "To Do": "To Do",
+  "Done": "Done",
+  "In Progress": "In Progress"
 }
 const list = {
-    "create a new practice task": "In Progress", 
-    "make a bed": "Done", // задача "заправить кровать" в статусе "Готово"
-    "write a post": "To Do",
+  "create a new practice task": "In Progress", 
+  "make a bed": "Done", // задача "заправить кровать" в статусе "Готово"
+  "write a post": "To Do"
+}
+function changeStatus(task, status){
+  if(!(task in list)){
+      return console.log("Такой задачи нет в списке");
+  }
+  if(!(status in STATUS)){
+      return console.log('Вы ввели неверный статус, доступные статусы: "To Do", "Done", "In Progress"');
+  }
+  const lastStatus = list[task];
+  list[task] = status;
+  console.log(`Статус для "${task}" был изменен с "${lastStatus}" на "${status}"`);
+}
+function addTask(task){
+  if(task in list){
+      console.log("Такая задачка уже есть");
+  }
+  if(task === "" || task === undefined){
+      console.log("Вы не ввели задачку");
+  }
+  list[task] = "To Do";
+  console.log(`Задача "${task}" была успешно добавлена в список`);
+}
+function deleteTask(task){
+  if(!(task in list)){
+      return console.log("Такой задачи нет в списке");
+  }
+  if(task === "" || task === undefined){
+      return console.log("Вы не ввели задачку для удаления");
+  }
+  delete list[task];
+  console.log(`Задача "${task}" была успешно удалена из списка`);
+}
+function test(status){
+    if(!(status in STATUS)){
+        return console.log('Вы ввели неверный статус, доступные статусы: "To Do", "Done", "In Progress"');
+    }
+    flag = false;
+    let text = `${status}: \n`;
+    for(key in list){
+        if(list[key] === status){
+            text += `\t ${key} \n`;
+            flag = true;
+        }
+    }
+    if(!flag){
+        text += '\t - \n';
+    }
+    console.log(text);
+}
+function showList(){
+    test("To Do");
+    test("Done");
+    test("In Progress");
 }
 changeStatus("write a post", "Done") // меняет статус задачи
 addTask("learn PHP"); // добавляет новую задачу
