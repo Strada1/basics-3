27f39610920fbbad780db1874811a41843ce6390
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-04 06:40:05 +0300

    ToDo with alert - problem with editTask function

--- main.js
+++ main.js
@@ -1,58 +1,94 @@
 const taskList = {
-list: {
-  "Выбрать банк": "ToDo",
-  "Составить план": "Done",
-  "Ограбить банк": "ToDo",
-  "Спрятать деньги": "In Progress"
-},
-
-//Функция добавления
-  add(name, status) {
-    this.list[name] = status
+  list: {
+    "Выбрать банк": "Done",
+    "Составить план": "Done",
+    "Ограбить банк": "ToDo",
+    "Спрятать деньги": "In Progress"
   },
   
-  //Функция удаления
-  delete(name) { 
-    delete this.list[name]
-  },
+    //Функция добавления
+    add(name, status) {
+      this.list[name] = status
+    },
 
-  changeStatus(name, status) {
-    this.list[name] = status
-  }
-  
+    //Функция удаления
+    deleteTask(name) { 
+      delete this.list[name]
+    },
+
+    changeStatus(name, status) {
+      this.list[name] = status
+    }
+      
 };
 
-/* ДЛЯ ПРОВЕРКИ РАЗНЫХ ФУНКЦИЙ
-ОБЪЯВИЛ РАЗНЫЕ ПЕРЕМЕННЫЕ, ЧТОБЫ
-МОЖНО БЫЛО ИСПОЛЬЗОВАТЬ ОДНОВРЕМЕННО 
-ДЛЯ НАГЛЯДНОСТИ. НА РЕАЛЬНОМ ПРОЕКТЕ ПЕРЕМЕННЫЕ
-НАЗВАНИЯ ЗАДАЧИ И СТАТУСА БУДЕТ ПО ОДНОЙ */
 
-// ПРОВЕРКА ФУНКЦИИ ДОБАВЛЕНИЯ ЗАДАЧИ
-const newTaskName = "Найти оружие" //Имя новой задачи
-const newTaskStatus = "To Do" //Статус новой задачи
-taskList.add(newTaskName, newTaskStatus); //Обращение к функции добавления
 
-//ПРОВЕРКА ФУНКЦИИ УДАЛЕНИЯ ЗАДАЧИ
-const deleteTaskName = "Выбрать банк"; //Имя удаляемой задачи
-taskList.delete(deleteTaskName); //Обращение к функции удаления
+// ОБЪЯВЛЕНИЕ ТИПОВ СТАТУСА ЗАДАЧИ
+const toDoStatus = 'ToDo'
+const defaultStatus = toDoStatus //значение статуса по умолчанию
+const inProgressStatus = 'In Progress'
+const doneStatus = 'Done'
+const deleteTask = 'Deleted'
 
-//ПРОВЕРКА ФУКНЦИИ ИЗМЕНЕНИЯ СТАТУСА ЗАДАЧИ
-const editingTaskName = 'Составить план'; //Имя изменяемой задачи
-const editingTaskStatus = 'In Progress'; //Новый статус задачи
-taskList.changeStatus(editingTaskName, editingTaskStatus); //Обращение к фукнции изменения
+// ВВОД ПЕРЕМЕННЫХ
+let taskName = "Ограбить банк";
+let taskStatus = inProgressStatus;
 
-//ВЫВОД В КОНСОЛЬ РЕЗУЛЬАТОВ
-console.log(taskList.list)
 
-/* РЕЗУЛЬТАТ
-"Выбрать банк" // – УДАЛЁН
 
-{
-  'Составить план': 'In Progress', // – ИЗМЕНИЛСЯ СТАТУС
-  'Ограбить банк': 'ToDo',
-  'Спрятать деньги': 'In Progress',
-  'Найти оружие': 'To Do' // – ДОБАВИЛСЯ
+//ФУНКЦИЯ ПРОВЕРКИ СТАТУСА ЗАДАЧИ
+function editStatus(status) {
+  switch (status) {
+    case inProgressStatus: // ЕСЛИ СТАТУС IN PROGRESS
+    case doneStatus: // ИЛИ ЕСЛИ СТАТУС DONE
+    return status;  // ВОЗВРАЩАЕТ УКАЗАННЫЙ СТАТУС
+    break;
+    
+    default: // В ОСТАЛЬНЫХ СЛУЧАЯХ ВОЗВРАЩАЕТ TO DO
+    return defaultStatus
+  }
 }
-*/
 
+
+
+function editTask (name, status) {
+  for(name in taskList.list) {
+    if(name === taskList.list && status !== deleteTask){
+      console.log(`Изменён статус задачи "${taskName}" на "${taskStatus}"`)
+      return taskList.changeStatus(taskName, taskStatus)
+    }
+  
+    if(name === taskList.list && status === deleteTask){
+      console.log(`Удалена задача "${taskName}"`)
+      return taskList.deleteTask(taskName, taskStatus)
+    }
+    
+    if (name !== taskList.list && status !== deleteTask) {
+      console.log(`Добавлена задача "${taskName}" со статусом "${taskStatus}"`)
+      return taskList.add(taskName, taskStatus)
+    }
+
+    if(name !== taskList.list && status === deleteTask){
+      console.log(`Задачи "${taskName}" нет в списке`)
+    }
+
+  break;
+}
+}
+  
+
+let functionTask = editTask(taskName, taskStatus)
+// let resultTaskName = functionTask
+// taskList.resultTaskName(taskName, taskStatus)
+
+
+// Проверка статуса
+let resultStatus = editStatus(taskStatus)
+
+
+// УВЕДОМЛЕНИЕ
+// console.log(alertText)
+
+// СПИСОК ЗАДАЧ
+console.log(taskList.list)
