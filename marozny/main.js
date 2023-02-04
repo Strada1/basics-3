2c2eb878d2d1875d4a797e3eba7488c79dbc5db7
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-02-04 02:08:52 +0100

    to do on objects

--- marozny/main.js
+++ marozny/main.js
@@ -1,28 +1,63 @@
-function calc(operation, a, b) {
-	if (operation && typeof operation === 'string') {
-		if (typeof a === 'number' && typeof b === 'number') {
-			switch (operation) {
-				case 'add':
-				case 'plus':
-					return a + b;
-				case 'multi':
-					return a * b;
-				case 'subtract':
-				case 'minus':
-					return a - b;
-				default:
-					return 'Unknown operation';
-			}
-		} else {
-			return 'A or B is not a number'
-		}
+const tasks = {
+	"create a new practice task": "In Progress",
+	"make a bed": "To Do",
+	"write a post": "To Do",
+};
+
+function changeStatus(name, status) {
+	tasks[name] = status;
+	return console.log("Status of task is successfully changed!");
+};
+
+function addTask(name, status) {
+	status ? tasks[name] = status : tasks[name] = "To Do";
+	return console.log("New task is added!");
+};
+
+function deleteTask(name) {
+	delete tasks[name];
+	return console.log("Task is successfully deleted!");
+}
+
+function showList() {
+	const cloneToDo = {};
+	const cloneInProgress = {};
+	const cloneDone = {};
+	for (let name in tasks) {
+		if (tasks[name] === "To Do") {
+			cloneToDo[name] = tasks[name];
+		} else if (tasks[name] === "In Progress") {
+			cloneInProgress[name] = tasks[name];
+		} else if (tasks[name] === "Done") {
+			cloneDone[name] = tasks[name];
+		};
+	};
+	if (Object.keys(cloneToDo).length === 0 && cloneToDo.constructor === Object) {
+		console.log("Nothing to do");
 	} else {
-		return 'Operation is empty'
+		for (let name in cloneToDo) {
+			console.log(name + ": " + cloneToDo[name])
+		}
+	}
+	if (Object.keys(cloneInProgress).length === 0 && cloneInProgress.constructor === Object) {
+		console.log("Nothing In Progress");
+	} else {
+		for (let name in cloneInProgress) {
+			console.log(name + ": " + cloneInProgress[name])
+		}
+	}
+	if (Object.keys(cloneDone).length === 0 && cloneDone.constructor === Object) {
+		console.log("Nothing to Done");
+	} else {
+		for (let name in cloneDone) {
+			console.log(name + ": " + cloneDone[name]);
+		}
 	}
 }
 
-//    calc('minus', 4, 4);
-//    calc(4, 4, 4);
-//    calc('', 1, 5);
-//    calc(' ', 4, 3);
-//    calc('multi', 'as', 2);
+// changeStatus("write a post", "Done") // меняет статус задачи
+// console.log(tasks);
+// addTask('have a walk'); // добавляет новую задачу
+// deleteTask('have a walk'); // удаляет задачу
+
+showList(); // показывает список всех задач
