37a2dcabfd92343c2f0fbbae0df7a9c21f3be45d
RenaultSport13 <truejdmfan@gmail.com>
2023-02-04 19:42:22 +0500

    My ToDo app

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
+const toDo = {
+  list: {
+    'Have a breakfast': 'To do',
+    'Take a walk with the dog': 'To do',
+    'Create a new practice task': 'In progress',
+    'To cook dinner': 'To do'
+  },
+  addTask(task) {
+    this.list[task] = 'To do';
 
-function calc( a, operation, b) {
-    
-    switch (operation) {
-        case '+':
-            return a + b;
-        
-        case '*':
-            return a * b;
-
-        case '-':
-            return a - b;
-    
-        default:
-            break;
+  },
+  delTask(task) {
+    delete this.list[task];
+  },
+  changeStatus(task, status) {
+    this.list[task] = status;
+  },
+  showList() {
+    let checkToDo = 0;
+    let checkInProgress = 0;
+    let checkDone = 0;
+    console.log('To do:');
+    for (task in toDo.list) {
+      if (toDo.list[task] == 'To do') {
+        console.log(task);
+        checkToDo++;
+      }
     }
-}
-  // if (operation == '+') {
-    //     return a + b;
-    // }
-    // else if (operation == '*') {
-    //     return a * b;
-    // }
-    // else if (operation == '-') {
-    //     return a - b;
-    // }
-    
-let add = calc(7, '-', 2); // 5
-let multi = calc(6, '+', 6); // 12
-let subtract = calc(17, '/', 27); //459
-console.log(add);
-console.log(multi);
-console.log(subtract);
+    if (checkToDo === 0) {
+      console.log('-');
+    }
+    console.log('In progress:');
+    for (task in toDo.list) {
+      if (toDo.list[task] == 'In progress') {
+        console.log(task);
+        checkInProgress++;
+      }
+    }
+    if (checkInProgress === 0) {
+      console.log('-');
+    }
+    console.log('Done:');
+    for (task in toDo.list) {
+      if (toDo.list[task] == 'Done') {
+        console.log(task);
+        checkDone++;
+      }
+    }
+    if (checkDone === 0) {
+      console.log('-');
+    }
+
+  }
+};
+
+toDo.addTask('Go to sopping');
+toDo.delTask('To cook dinner');
+toDo.changeStatus('Take a walk with the dog', 'Done');
+toDo.changeStatus('Have a breakfast', 'Done');
+toDo.showList();
