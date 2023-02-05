58c2f6966e1c2a3e0391deccbabe3a77c943c2d6
andrey ryabov <araibov@mail.ru>
2023-02-05 16:27:51 +0500

    вынес функции отдельно

--- #19 toDo/script.js
+++ #19 toDo/script.js
@@ -1,43 +1,44 @@
+function addTask(key) {
+    this.list[key] = 'Todo'
+};
+
+function changeStatus(key, value) {
+    this.list[key] = value;
+};
+
+function deleteTask(key) {
+    delete this.list[key];
+};
+
+function showList() {
+    for (let item in this.list) {
+        switch (this.list[item]) {
+            case 'Todo':
+                console.log(`"${item}" : Todo`);
+                break;
+            case 'In Progress':
+                console.log(`"${item}" : In Progress`);
+                break;
+            case 'Done':
+                console.log(`"${item}" : Done`);
+                break;
+            default:
+                console.log(`"${item}" : incorrect value`);
+        }
+    }
+    if (!Object.values(this.list).includes('Done')) {
+        console.log('Nothing is done!');
+    }
+};
+
 const toDo = {
     list: {
         "create a new practice task": "Todo",
         "make a bed": "In Progress",
         "write a post": "todo",
     },
-
-    changeStatus(key, value) {
-        this.list[key] = value;
-    },
-
-    addTask(key) {
-        this.list[key] = 'Todo';
-    },
-
-    deleteTask(key) {
-        delete this.list[key];
-    },
-
-    showList() {
-        for (let item in this.list) {
-            switch (this.list[item]) {
-                case 'Todo':
-                    console.log(`"${item}" : Todo`);
-                    break;
-                case 'In Progress':
-                    console.log(`"${item}" : In Progress`);
-                    break;
-                case 'Done':
-                    console.log(`"${item}" : Done`);
-                    break;
-                default:
-                    console.log(`"${item}" : incorrect value`);
-            }
-        }
-        if (!Object.values(this.list).includes('Done')) {
-            console.log('Nothing is done!');
-        }
-    }
-}
-toDo.addTask('do homework');
-toDo.changeStatus('write a post', 'Done');
-toDo.showList();
+    deleteTask,
+    changeStatus,
+    addTask,
+    showList
+};
