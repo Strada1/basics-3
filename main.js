af13249ae024171a84d4ff27dee6d0be79222077
ARSTSoftware <95930407+ARSTSoftware@users.noreply.github.com>
2023-02-06 20:59:47 +0700

    todo лист

---
+++ main.js
@@ -0,0 +1,36 @@
+let userName = 'Арс';
+let userAge = 31;
+let userHeight;
+console.log(userHeight);
+userHeight = 180;
+console.log(userHeight);
+console.log('меня зовут ' + userName + ' и мне ' + userAge);
+
+console.log(5 + 10);
+console.log(10 * 2);
+console.log(2 > 10);
+
+console.log(4 + 4);
+
+
+
+let a = 8;
+let b = a + ' Vertino';
+console.log(b);
+
+
+// console.log(5 > 4); // true
+// console.log("ананас" > "яблоко"); // false
+// console.log("2" > "12"); // true
+// console.log(undefined == null); // true
+// console.log(undefined === null); // false
+// console.log(null == "\n0\n"); //false
+// console.log(null === +"\n0\n"); // false
+
+// console.log('Apple' > 12);
+// console.log('false' > 12);
+// console.log(false > 12);
+
+let age = prompt('Сколько вам лет?', '');
+let result = (age > 18) ? true : false;
+console.log(result);
