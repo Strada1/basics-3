c5aa1714c0164b13b4949eaf498066769264b739
Maxim Savelyev <maximussav@yandex.ru>
2023-02-06 14:27:01 +0300

    Made bonus task

--- todo.js
+++ todo.js
@@ -24,31 +24,32 @@ function showList() {
         let report = name + " : " + toDo.list[name];
         if (toDo.list[name] === "Done") {
             console.log(report);
-        };
+        }
 
         if (toDo.list[name] === "In progress") {
             console.log(report);
-        }; 
+        }
 
         if (toDo.list[name] === "Shift to tomorrow") {
             console.log(report);
-        }; 
+        }
 
         if (toDo.list[name] === "Awaiting") {
             console.log(report);
-        }; 
-        
+        }
+
         if (toDo.list[name] === "Planned") {
             console.log(report);
-        }; 
+        }
     };
 };
 
 // Satus: Awaiting, In progress, Done, Shift to tomorrow, Planned
 
 toDo.changeStatus("Wake up", "Done");
+toDo.add("Clean the room", "Shift to tomorrow",);
 
-toDo.changeStatus("Brush the theeth", "Done",);
+toDo.changeStatus("Brush the theeth", "Planned",);
 toDo.changeStatus("Walk the dog", "Done");
 
 toDo.add("Meet with friends", "Planned",);
@@ -56,6 +57,21 @@ toDo.deleteTask("Clean the room");
 toDo.changeStatus("Visit the museum", "Done");
 
 toDo.changeStatus("Have a lunch", "Done",);
-toDo.add("Clean the room", "Shift to tomorrow",);
 
-showList();
+//showList();
+
+function showListMod(status) {
+    console.log(status + ":");
+    for (const name in toDo.list) {
+        if (toDo.list[name] === status) {
+            //console.log("   \"" + name + "\"");
+            console.log(`   \"${name}\"`); // разобрался в $
+        }
+    };
+};
+
+showListMod("Done");
+showListMod("Awaiting");
+showListMod("In progress");
+showListMod("Shift to tomorrow");
+showListMod("Planned");
