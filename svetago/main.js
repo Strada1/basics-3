d8e8d3e47bd5fbdb6e99b9ee5ceee81e4c8bb5c1
SvetaGo <golimbovskaya.svetlana@gmail.com>
2023-02-06 19:15:34 +0300

    урок 19, версия 2

--- svetago/main.js
+++ svetago/main.js
@@ -1,12 +1,10 @@
+const TODO = "To Do";
+const INPROGRESS = "In Progress";
+const DONE = "Done"
 const toDo = {
-    list: {
-    "приготовить завтрак": "Done",
-    "приготовить обед": "Done",
-    "приготовить ужин": "Done",
-    "разобрать вещи": "In Progress",
-    },
+    list: {},
     addTask(task) {
-        this.list[task] = "To Do";
+        this.list[task] = TODO;
     },
     deleteTask(task) {
         delete this.list[task];
@@ -15,13 +13,48 @@ const toDo = {
         this.list[task] = status;
     },
     showList() {
-        console.log(this.list)
-    },
-};
+        let i = 0;
+        console.log(TODO);
+        for (const task in this.list) {
+            if (this.list[task] === TODO) {
+                i++;
+                console.log(task+":", this.list[task]);
+            }
+        }
+        if (i === 0) {
+            console.log("Nothing is To Do");
+        } 
+
+        i = 0;
+        console.log(INPROGRESS);
+        for (const task in this.list) {
+            if (this.list[task] === INPROGRESS) {
+                i++;
+                console.log(task+":", this.list[task]);
+            }
+        }
+        if (i === 0) {
+            console.log("Nothing is In Progress");
+        } 
+
+        i = 0;
+        console.log(DONE);
+        for (const task in this.list) {
+            if (this.list[task] === DONE) {
+                i++;
+                console.log(task+":", this.list[task]);
+            }
+        }
+        if (i === 0) {
+            console.log("Nothing is Done");
+        } 
+    }
+}
+toDo.addTask("сделать зарядку");
+toDo.addTask("приготовить завтрак");
+toDo.addTask("приготовить обед");
+toDo.addTask("приготовить ужин");
 toDo.addTask("сделать домашнее задание");
-toDo.addTask("прочесть 1 главу книги");
-toDo.deleteTask("приготовить завтрак");
-toDo.changeStatus("разобрать вещи", "Done");
-toDo.changeStatus("сделать домашнее задание", "Done");
-toDo.changeStatus("прочесть 1 главу книги", "Done");
+toDo.deleteTask("сделать зарядку");
+toDo.changeStatus("приготовить завтрак", INPROGRESS);
 toDo.showList();
