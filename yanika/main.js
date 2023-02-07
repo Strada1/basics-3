7b7c8cd494e0babc0b367bb628459c0ebac932aa
Yana <goyana72@gmail.com>
2023-02-07 17:19:14 +0300

    функция преобразует строку в вертикальный вид

---
+++ yanika/main.js
@@ -0,0 +1,9 @@
+"use strict"
+//Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль
+function showVerticalMessage(anyWord) {
+    
+        for (let lol of anyWord) {
+            console.log(lol)
+        }
+    }
+showVerticalMessage("strada");
