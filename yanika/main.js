56122f369f88dbd5c79f9ea2ee8590f438434795
Yana <goyana72@gmail.com>
2023-02-07 17:27:06 +0300

    ф-я повышает регистр первой буквы,  показывая 7 символов

--- yanika/main.js
+++ yanika/main.js
@@ -1,9 +1,11 @@
 "use strict"
 //Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль
 function showVerticalMessage(anyWord) {
-    
-        for (let lol of anyWord) {
-            console.log(lol)
-        }
+    //Если строка начинается с буквы s - нужно вывести эту строку с первой заглавной буквой
+    //Если строка больше 7 символов - вывести только первые 7
+    console.log(anyWord.charAt(0).toUpperCase())
+    for (let lol of anyWord.slice(1, 7)) {
+        console.log(lol)
     }
-showVerticalMessage("strada");
+}
+showVerticalMessage("st rada");
