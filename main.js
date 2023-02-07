a6f9f4c4bf89cbbd1ff5fcba20e9186674058aac
Robert <artemyoutubed@mail.ru>
2023-02-07 07:56:27 +0300

    #23 Строки

--- main.js
+++ main.js
@@ -1,54 +1,15 @@
-const list = {  
-  "Открыть задание": "In Progress",
-  "Завершить задачу": "In Progress",
-  "Учить": "In Progress",
-  "Смотреть телек": "Done",
-  "Играть в кс": "Done",
-  "Что то еще делать": "Done", // задача "заправить кровать" в статусе "Готово"
-  // "Комментарии оставить": "To Do",
-  // "Как дела": "To Do",
-  // "Написать пост": "To Do"
-};
-
-const statuses = {
-  TO_DO: "To Do",
-  DONE: "Done",
-  IN_PROGRESS: "In Progress",
-  FLAG: false,
-};
-
-const todoList = {
-  list,
-  addTask(task) {
-    this.list[task] = statuses.TO_DO;
-  }, 
-  checkEmptyString(str = '') {
-    return Boolean(str);
-  },
-  deleteTask(task) {
-    delete this.list[task];
-  },
-  changeStatus(task, status) {
-    this.list[task] = status;
-  },
-  showList(status) {
-    for (let key in this.list) {
-      if (this.list[key] === status && !statuses.FLAG) {
-        console.log(`\n${this.list[key]} : \n ${key}  `);
-        statuses.FLAG = true;
-      } else if (this.list[key] === status && statuses.FLAG) {
-        console.log(`${key} `);
+const showVerticalMessage = (str) => {
+  let arr = str.split('')
+  if(str.startsWith('s') ||str.startsWith('S')) {
+      console.log('S')
+  }
+  for(let i =1; i < 6; i ++) {
+      if(arr[i] === undefined) {
+      }else {
+          console.log(arr[i])
       }
-    }
-    if (!statuses.FLAG) {
-      console.log(`\n${status}\n -`);
-    }
-    statuses.FLAG = false;
-  },
-};
+  }
+}
 
-todoList.addTask("have a walk");
-todoList.deleteTask("have a walk");
-todoList.showList(statuses.TO_DO);
-todoList.showList(statuses.DONE);
-todoList.showList(statuses.IN_PROGRESS);
+
+showVerticalMessage('Strada');
