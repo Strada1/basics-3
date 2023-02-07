499b4902f79ce9d7957a1c6f0a4d8756b8725bae
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-07 17:30:16 +0300

    Checking type of value feature

--- string.js
+++ string.js
@@ -1,11 +1,16 @@
 function showVerticalMessage (text) {
-    text = text.substring(0,7)
-        if (text.startsWith('s')) {        
-            text = text[0].toUpperCase() + text.slice(1)
-        } 
-    for (let char of text) {
-        console.log(char)
+    if (typeof(text) !== 'string') {
+        console.log('Wrong type of value')
+    } else {
+        text = text.substring(0,7)
+            if (text.startsWith('s')) {        
+                text = text[0].toUpperCase() + text.slice(1)
+            } 
+        for (let char of text) {
+            console.log(char)
+        }
     }
 }
 
-showVerticalMessage('stradahello')
+const testValue = '3'
+showVerticalMessage(testValue)
