c3196c27a374c90210b02a846fcd38234ea684d4
Tatiana Ershova <ershovati24@gmail.com>
2023-02-07 11:01:11 +0300

    Practice object's methods

--- favouriteoffgod/phoneBook.js
+++ favouriteoffgod/phoneBook.js
@@ -1,26 +1,38 @@
 const telegramContacts = {
-    "лучик света": "@sivveatna",
-    "НЕвынужденный друг": "@gsv_ylu",
-    "lisa": "@lihidden",
-    "Utochkin.": "@zoran22",
-    "таджана буджанова": "@tatiana2102",
-    "юлия": "@Vzd0r",
-    "Polina": "@pdbra",
-    "вальтер": "@bthrypp",
-    "като": "@kkkwqx",
-    "владislove": "@unbearable_void",
-    "настенька": "@Etica",
-    "кики": "@twigtwigtw",
-    "сабмиссив": "@klauss_vass",
-    "настин никита": "@nmihalyov",
-    "аленушка": "@olenabl",
-    "настя влада": "@cummio",
-    "Никита Арбузов": "@arbuztea"
-}
+    list: {
+        "лучик света": "@sivveatna",
+        "НЕвынужденный друг": "@gsv_ylu",
+        "lisa": "@lihidden",
+        "Utochkin.": "@zoran22",
+        "таджана буджанова": "@tatiana2102",
+        "юлия": "@Vzd0r",
+        "Polina": "@pdbra",
+        "вальтер": "@bthrypp",
+        "като": "@kkkwqx",
+        "владislove": "@unbearable_void",
+        "настенька": "@Etica",
+        "кики": "@twigtwigtw",
+        "сабмиссив": "@klauss_vass",
+        "настин никита": "@nmihalyov",
+        "аленушка": "@olenabl",
+        "настя влада": "@cummio",
+        "Никита Арбузов": "@arbuztea",
+    },
+    add (name, nickname) {
+        this.list[name] = nickname;
+    },
+    delete (name) {
+        delete this.list[name];
+    }
+};
 
-telegramContacts["бариста из бравозза"] = "@FalseMontana";
-telegramContacts["андрей"] = "@zzzmnx";
-telegramContacts["катафалк"] = "@tobyretka";
-telegramContacts["леся"] = "@kolesiya";
-telegramContacts.Polina = "@pdbraa";
-delete telegramContacts.андрей;
+telegramContacts.add("бариста из бравозза", "@FalseMontana");
+telegramContacts.add("андрей", "@zzzmnx");
+telegramContacts.add("катафалк", "@tobyretka");
+telegramContacts.add("леся", "@kolesiya");
+telegramContacts.add("Polina", "@pdbraa");
+telegramContacts.delete("андрей");
+
+for (const name in telegramContacts.list) {
+    console.log(name + " - " + telegramContacts.list[name]);
+};
