fbee7f6ada9b0470c325121a2d0b118301e4d5c4
Dmitry <dmitriikomarow@gmail.com>
2023-02-07 21:42:42 +0300

    function

--- dkomarov/main.js
+++ dkomarov/main.js
@@ -1,14 +1,14 @@
 
-
-let str;
 function showVerticalMessage(str) {
     if (str.startsWith('s')) {
-        let str2 = str[0].toUpperCase() + str.slice(1,6) ;
+        let str2 = str[0].toUpperCase() + str.slice(1,6);
         for (let char of str2) {
-            console.log(char)
+            console.log(char);
         } 
     } else {
-        console.log('start to "s"')
+        for (char of str) {
+            console.log(char);
+        }
     }
 } 
 showVerticalMessage("strada7");
