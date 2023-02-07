0c28b3e20d823c4063a4fd566217843fbbd452c2
Alex M <balurus37@gmail.com>
2023-02-07 19:21:48 +0300

    add string check and minor fixes

--- balu1973/main.js
+++ balu1973/main.js
@@ -1,10 +1,32 @@
 function showVerticalMessage(str) {
-  const firstSymbol = str[0] === "s" ? "S" : str[0];
-  str = firstSymbol + str.substring(1, 7);
+  if (!str || typeof str !== "string" || str.trim() === "") {
+    console.log("неверные входные данные!");
+    return;
+  }
 
+  str = str.trim();
+  const MAX_LENGTH_STRING = 7;
+  const SEARCH_CHAR = "s";
+  const firstChar = (str[0] === SEARCH_CHAR) ? SEARCH_CHAR.toUpperCase() : str[0];
+
+  str = firstChar + str.substring(1, MAX_LENGTH_STRING);
   for (const char of str) {
     console.log(char);
   }
 }
 
 showVerticalMessage("stradasss");
+console.log("------");
+showVerticalMessage("trada");
+console.log("------");
+showVerticalMessage("a");
+console.log("------");
+showVerticalMessage("   ");
+console.log("------");
+showVerticalMessage(null);
+console.log("------");
+showVerticalMessage(22);
+console.log("------");
+showVerticalMessage(undefined);
+console.log("------");
+showVerticalMessage(NaN);
