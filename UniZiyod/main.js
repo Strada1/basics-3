ba079da42b6635f8a5bea0a4fb44911443d352ee
UniZiyod <pupilstudy2001@gmail.com>
2023-02-07 11:57:03 +0500

    Задание 23 — СТРОКИ

--- UniZiyod/main.js
+++ UniZiyod/main.js
@@ -1,57 +1,24 @@
-// #19 Практика ToDo
+// #23 Строки
+/*
+Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль.
+Если строка начинается с буквы s - нужно вывести эту строку с первой заглавной буквой
+Если строка больше 7 символов - вывести только первые 7
+*/
 
-const todoList = {
-    listofTasks: {
-        'Изучить копирование объектов JS': 'To Do',
-        'Пройти урок в Strada': 'In Progress',
-        'Написать статью для заказчика': 'Done',
-    },
-    addTask(task) {
-        this.listofTasks[task] = 'To Do';
-    },
-    changeTask(task, status) {
-        this.listofTasks[task] = status;
-    },
-    deleteTask(name) { // Удаление задачи
-        delete this.listofTasks[name]
-    },
-    showTask() { // Показ всех задач
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        for (key in this.listofTasks) {
-            if (this.listofTasks[key] === 'Done' && a == 0) {
-                a++
-                console.log(`\n${key} :\t ${this.listofTasks[key]}`);
-            } else if (this.listofTasks[key] === 'Done' && a >= 1) {
-                console.log(`${key} :\t ${this.listofTasks[key]}`);
-            }
+let anyStr = 'semznakov';
+
+function showVerticalMessage(anyStr) {
+    if (anyStr.length > 7) {
+        console.log(anyStr[0].toUpperCase());
+        for (let showWords of anyStr.slice(1, 7)) {
+            console.log(showWords);
         }
-
-        for (key in this.listofTasks) {
-            if (this.listofTasks[key] === 'To Do' && b == 0) {
-                b++
-                console.log(`\n${key} :\t ${this.listofTasks[key]}`);
-            } else if (this.listofTasks[key] === 'To Do' && b >= 1) {
-                console.log(`${key} :\t ${this.listofTasks[key]}`);
-            }
+    } else {
+        console.log(anyStr[0].toUpperCase());
+        for (let showWords of anyStr.slice(1)) {
+            console.log(showWords);
         }
-
-        for (key in this.listofTasks) {
-            if (this.listofTasks[key] === 'In Progress' && c == 0) {
-                c++
-                console.log(`\n${key} :\t ${this.listofTasks[key]}`);
-            } else if (this.listofTasks[key] === 'In Progress' && c >= 1) {
-                console.log(`${key} :\t ${this.listofTasks[key]}`);
-            }
-        }
-
     }
 
 }
-todoList.addTask('Смонтировать видео')
-todoList.addTask('Попить кофе')
-todoList.addTask('Поесть торт')
-todoList.changeTask('Смонтировать видео', 'Done')
-todoList.changeTask('Поесть торт', 'In Progress')
-todoList.showTask()
+showVerticalMessage(anyStr)
