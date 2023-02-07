0ebb062f876d9cb054f6bc677856a31f3f4ebb32
KYKI <semkyki@gmail.com>
2023-02-07 19:16:22 +0300

    #23 Practice

--- String/mai.js
+++ String/mai.js
@@ -1,4 +1,26 @@
-let srt = 'Hi My Friend!'
-for (let i = 0; i < srt.length; i++) {
-  console.log(srt[i])
+const str = 'styuiop'
+
+if (str[0] === 's' || str[0] === 'S') {
+  const string = str[0].toUpperCase() + str.slice(1)
+  if (string.length <= 7) {
+    for (let i = 0; i < string.length; i++) {
+      console.log(string[i])
+    }
+  } else {
+    for (let i = 0; i <= 7; i++) {
+      console.log(string[i])
+    }
+  }
+}
+
+if (!(str[0] === 's' || str[0] === 'S')) {
+  if (str.length <= 7) {
+    for (let i = 0; i < str.length; i++) {
+      console.log(str[i])
+    }
+  } else {
+    for (let i = 0; i <= 7; i++) {
+      console.log(str[i])
+    }
+  }
 }
