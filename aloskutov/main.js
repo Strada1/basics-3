8b6c8f045d6e19526e0ab75e915fc8634720a7d3
andreu03 <andrey.loskutov.2003@mail.ru>
2023-02-07 16:58:28 +0300

    Улучшил задание

--- aloskutov/main.js
+++ aloskutov/main.js
@@ -1,26 +1,10 @@
-function showVerticalMessage(word) {
-    if (word.at(0) === 's') {
-        let newWord = word[0].toUpperCase() + word.slice(1);
-        if (word.length > 7) {
-            newWord = newWord.substring(0,7);
-            for (key in newWord) {
-                console.log(newWord[key]);
-            }
-        } else {
-            for (key in newWord) {
-                console.log(newWord[key]);
-            }
-        }
-    } else if (word.length < 7){
-        for (key in word) {
-            console.log(word[key]);
-        }
+function showVerticalMessage(text) {
+    text = text.substring(0,6);
+    if (text[0] === 's') {
+        text = text[0].toUpperCase() + text.slice(1);
     }
-    if (word.length > 7 && word.at(0) !== 's') {
-        let olWord = word.substring(0,7)
-        for (key in olWord) {
-            console.log(olWord[key]);
-        }
+    for (key in text) {
+        console.log(text[key]);
     }
 }
-showVerticalMessage('Strada');
+showVerticalMessage('stradastart');
