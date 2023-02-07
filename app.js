d1545b22483abea5b818648c8f11561761f0382f
legendes123 <global.lolo@yandex.ru>
2023-02-07 21:39:34 +0300

    commit string

--- app.js
+++ app.js
@@ -1,75 +1,24 @@
-const list = {
-	"create a new practice task": "In Progress", 
-	"make a bed": "Done", 
-    "write a post": "To Do",
-}
 
-function changeStatus(val, status){
-    list[val] = status
-
-}
-
-function deleteTask(val){
-    delete list[val]
-}
-
-function addTask (val){
-    list[val] = "In Progress"
-}
-
-function showList() {
-    const todo = {}
-    const progress = {}
-    const done = {}
-
-    for (const status in list) {
+function showVerticalMessage(str){
+    let maxValueString = 7;
+    if(typeof str === "string"){
+        for(let i = 0; i < maxValueString - 1; i++){
         
-        if (list[status] === 'To Do') {
-            todo[status] = 'To Do'
-        }
-        if (list[status] === 'In Progress') {
-            progress[status] = 'In Progress'
-
-        }
-        if (list[status] === 'Done') {
-            done[status] = 'Done'
-
-        }
+            if(str[i] === str[0]){
+                console.log(str[i].toUpperCase())
     
+            }
+            else console.log(str[i])
+    
+    
+    
+    
+        } 
     }
-
-
-    console.log("Todo:")
-    if(Object.entries(todo).length === 0){
-        console.log("-")
-    }
-    for (const status in todo) {
-        console.log(status)
-    }
-
-    console.log("In Progress:")
-    if(Object.entries(progress).length === 0){
-        console.log("-")
-    }
-    for (const status in progress) {
-        console.log(status)
-    }
-
-    console.log("Done:")
-    if(Object.entries(done).length === 0){
-        console.log("-")
-    }
-    for (const status in done) {
-        console.log(status)
-    }
-
-
-
+    else console.log("Wrong type")
 }
 
+showVerticalMessage('strada')
+showVerticalMessage('stradaaaaaaaa')
 
 
-changeStatus("write a post", "Done")
-addTask('have a walk');
-deleteTask('have a walk');
-showList();
