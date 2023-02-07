d33e18f868fd6a87665a861aa04a33ae252a7601
Alex <itrobotom@gmail.com>
2023-02-07 17:50:41 +0700

    Прописал создание новой строки с символами в столбик и вывод ее целиком

--- 23less_string/main.js
+++ 23less_string/main.js
@@ -2,16 +2,20 @@
 //Если строка начинается с буквы s - нужно вывести эту строку с первой заглавной буквой
 //Если строка больше 7 символов - вывести только первые 7
 function showVerticalMessage(str){
+    let newStr = '';
     if (str[0] === 's'){ //изменить проверку на букву s
-        console.log(str[0].toUpperCase());
+        newStr = `${str[0].toUpperCase()}\n`;
+        //console.log(str[0].toUpperCase());
         for (let i = 1; i < 7; i++) {
-            console.log(str[i]);
+            //console.log(str[i]);
+            newStr = newStr + `${str[i]}\n`;
         }
     }  else {
         for (let i = 0; i < 7; i++) {
-            console.log(str[i]);
+            newStr = newStr + `${str[i]}\n`;
         }
     }
+    console.log(newStr);
 }
 
 showVerticalMessage("stradagjjh");
