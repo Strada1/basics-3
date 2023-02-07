b457e54a8b6dd73fdf135c5069108e9ebb8c2807
Alex <itrobotom@gmail.com>
2023-02-07 17:55:45 +0700

    Удалил лишнее. Прописал максимальную длину строки константой

--- 23less_string/main.js
+++ 23less_string/main.js
@@ -1,18 +1,17 @@
 //Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль.
 //Если строка начинается с буквы s - нужно вывести эту строку с первой заглавной буквой
 //Если строка больше 7 символов - вывести только первые 7
+const maxLengthStr = 7;
 function showVerticalMessage(str){
     let newStr = '';
     if (str[0] === 's'){ //изменить проверку на букву s
         newStr = `${str[0].toUpperCase()}\n`;
-        //console.log(str[0].toUpperCase());
-        for (let i = 1; i < 7; i++) {
-            //console.log(str[i]);
-            newStr = newStr + `${str[i]}\n`;
+        for (let i = 1; i < maxLengthStr; i++) {
+            newStr += `${str[i]}\n`;
         }
     }  else {
-        for (let i = 0; i < 7; i++) {
-            newStr = newStr + `${str[i]}\n`;
+        for (let i = 0; i < maxLengthStr; i++) {
+            newStr += `${str[i]}\n`;
         }
     }
     console.log(newStr);
