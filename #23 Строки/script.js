a4e6b624d9935d37939d29402710d3421c730407
andrey ryabov <araibov@mail.ru>
2023-02-08 14:06:33 +0500

    сделал второй вариант функции

--- #23 Строки/script.js
+++ #23 Строки/script.js
@@ -1,11 +1,25 @@
+// function showVerticalMessage(str, amount) {
+//     if (str[0] === 's') {
+//         str = str[0].toUpperCase() + str.slice(1);
+//     }
+//     for (let i = 0; i < amount; i++) {
+//         if (i >= str.length) break
+//         console.log(str[i]);
+//     }
+// }
+
+
 function showVerticalMessage(str, amount) {
     if (str[0] === 's') {
-        str = str[0].toUpperCase() + str.slice(1);
+        str = str[0].toUpperCase() + str.slice(1, amount);
+    } else {
+        str = str.slice(0, amount)
     }
-    for (let i = 0; i < amount; i++) {
-        if (i >= str.length) break
+    for (i in str) {
         console.log(str[i]);
     }
 }
 
-showVerticalMessage('strada', 15)
+
+
+showVerticalMessage('strada', 4)
