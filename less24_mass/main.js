984018550b01deb9686ec4df38455575d34bc083
Alex <itrobotom@gmail.com>
2023-02-08 20:18:52 +0700

    Добавлены функции по добавлению и удалению задач в массив. Протестирован метод Push

--- less24_mass/main.js
+++ less24_mass/main.js
@@ -3,8 +3,34 @@
 //Использовать методы массива чтобы добавлять и удалять задач
 //Используйте методы массива чтобы вывести все задачи
 
-let todoArr = ["create a new practice task", "make a bed", "write a post"];
-
-for (const task of todoArr){
-    console.log(task);
+let todoArr = [];
+const numTask = 1; //количество удаляемых задач
+//добавить новую задачу через функцию
+function addTask(task, arr, position){
+    arr.splice(+position, 0, task); //0 - не удаляем никакую задачу
 }
+//удалить задачу, принимаем пока, что task точно есть в массиве
+function delTask(arr, taskDel){
+    let countTask = 0;
+    for (const task of arr){
+        if(task === taskDel){
+            arr.splice(countTask, numTask);
+            return;
+        }
+        countTask++;
+    }
+}
+
+function showTodoArr(){
+    for (const task of todoArr){
+        console.log(task);
+    }
+}
+showTodoArr();
+todoArr.push("create a new practice task");
+todoArr.push("make a bed");
+todoArr.push("write a post");
+showTodoArr();
+addTask("take a shower", todoArr, 2);
+delTask(todoArr, "make a bed");
+showTodoArr();
