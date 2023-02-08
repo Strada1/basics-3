d0e2f006304010f8e73714f684817fcda1c462b1
Kvazarus <kvazarus13@yandex.ru>
2023-02-08 20:51:42 +0300

    #24 массивы

--- Kvazarus/main.js
+++ Kvazarus/main.js
@@ -1,15 +1,24 @@
-function showVerticalMessage(str) {
-    if (str.slice(0, 1) === 's' || str[0] === 's' || str.at(0) === 's' || str.startsWith('s')) {
-        str = 'S' + str.slice(1);
-    }
+list = ['задача 1', 'задача 2', 'задача 3', 'задача 4', 'задача 5'];
+lastElem = list.pop();
+console.log(list, lastElem);
+list.push(lastElem);
 
-    if (str.length > 7) { 
-        str = str.slice(0, 7) 
-    }
+firstElem = list.shift();
+console.log(list, firstElem);
+list.unshift(firstElem)
 
-    for (let char of str) {
-        console.log(char);
+console.log(list);
+deletedElements = list.splice(4, 10);
+console.log(list, deletedElements);
+list.splice(2, 0, deletedElements)
+console.log(list);
+
+for (const task of list) {
+    if (typeof(task) === 'object') {
+        for (const task1 of task) {
+            console.log(task1);
+        }
+    } else {
+        console.log(task);
     }
 }
-
-showVerticalMessage("strada111");
