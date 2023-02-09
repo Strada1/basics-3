643711b76695a9f4d8547cfda9df8e33b10d66da
kihrynko <ki.hrynko@gmail.com>
2023-02-09 08:25:04 +0200

    added task three

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict';
 
-// Task one
+const numArray = [1, 11, -2, 3, -10, 4];
 const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+
+// Task one
 const uniqueAnimal = animals.find((animal) => animal.length > 7);
 console.log(uniqueAnimal); // elephant
 
 // Task two
-const numArray = [1, 11, -2, 3, -10, 4];
 const negativeNumArray = numArray.filter((num) => num < 0);
 console.log(negativeNumArray); // [ -2, -10 ]
+
+// Task three
+const absoluteNumArray = numArray.map((num) => Math.abs(num));
+console.log(absoluteNumArray); // [ 1, 11, 2, 3, 10, 4 ]
