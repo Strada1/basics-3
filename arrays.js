f5093364e3cf7f5590e660dd980a8b4206c2ebff
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-09 21:49:07 +0300

    For each method

--- arrays.js
+++ arrays.js
@@ -1,25 +1,5 @@
-const toDoList = ['Найти', 'Заказать', 'Оплатить', 'Купить']
+const toDoList = [1, 2, 3, 4]
 
-// const taskCount = toDoList.length
-
-// toDoList[1] = 'Заменить'
-// const taskFirst = toDoList[1]
-// const taskLast = toDoList[toDoList.length - 1]
-// const shortList = toDoList.slice(0,2)
-// const middleList = toDoList.splice(1,4)
-
-// toDoList.push('Забрать заказ')
-// toDoList[1] = toDoList.pop()
-// toDoList.unshift('Выбрать товар')
-
-// console.log(taskCount)
-// console.log(taskFirst)
-// console.log(taskLast)
-// console.log(shortList)
-// console.log(middleList)
-
-// console.log(toDoList)
-
-for(const task of toDoList) {
-    console.log(task)
-}
+toDoList.forEach(task =>
+    console.log(`Number is ${task}`)
+)
