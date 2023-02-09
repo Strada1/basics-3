3031483bd9edf58b1c8138fc84a4cb2e708ebd28
AngelinaRozhkova <angerozhkova@gmail.com>
2023-02-09 11:53:08 +0300

    method find

--- 24.js
+++ 24.js
@@ -1,5 +1,3 @@
-let arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-arrayNum.forEach( number => {
-  console.log(`Number is ${number}`);
-});
+let animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+const firstWord = animals.find(animal => animal.length > 6);
+console.log(firstWord);
