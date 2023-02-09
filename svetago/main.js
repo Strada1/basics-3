cfeda6de4aad08f3888ded91a7f7d11a2f7b1d4e
SvetaGo <golimbovskaya.svetlana@gmail.com>
2023-02-09 17:12:24 +0300

    урок 25, задание 5

--- svetago/main.js
+++ svetago/main.js
@@ -1,3 +1,3 @@
 const numbers = [1, 11, -2, 3, -10, 4];
-const absoluteValuesNumbers = numbers.map(number => Math.abs(number));
-console.log(absoluteValuesNumbers);
+const sortedNumbers = numbers.sort ((a,b) => b-a);
+console.log(sortedNumbers);
