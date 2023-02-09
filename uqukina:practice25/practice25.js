bfb921aab7ab67c2a01d71d73f92cd15b8ec01eb
katzuto <vstepovoi@gmail.com>
2023-02-09 13:44:22 +0300

    arrays

---
+++ uqukina:practice25/practice25.js
@@ -0,0 +1,27 @@
+// find() more than 7 lenght in array
+// const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+//
+// const MoreThanSeven = animals.find(animal => animal.length > 6);
+// console.log(MoreThanSeven)
+
+
+// only negative numbers new array filter()
+// const numbers = [1, 11, -2, 3, -10, 4]
+//
+// const onlyNegativeNumbers = numbers.filter(number => number < 0);
+// console.log(onlyNegativeNumbers)
+
+
+// new array with positive numbers filter()
+// const numbers = [1, 11, -2, 3, -10, 4]
+//
+// const positiveNumbers = numbers.filter(number => number > 0);
+// console.log(positiveNumbers)
+
+// array sort()
+// const numbers = [1, 11, -2, 3, -10, 4];
+//
+// const newArr = numbers.sort((a, b) => b - a);
+// console.log(newArr);
+
+
