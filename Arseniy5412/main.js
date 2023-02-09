231b1416de8c8addb91e4a2fe716e257b738a12a
Arseniy5412 <arseniyrazhin5412@gmail.com>
2023-02-09 16:43:15 +0300

    string

--- Arseniy5412/main.js
+++ Arseniy5412/main.js
@@ -1,39 +1,13 @@
-
-const toDo = {
-	list: {
-		"create a new practice task": "In Progress", 
-		"make a bed": "Done", 
-		"write a post": "To Do"
-	},
-
-	changeStatus(task, status) {
-		this.list[task] = status;
-	},
-
-	addTask(task, status) {
-		this.list[task] = status;
-	},
-
-	deleteTask(task) {
-		delete this.list[task];
-	},
-
-	showList () {
-		for (let key in this.list ) {
-			if (this.list[key] === "In Progress") {
-				console.log(key + ' : ' + this.list[key]);
-			} else if (this.list[key] === "Done") {
-				console.log(key + ' : ' + this.list[key]);
-			} else if (this.list[key] === "To Do") {
-				console.log(key + ' : ' + this.list[key]);
-			} else {
-				console.log('Nothing is done');
-			}
+function showVerticalMessage(word) {
+	if (word[0] == 's') {
+		let str = word[0].toUpperCase() + word.slice(1, 6);
+		for (let key of str) {
+				console.log(key);
+		}
+	} else {
+		for (let key of word.slice(0, 6)) {
+			console.log(key);
 		}
 	}
-};	
-
-toDo.changeStatus("write a post", "gfgfdfdf") // меняет статус задачи
-toDo.addTask('payng compucter', 'Done') // добавляет новую задачу
-//toDo.deleteTask('have a walk'); // удаляет задачу
-toDo.showList(); // показывает список всех задач
+}
+showVerticalMessage('stradamylife');
