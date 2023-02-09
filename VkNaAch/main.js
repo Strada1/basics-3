54186e10081cbd44bf561f9184422dc4caab10ba
VkNaAch <eb.birds@mail.ru>
2023-02-09 22:23:39 +0600

    ToDo Ok

--- VkNaAch/main.js
+++ VkNaAch/main.js
@@ -1,42 +1,4 @@
 
-//задача 1
-
-function checkAge(age){
-   if(age<18||age<=18)
-   {
-      console.log('you are not allowed')
-   }
-   else
-   {
-      console.log('you are welcome!')
-   }
-
- }
- checkAge(17)
- checkAge(19)
-
-//задача 2
-
-function calc(operation, a, b) {
-   if (operation === 'add'){
-     return (a + b);
-   } else if (operation === 'multi') {
-     return (a * b);
-   } else if (operation === 'subtract') {
-     return (a - b);
-   } 
- }
- 
- 
-resultAdd = calc('add', 5, 2);
- console.log(resultAdd);
- 
-resultMulti = calc('multi', 3, 2);
- console.log(resultMulti);
- 
-resultSubtract = calc('subtract', 3, 4);
- console.log(resultSubtract);
- 
  
  
  
