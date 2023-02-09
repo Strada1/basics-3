7746ef550fec5c493f13948ac2ceb681532848e1
Elena-Kr <korostellen@gmail.com>
2023-02-10 00:24:34 +0500

    #25 Методы массивов

--- elenakorstlv/m.js
+++ elenakorstlv/m.js
@@ -1,4 +1,4 @@
-const listToDo = {
+/*const listToDo = {
     list: {
     'Поставить брекеты': 'Done',
     'Оплатить Strada' : 'To do',
@@ -45,4 +45,37 @@ listToDo.showList();
 //удаление
 listToDo.deleteTask('Задача для удаления');
 
-listToDo.showList();
+listToDo.showList();*/
+
+/*const listToDo = ['Поставить брекеты', 'Оплатить Strada','Пройти Strada','Найти работу Frontend-разработчиком','Переехать из РФ','Задача для удаления']
+listToDo.push('полить цветы');
+console.log(listToDo);
+
+const lastItem = listToDo.pop();
+console.log(listToDo);
+console.log(lastItem);
+
+for (const task of listToDo){
+    console.log(task);
+}*/
+
+const numbers =[1,2,3,4,5,6,7];
+numbers.forEach(number => {
+    console.log(`Number is ${number}`);
+});
+
+const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+const moreThanSeven = animals.find(animal => animal.length > 7);
+console.log(moreThanSeven);
+
+const newNumbers = [1, 11, -2, 3, -10, 4];
+const negativeNumbers = newNumbers.filter( number => number<0);
+console.log(negativeNumbers);
+
+const againNumbers = [1, 11, -2, 3, -10, 4];
+const absolutNumbers = againNumbers.map(number => Math.abs(number));
+console.log(absolutNumbers);
+
+const numbersForSort = [1, 11, -2, 3, -10, 4];
+const sortedNumbers = numbersForSort.sort((a,b) => a-b);
+console.log(sortedNumbers);
