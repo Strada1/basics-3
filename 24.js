9590b375e45209ec065dd9d2fe332b0108d8ae5b
AngelinaRozhkova <angerozhkova@gmail.com>
2023-02-09 12:21:03 +0300

    method sort

--- 24.js
+++ 24.js
@@ -1,3 +1,3 @@
 let num = [1, 11, -2, 3, -10, 4];
-const absolutNum = num.map(number => Math.abs(number));
-console.log(absolutNum);
+const sortedNum = num.sort((a,b) => b - a);
+console.log(sortedNum );
