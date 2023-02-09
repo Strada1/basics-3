6c0d249edffc752b3f218ba9236e2d74f93121fc
Yana <goyana72@gmail.com>
2023-02-09 14:00:47 +0300

    forEach method

--- yanika/main.js
+++ yanika/main.js
@@ -1,11 +1,10 @@
-"use strict"
-//Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль
-function showVerticalMessage(anyWord) {
-    //Если строка начинается с буквы s - нужно вывести эту строку с первой заглавной буквой
-    //Если строка больше 7 символов - вывести только первые 7
-    console.log(anyWord.charAt(0).toUpperCase())
-    for (let lol of anyWord.slice(1, 7)) {
-        console.log(lol)
-    }
-}
-showVerticalMessage("st rada");
+const numbers = [
+        1,
+        2,
+        3,
+        4
+    ];
+    numbers.forEach(numbers => {
+        console.log(`Number is ${numbers}`);
+    }    
+    );
