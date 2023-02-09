d87be2b4340e46aba96d32271c769aae9645c043
kihrynko <ki.hrynko@gmail.com>
2023-02-09 08:19:13 +0200

    added task two

--- app.js
+++ app.js
@@ -4,3 +4,8 @@
 const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
 const uniqueAnimal = animals.find((animal) => animal.length > 7);
 console.log(uniqueAnimal); // elephant
+
+// Task two
+const numArray = [1, 11, -2, 3, -10, 4];
+const negativeNumArray = numArray.filter((num) => num < 0);
+console.log(negativeNumArray); // [ -2, -10 ]
