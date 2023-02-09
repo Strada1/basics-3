bd2a34cc653d6c94fb8550cb8f564b282f3b97f8
Yana <goyana72@gmail.com>
2023-02-09 14:14:43 +0300

    map method

--- yanika/main.js
+++ yanika/main.js
@@ -1,3 +1,7 @@
 const numbers = [1, 11, -2, 3, -10, 4];
 const negativeNumbers = numbers.filter(numbers => numbers < 0);
 console.log(negativeNumbers);
+
+const absolutNum = numbers.map(uppNumber => Math.abs(uppNumber));
+console.log(absolutNum);
+//
