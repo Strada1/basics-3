aae214a0cd2ebe5b04c3736ae5b418f60d245263
Elena-Kr <korostellen@gmail.com>
2023-02-09 23:11:48 +0500

    practice-23

--- elenakorstlv/m.js
+++ elenakorstlv/m.js
@@ -1,48 +1,26 @@
-const listToDo = {
-    list: {
-    'Поставить брекеты': 'Done',
-    'Оплатить Strada' : 'To do',
-    'Пройти Strada' : 'In progress',
-    'Найти работу Frontend-разработчиком' : 'To do',
-    'Переехать из РФ' : 'To do',
-    'Задача для удаления': 'To do'
-    },
-    // меняет статус задачи
-        changeStatus(task, status) {
-        this.list[task] = status
-        },
-
-    // добавляет новую задачу
-        addTask(task, status) {
-        this.list[task]= status
-        },
-
-    // удаляет задачу
-        deleteTask(task, status) {
-            delete this.list[task]; 
-        },
-
-    // показывает список всех задач
-        showList(){
-        console.log(this.list)
-        },
+function showVerticalMessage1(message){
+    if (message.length<2||message.length>20){
+        console.log('Слово должно быть от 2 до 20 символов!');
+    } else if(message.length<8&&message.startsWith("s")){
+        console.log( message[0].toUpperCase() );
+        for (let char of message.slice(1)) {
+            console.log(char); 
+        };
+    }else if(message.length<7){
+        for (let char of message) {
+            console.log(char); 
+        };
+    }else if(message.length>7&&message.startsWith("s")){
+        console.log( message[0].toUpperCase() );
+        for (let char of message.slice(1,7)) {
+            console.log(char); 
+        };
+    }else if(message.length>7){
+        for (let char of message.slice(0,7)) {
+            console.log(char); 
+        }
+    }else{
+        console.log('error, please try again another type')
+    };       
 };
-
-
-//чтение
-listToDo.showList();
-
-//добавление
-listToDo.addTask('Выиграть суд в ЕСПЧ', 'In progress');
-
-listToDo.showList();
-
-//изменение 
-listToDo.changeStatus('Оплатить Strada', 'Done');
-
-listToDo.showList();
-
-//удаление
-listToDo.deleteTask('Задача для удаления');
-
-listToDo.showList();
+showVerticalMessage1('stradalec');
