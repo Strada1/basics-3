8153f338b166b86464a74d442bc910e1510dd153
Maxim <maximguwop@icloud.com>
2023-02-09 17:39:09 +0300

    array__new

--- max_liashevich/JS/array.js
+++ max_liashevich/JS/array.js
@@ -1,76 +1,76 @@
-// let cookers = ['Alisher', 'Muslim', 'Andrey'];
+let cookers = ['Alisher', 'Muslim', 'Andrey'];
 
-// cookers[0] = 'Pushkin';
-// cookers[1] = 'Uebok';
-// cookers[3] = 'Petushara';
+cookers[0] = 'Pushkin';
+cookers[1] = 'Uebok';
+cookers[3] = 'Petushara';
 
-// console.log(cookers.length);
-// console.log(cookers);
+console.log(cookers.length);
+console.log(cookers);
 
-// let arr = ['Petushara', 
-//             'Igorek',
-//             function petuh() {console.log('oreshkin-suka')},
-//           ];
+let arr = ['Petushara', 
+            'Igorek',
+            function petuh() {console.log('oreshkin-suka')},
+          ];
 
-// arr.pop();
-// arr.push('Putin', 'Shoigu');
-// console.log(arr);
+arr.pop();
+arr.push('Putin', 'Shoigu');
+console.log(arr);
+arr.shift();
+arr.unshift('Ssiklo', 'Net');
 // arr.shift();
-// arr.unshift('Ssiklo', 'Net');
-// // arr.shift();
-// console.log(arr);
+console.log(arr);
 
 
 
-// // ------------------------------------
-// // ------------------------------------
-// // ------------------------------------
-// let fruits = ["Яблоки", "Груша", "Апельсин"];
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+let fruits = ["Яблоки", "Груша", "Апельсин"];
 
-// // добавляем новое значение в "копию"
-// let shoppingCart = fruits;
-// shoppingCart.push("Банан");
+// добавляем новое значение в "копию"
+let shoppingCart = fruits;
+shoppingCart.push("Банан");
 
-// // что в fruits?
-// console.log( fruits.length ); // ?
-// console.log(fruits);
-// // ------------------------------------
-// // ------------------------------------
-// // ------------------------------------
-// let styles = ["Jazz", "Blues"];
+// что в fruits?
+console.log( fruits.length ); // ?
+console.log(fruits);
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
+let styles = ["Jazz", "Blues"];
 
-// styles.push('Rock-n-roll');
-// console.log(styles.length);
-// console.log(styles[Math.floor((styles.length - 1) / 2)]);
-// styles[Math.floor((styles.length - 1) / 2)] = "Classic";
-// styles.shift();
-// styles.unshift('Rap', 'Raeggi')
-// console.log(styles);
+styles.push('Rock-n-roll');
+console.log(styles.length);
+console.log(styles[Math.floor((styles.length - 1) / 2)]);
+styles[Math.floor((styles.length - 1) / 2)] = "Classic";
+styles.shift();
+styles.unshift('Rap', 'Raeggi')
+console.log(styles);
 
-// // ------------------------------------
-// // ------------------------------------
-// // ------------------------------------
+// ------------------------------------
+// ------------------------------------
+// ------------------------------------
 
-// function sumInput() {
-//   const arr = [];
+function sumInput() {
+  const arr = [];
 
-//   while (true) {
-//     const number = prompt('введите число: ', '');
+  while (true) {
+    const number = prompt('введите число: ', '');
     
-//     if (number === null || number === '' || isNaN(number)) break;
+    if (number === null || number === '' || isNaN(number)) break;
     
-//     arr.push(Number(number));
-//   }
+    arr.push(Number(number));
+  }
   
-//   let sum = 0;
-//   for (const item of arr) {
-//     sum += item;
-//   }
+  let sum = 0;
+  for (const item of arr) {
+    sum += item;
+  }
   
-//   return sum;
-// }
+  return sum;
+}
 
-// console.log(sumInput());
+console.log(sumInput());
 
 
 // ------------------------------------
