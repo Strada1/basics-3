fd0640488fd18bd81c7e2c61f98e0d6deec69cc5
Evgeniy B <beliy-j@mail.ru>
2023-02-09 21:02:20 +0300

    task-25

--- lesson 25/script.js
+++ lesson 25/script.js
@@ -1,21 +1,3 @@
-// let tasks = ['learn Java Script', 'learn Java Script better', 'learn React', 'find work']
-// tasks.pop()
-// console.log(tasks);
-// tasks.push('find work')
-// console.log(tasks);
-// tasks.shift()
-// console.log(tasks);
-// tasks.unshift('learn Java Script')
-// console.log(tasks);
-// let newTasks1 = tasks.slice(1,3)
-// console.log(tasks);
-// console.log(newTasks1);
-// let newTasks2 = tasks.splice(0, 1)
-// console.log(tasks);
-// console.log(newTasks2);
-// tasks.splice(0, 0, 'learn Java Script')
-// console.log(tasks);
-
 const numbers1 = [6, 100, 45, 5, 56, 94, 72, 8, 123]
 
 numbers1.forEach(number => {
