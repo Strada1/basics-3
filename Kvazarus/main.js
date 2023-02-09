5f228987b6e8cdfc23f8726d32305eb66ccf74f5
Kvazarus <kvazarus13@yandex.ru>
2023-02-09 22:22:14 +0300

    #25 Методы массивов

--- Kvazarus/main.js
+++ Kvazarus/main.js
@@ -1,24 +1,26 @@
-list = ['задача 1', 'задача 2', 'задача 3', 'задача 4', 'задача 5'];
-lastElem = list.pop();
-console.log(list, lastElem);
-list.push(lastElem);
+const mas = [1,2,3,4,5];
+mas.forEach(number => {
+    console.log(`Number is ${number}`);
+})
 
-firstElem = list.shift();
-console.log(list, firstElem);
-list.unshift(firstElem)
-
-console.log(list);
-deletedElements = list.splice(4, 10);
-console.log(list, deletedElements);
-list.splice(2, 0, deletedElements)
-console.log(list);
-
-for (const task of list) {
-    if (typeof(task) === 'object') {
-        for (const task1 of task) {
-            console.log(task1);
-        }
-    } else {
-        console.log(task);
+const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+animals.forEach(animal => {
+    if (animal.length > 7) {
+        console.log(animal);
+        return;
     }
-}
+})
+
+const numbers = [1, 11, -2, 3, -10, 4];
+negativeNumbers = numbers.filter(number => number < 0);
+console.log(negativeNumbers);
+
+absoluteNumbers = numbers.map(number => Math.abs(number));
+console.log(absoluteNumbers);
+
+const sortedNumbers = numbers.sort((a, b) => b - a)
+console.log(sortedNumbers); // [-10, -2, 1, 3, 4, 11]
+console.log(numbers);
+
+const ab = numbers.reverse(sortedNumbers)
+console.log(ab, numbers);
