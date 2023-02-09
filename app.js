06f6a147ff1a3394347cff40ad40799f7e4f3631
kihrynko <ki.hrynko@gmail.com>
2023-02-09 08:16:45 +0200

    added task one

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Task one
+const animals = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+const uniqueAnimal = animals.find((animal) => animal.length > 7);
+console.log(uniqueAnimal); // elephant
