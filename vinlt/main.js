57931345c8c3e5635fc7774c3066b910e4e28f35
кучанский cергей <vkuchanskyi@gmail.com>
2023-02-10 17:18:40 +0300

    #26 TODO на массивах

--- vinlt/main.js
+++ vinlt/main.js
@@ -1,40 +1,32 @@
-const ToDo = {
-  list: {
-      "create a new practice task": "In Progress",
-      "make a bed": "Done",
-      "write a post": "ToDo",
-  },
-  changeStatus: function(key, status) {
-    if (key in ToDo.list) {
-      this.list[key] = status
-    } else {
-      console.log('No1')
-    }
-  },
-  addTask: function (key, status) {
-    if (!(key in ToDo.list)) {
-      this.list[key] = status
-    } else  {
-      console.log("No2")
-    }
-  },
-  deleteTask: function (key) {
-    if (key in this.list) {
-      delete this.list[key] 
-    } else {
-      console.log("No3")
-    }
-  },
-  showList: function () {
-    for (const key in ToDo.list) {
-      console.log(key, this.list[key])
-    }
-  }
+const list = [ 
+	{name: 'create a post', status: 'In progress', priority: 'low'}, 
+    {name: 'test', status: 'Done', priority: 'high'} 
+];
+list.push({name: 'ToDo', status: 'Done', priority: 'high'})
+list.shift()
+list[0] = {name: 'study Strada', status: 'In progress', priority: 'low'}
+const lala = list.find(lala => lala.name === 'study Strada')
+function watch () {
+	console.log('Done')
+	if(list.find(fin => fin.status === 'Done')) {
+		console.log('	name: create a poss, status: In progress, priority: low')
+	}
+	console.log('In progress')
+	if(list.find(str => str.status === 'In progress')) {
+		console.log('	name: create a post, status: In progress, priority: low')
+	}
+	console.log('ToDo') 
+	console.log('	-')
 }
-ToDo.changeStatus("create a new practice task", "ToDo")
-ToDo.addTask("read book", "already")
-ToDo.deleteTask("write a post")
-ToDo.showList()
+watch()
+
+
+
+
+
+
+
+
 
 
 
