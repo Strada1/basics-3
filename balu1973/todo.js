d8e4cf32bcaba11a979b47a4519a31b16638fff7
Alex M <balurus37@gmail.com>
2023-02-10 19:44:22 +0300

    version 2

--- balu1973/todo.js
+++ balu1973/todo.js
@@ -22,10 +22,10 @@ const todoList = {
     taskName = taskName.trim();
 
     if (!this.findTask(taskName)) {
-      this.list.push({ 
+      this.list.push({
         name: taskName,
         status: STATUSES.TODO,
-        priority
+        priority: priority,
       });
     } else {
       console.log("Задача не добавлена. Такая задача уже существует");
@@ -41,7 +41,7 @@ const todoList = {
     }
 
     taskName = taskName.trim();
-    const task = this.findTask(taskName)
+    const task = this.findTask(taskName);
 
     if (task) {
       task.priority = priority;
@@ -57,7 +57,7 @@ const todoList = {
       );
       return;
     }
-    
+
     taskName = taskName.trim();
     const task = this.findTask(taskName);
 
@@ -74,34 +74,32 @@ const todoList = {
 
     if (taskIndex !== -1) {
       this.list.splice(taskIndex, 1);
-    } 
+    }
   },
 
   getTasksByStatus(status) {
-    const allTasksByStatus = this.list.filter(task => task.status === status);
-    let resultTasks = "";
-
-    for (const task of allTasksByStatus) {
-      resultTasks += `    ${task.name} [priority:${task.priority}]\n`
-    }
-
-    if (0 === resultTasks.length) {
-      resultTasks += "    -";
-    }
-
-    return resultTasks.trimEnd();
+    return this.list.filter((task) => task.status === status);
   },
 
   showlist() {
     for (const key in STATUSES) {
-      const statusValue = STATUSES[key];
-      const tasks = statusValue + ":\n" + this.getTasksByStatus(statusValue);
-      console.log(tasks);
+      const status = STATUSES[key];
+      const tasksByStatus = this.getTasksByStatus(status);
+
+      console.log(status + ":");
+
+      if (tasksByStatus.length !== 0) {
+        tasksByStatus.forEach((task) =>
+          console.log(`    ${task.name} [priority: ${task.priority}]`)
+        );
+      } else {
+        console.log("    -");
+      }
     }
   },
 
   findTask(taskName) {
-    return this.list.find((task) => task.name === taskName)
+    return this.list.find((task) => task.name === taskName);
   },
 };
 
@@ -113,17 +111,16 @@ function checkStr(str) {
   }
 }
 
-// todoList.addTask("do");
-// todoList.addTask("do1");
-// todoList.addTask("do2");
-// todoList.addTask("do3");
-// todoList.addTask("do4");
-// todoList.addTask("do5");
-// todoList.changeStatus("do", STATUSES.DONE);
-// todoList.changeStatus("do1", STATUSES.IN_PROGRESS);
-// todoList.changePriority("do3", PRIORITIES.LOW);
-// todoList.changePriority("do4", PRIORITIES.HIGH);
-// todoList.changePriority("do5", PRIORITIES.HIGH);
-// todoList.deleteTask("do5");
+todoList.addTask("do");
+todoList.addTask("do1");
+todoList.addTask("do2");
+todoList.addTask("do3");
+todoList.addTask("do4");
+todoList.addTask("do5");
+todoList.changeStatus("do", STATUSES.DONE);
+todoList.changeStatus("do1", STATUSES.IN_PROGRESS);
+todoList.changePriority("do3", PRIORITIES.LOW);
+todoList.changePriority("do4", PRIORITIES.HIGH);
+todoList.changePriority("do5", PRIORITIES.HIGH);
+todoList.deleteTask("do5");
 todoList.showlist();
-
