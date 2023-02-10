6db20ac7d86dcf1a2ed157819466b9fee1048c1a
Sakenuly <sakenuly2016@yandex.ru>
2023-02-10 20:10:02 +0600

    ToDO  массивы

--- Sakenuly/javascript/main.js
+++ Sakenuly/javascript/main.js
@@ -1,85 +1,36 @@
-const list = {
-  "create a new practice task": "In progress",
-  "make cookie": "ToDo",
-  "Go to the bed": "ToDo",
-  "Make meme": "ToDo",
-  "Meme Time": "In progress",
-  "Write code": "In progress",
-  "Learn js": "ToDo",
-  }
-  
-  function changeStatus(task, status) {
-  
-  return list[task] = status, console.log('Статус задачи "' + task + '" изменен'); 
-  }
-  function addTask(task,status) {
-      if (task in list) {
-       return console.log("Уже существует"); 
-      }
-      else {
-      return list[task] = status = "ToDo", console.log('Задача добавлена')
-    }
-  }
-  function deleteTask(task) { 
-    if (task in list) {
-      return delete list[task], console.log('Задача "' + task + '" удалена');
-    }
-    else{
-      console.log('Задачи "' + task + '" нет в списке' );
-    }
-  }
-  let doneCount = 0;
-
-
-  function showDoneCount() {
-    for (rezultat in list) { 
-      
-      if (list[rezultat] == "Done") {doneCount = doneCount + 1;
-      }
-     
-    }
-    if (doneCount == 0) {
-      console.log('Ты лентяй!');
-    }
-      else if (doneCount > 0) {
-        console.log('Выполненных задач ' + doneCount);
-        }
-        
-    }
-  function showDone() {
-      console.log('Done:');
-    for (rezultat in list) {
-      if (list[rezultat] == "In progress" || list[rezultat] == "ToDo" ) continue;
-      console.log('\t' + rezultat + '  ' + list[rezultat]); //можно было убрать list[rezultat] для бонусного задания но для наглядности пусть будет)
-      
-        }
-  }
-  function showToDo() {
-    console.log('To Do:');
-    for (rezultat in list) {
-      if (list[rezultat] == "In progress" || list[rezultat] == "Done" ) continue;
-      console.log('\t' + rezultat + '  ' + list[rezultat]);
-        }
-  }
-  function showInprogress() {
-    console.log('In Progress:');
-    for (rezultat in list) {
-      if (list[rezultat] == "ToDo" || list[rezultat] == "Done" ) continue;
-      console.log('\t' + rezultat + '  ' + list[rezultat]);
-        }
-  }
-  function showMeALL() {
-    console.log('')
-    showDone();
-    console.log('')
-    showInprogress();
-    console.log('')
-    showToDo();
-    console.log('')
-    showDoneCount();
-  }
- addTask("Make a zavtrak");
- changeStatus("Make a zavtrak", "Done");
- addTask("Make mass");
- changeStatus("Make mass", "In progress");
- showMeALL();
+const list = [
+  {name: "create a new practice task", status: "In progress"},
+  {name: "make cookie", status: "ToDo"},
+  {name: "Go to the bed", status: "ToDo"},
+  {name: "Meme Time", status: "In progress"},
+  {name: "Make meme", status: "ToDo"},
+  {name: "Write code", status: "In progress"},
+  {name: "Learn js", status: "Done"}
+]
+function addTask(task, status) {
+   list.push({name: task, status: status});
+}
+function changeStatus(task, status){
+   const findForChange = list.findIndex(user => user.name === task);
+   return list[findForChange] = {name: task, status: status};
+   
+}
+function deleteTask(task) {
+   const findForDelete = list.findIndex(user => user.name === task);
+   const deleteIndex = list.splice(findForDelete, 1);
+   console.log('Вы удалили задачу');
+   console.log(deleteIndex, '\n');
+ 
+}  
+//function showTask(){
+//const sortedList = list.sort((name1, name2) => name1.status - name2.status); 
+//  console.log(sortedList); не смог сделать сортировку
+//}
+function showTask(){
+const filterToDo = console.log(list.filter(word => word.status === "ToDo"));
+const filterInprogress = console.log(list.filter(word => word.status === "In progress"));
+const filterDone = console.log(list.filter(word => word.status === "Done"));
+}
+showTask()
+ 
+ 
