17dd257c508aaca3670709c92c774d3cdb642be6
youngjanby <janby3521@gmail.com>
2023-02-10 20:06:01 +0300

    Не понимаю как убрать скобки при выводе массива

--- youngjanby/main.js
+++ youngjanby/main.js
@@ -1,6 +1,7 @@
 const list = [ 
 	{name: 'create a post', status: 'In progress', priority: 'low'}, 
-    {name: 'test', status: 'Done', priority: 'high'} 
+    {name: 'test', status: 'Done', priority: 'high'},
+    {name: 'create s ToDo List', status: 'Done', priority: 'high'}
 ];
 
 function changeStatus(name, status) {
@@ -18,13 +19,34 @@ function deleteTask(name) {
 }
 
 function showList() {
-    for(let show of list) {
-        console.log(show)
-    }
+    const filterForDone = list.filter(list => list.status === 'Done');
+    const filterForToDo = list.filter(list => list.status === 'ToDo');
+    const filterForInProgress = list.filter(list => list.status === 'In Progress')
+    
+    
+        if (filterForDone.length <= 0) {
+            console.log('Nothing is \'Done\'')
+        } else if (filterForDone.length >= 1) {
+            console.log(filterForDone)
+        };
+
+
+        if (filterForInProgress.length <= 0) {
+            console.log('Nothing is \'In Progress\'')
+        } else if (filterForInProgress.length >= 1) {
+            console.log(filterForInProgress)
+        };
+    
+
+
+        if (filterForToDo.length <= 0) {
+            console.log('\nNothing is \'ToDo\'')
+        } else if (filterForToDo.length >= 1) {
+            console.log(filterForToDo)
+        };
 }
 
-
-changeStatus('create a post', 'Done') // меняет статус задачи
+changeStatus('create a post', 'In Progress') // меняет статус задачи
 addTask('have a walk', 'Done', 'Low'); // добавляет новую задачу
 deleteTask('test'); // удаляет задачу
-showList();
+showList()
