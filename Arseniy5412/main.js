296f6c3fa312aaf99b21d5a21900e434dfbcfa38
Arseniy5412 <arseniyrazhin5412@gmail.com>
2023-02-10 16:03:26 +0300

    array metods

--- Arseniy5412/main.js
+++ Arseniy5412/main.js
@@ -1,13 +1,31 @@
-function showVerticalMessage(word) {
-	if (word[0] == 's') {
-		let str = word[0].toUpperCase() + word.slice(1, 6);
-		for (let key of str) {
-				console.log(key);
-		}
-	} else {
-		for (let key of word.slice(0, 6)) {
-			console.log(key);
-		}
-	}
+let arr = [1, 2, 3, 4, 5];
+
+arr.forEach(number => {
+    console.log(`Number is ${number}`)
+});
+
+
+let arr2 = ['cat', 'dog', 'elephant', 'tiger', 'lion'];
+
+for (let i = 0; i < arr2.length; i++) {
+        if (arr2[i].length > 7) {
+            console.log(arr2[i]);
+        }
 }
-showVerticalMessage('stradamylife');
+
+
+let arr3 = [1, 11, -2, 3, -10, 4];
+
+let negativeNumber = arr3.filter(number => number < 0);
+console.log(negativeNumber);
+
+let arr4 = [1, 11, -2, 3, -10, 4];
+
+let absoluteNumber = arr4.map(number => Math.abs(number));
+
+console.log(absoluteNumber);
+
+
+const numbersForSort = [1, 11, -2, 3, -10, 4];
+const sortedNumbers = numbersForSort.sort((a,b) => a-b);
+console.log(sortedNumbers);
