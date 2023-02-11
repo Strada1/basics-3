b47483c8b8127d75842a851db04cb3cc6e248238
unknown <alexbechin333@gmail.com>
2023-02-11 15:59:35 +0300

    AlexBechin/practice-24

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ let toDoList = [];
 const newTask = function (task) {
     toDoList.unshift(task);
 }
+
 const deleteTask = function (task) {
     const index = toDoList.indexOf(task);
     if (index !== -1) {
         toDoList.splice(index, 1);
     }
 }
+
 newTask("Прогуляться");
 newTask("Приготовить еду");
 deleteTask("Заправить кровать");
+
 for (task of toDoList) {
     console.log(task);
 }
