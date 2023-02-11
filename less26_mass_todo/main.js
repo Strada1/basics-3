8f2daadecbf94df7dd5759684354db3ba982f17b
Alex <itrobotom@gmail.com>
2023-02-11 20:24:28 +0700

    Переписал функции добавления, удаления, изменения статуса на методы без forEach

--- less26_mass_todo/main.js
+++ less26_mass_todo/main.js
@@ -5,6 +5,7 @@
 //в значениях будут указаны статусы: "In Progress", "Done", "To Do"
 
 const arrStatus = ['In progress', 'Done', 'To Do'];
+const statusDefault = 'To Do';
 
 let todoArr = [ 
     {name: 'create a new practice task', status: 'In progress', priority: 'high'}, 
@@ -13,53 +14,36 @@ let todoArr = [
     {name: 'make a bed2', status: 'Done', priority: 'low'},
 ];
 
-function changeStatus(arr, task, stat) {
-    //найти задачу по name и по ключу status изменить статус
-    //если задачи такое нет, вывести сообщение об этом
-    let flagTask = true;
-    arr.forEach(element => {
-        if(element.name === task){
-            element.status = stat;
-            flagTask = false;
-        }  
-    });
-    if (flagTask){
+function changeStatus(arr, taskInTodoArr, newStat) {
+    //если задачи такой нет, вывести сообщение об этом    
+    if (arr.find(element => element.name === taskInTodoArr) === undefined){ //(!(arr.includes(arr.name) === taskInTodoArr)){
         console.log('Такой задачи не существует!');
+    } else { //иначе найти задачу по name и по ключу status изменить статус
+        arr[arr.findIndex(element => element.name == taskInTodoArr)].status = newStat;
     }
 }
 
 function addTask(arr, newTask, prior){
     //добавить задачу, если такой еще нет
     //если есть, сообщить что такая уже есть
-    let flagTask = true;
-    arr.forEach(element => {
-        if(element.name === newTask){
-            console.log('Такая зачача уже есть!')
-            flagTask = false;
-        }  
-    });
-    if (flagTask){
+    if (arr.find(element => element.name === newTask)){ //(!(arr.includes(arr.name) === taskInTodoArr)){
+        console.log('Такая зачача уже есть!');
+    } else { //иначе добавить задачу
         let newTaskObj = {};
         newTaskObj.name = newTask;
-        newTaskObj.status = 'To Do'; //по умолчанию To Do
+        newTaskObj.status = statusDefault; //по умолчанию To Do
         newTaskObj.priority = prior;
         arr.push(newTaskObj);
     }
 }
 
 function deleteTask(arr, delTask){
-    //удалить задачу, если такая есть
-    //в противном случае уведомить о том, что такой задачи нет
-    let flagTask = true;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i].name === delTask){
-            let delTaskConsole = arr.splice(i, 1);
-            flagTask = false;
-            console.log(`Задача "${delTaskConsole[0].name}" успешно удалена`);
-        }
-    }
-    if (flagTask){
+    //проверить, есть ли задача для удаления
+    if (!(arr.find(element => element.name === delTask))){
         console.log('Такой задачи не существует!');
+    } else { // удалить задачу
+        let delTaskConsole = arr.splice(arr.findIndex(element => element.name == delTask), 1);
+        console.log(`Задача "${delTaskConsole[0].name}" успешно удалена`);
     }
 }
    
@@ -74,7 +58,7 @@ function showTodo(arr) {
             const arrDone = arr.filter(element => element.status === stat);
             //console.log(arrDone); 
             if (arrDone.length === 0) {
-                console.log(`${stat}:`)
+                console.log(`${stat}:`) //вместо console.log('    -'); можно добавить в кавычки:  \n    -
                 console.log('    -'); 
             } else {
                 console.log(`${stat}:`)
@@ -88,7 +72,7 @@ function showTodo(arr) {
 
 showTodo(todoArr);
 changeStatus(todoArr, 'create a new practice task', 'Done');
-addTask(todoArr, 'take a shower', 'high')
+addTask(todoArr, 'take a shower', 'high'); //'take a shower'
 showTodo(todoArr);
 showTodo(todoArr);
 deleteTask(todoArr, 'create a new practice task');
