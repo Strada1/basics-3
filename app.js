6b6107711fe9fd14d5da2a90b9c8bb915acdd3be
legendes123 <global.lolo@yandex.ru>
2023-02-11 13:35:09 +0300

    todo array

--- app.js
+++ app.js
@@ -1,14 +1,70 @@
-const arr = ['cat', 'dog', 'elephant', 'tiger', 'lion']
-console.log(arr.find((elem) => elem.length > 7))
+
+const STATUSES = {
+    TODO: "To Do",
+    IN_PROGRESS: "In progress",
+    DONE: "Done",
+};
+const PRIORITIES = {
+    HIGH: "High",
+    LOW: "Low",
+};
+
+const list = [
+    {name: 'create a post', status: 'In progress', priority: 'low'},
+    {name: 'test', status: 'To Do', priority: 'high'},
+    {name: 'make task', status: 'To Do', priority: 'low'},
+    {name: 'create todo', status: 'In progress', priority: 'high'},
+
+];
 
 
-const number = [1, 11, -2, 3, -10, 4]
-const negativeNumber = number.filter((elem) => elem < 0)
-console.log(negativeNumber)
-const absoluteNumber =  number.map((elem) =>Math.abs(elem))
-console.log(absoluteNumber)
+function changeStatus(statusName,statusValue){
+    const index = list.findIndex((elem) => elem.name = statusName);
+    list[index].status = statusValue
+}
+function addTask(taskStatus = "To Do",taskName,taskPriorities){
 
-const sortedNumbers = number.sort((a, b) => a - b);
-console.log(sortedNumbers); 
+    list.push({
+        name:taskName,
+        status:taskStatus,
+        priority:taskPriorities
+    })
+
+}
+function deleteTask(taskName){
+    list.forEach((elem,index) => {
+        if(elem.name === taskName)
+            list.splice(index,1)
+
+    })
 
 
+}
+function showList() {
+
+    const arrayFindTodo = list.filter((elem) => elem.status === "To Do")
+    const arrayFindDone = list.filter((elem) => elem.status === "Done")
+    const arrayFindProgress = list.filter((elem) => elem.status === "In progress")
+
+
+    if(STATUSES.TODO === "To Do"){
+        console.log(STATUSES.TODO + ":")
+        arrayFindTodo.forEach((elem)=> console.log('\t' + elem.name))
+    }
+    if(STATUSES.DONE === "Done"){
+
+        console.log(STATUSES.DONE + ":")
+        arrayFindDone.forEach((elem)=> console.log('\t' + elem.name))
+    }
+    if(STATUSES.IN_PROGRESS === "In progress"){
+
+        console.log(STATUSES.IN_PROGRESS + ":")
+        arrayFindProgress.forEach((elem)=> console.log('\t' + elem.name))
+    }
+
+
+}
+changeStatus("create a post", "Done")
+addTask(STATUSES.DONE,'have a walk', PRIORITIES.HIGH);
+deleteTask('test');
+showList();
