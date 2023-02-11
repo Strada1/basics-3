27f08cff8656dbcb11e8b8d0e78df6bd037a58bd
Vlad Shamov <holovlad@yandex.ru>
2023-02-11 15:31:14 +1000

    Refactored todo

--- vladholo628/main.js
+++ vladholo628/main.js
@@ -1,66 +1,89 @@
-const list = [ 
-	{name: 'create a post', status: 'In progress', priority: 'low'}, 
-  {name: 'test', status: 'Done', priority: 'high'} ,
-  {name: 'another task', status: 'To Do', priority: 'high'} 
-];
+const DEFAULT_VALUES = {
+  defaultStatus: "To Do",
+  defaultPriority: "low",
+};
+const AVALIABLE_OPTIONS = {
+  statuses: {
+    done: "Done",
+    toDo: "To Do",
+    inProgress: "In Progress",
+  },
+  priorities: {
+    low: "Low",
+    high: "High",
+  },
+};
 
-const defaultStatus = 'To Do';
-const defaultPriority = 'low';
+const todo = {
+  list: [
+    { name: "create a post", status: "In progress", priority: "low" },
+    { name: "test", status: "Done", priority: "high" },
+    { name: "another task", status: "To Do", priority: "high" },
+  ],
 
-const changeStatus = (taskName, status) => {
-    const taskIndex = list.findIndex(item => item.name === taskName);
+  changeStatus(taskName, status) {
+    const taskIndex = this.list.findIndex((item) => item.name === taskName);
 
-    if(taskIndex === -1) {
-        console.log('Task not found...')
-        return;
+    if (taskIndex === -1) {
+      console.log("Task not found...");
+      return;
     }
 
-    list[taskIndex].status = status;
+    this.list[taskIndex].status = status;
+  },
+  changePriority(taskName, priority) {
+    const taskIndex = this.list.findIndex((item) => item.name === taskName);
 
-}
+    if (taskIndex === -1) {
+      console.log("Task not found...");
+      return;
+    }
 
-const addTask = (taskName, status = defaultStatus,priority = defaultPriority) => {
+    this.list[taskIndex].priority = priority;
+  },
+  addTask(
+    taskName,
+    status = DEFAULT_VALUES.defaultStatus,
+    priority = DEFAULT_VALUES.defaultPriority
+  ) {
     const newTask = {
-        name:taskName,
-        status: status,
-        priority:priority,
+      name: taskName,
+      status: status,
+      priority: priority,
+    };
+
+    this.list.push(newTask);
+  },
+  removeTask(taskName) {
+    const targetTaskIndex = this.list.findIndex(
+      (task) => task.name === taskName
+    );
+    if (targetTaskIndex === -1) {
+      console.log("Task not found...");
+      return;
     }
-
-    list.push(newTask)
-}
-
-const removeTask = (taskName) => {
-   const targetTaskIndex = list.findIndex(task => task.name === taskName);
-   if(targetTaskIndex === -1) {
-    console.log('Task not found...');
-    return;
-   }
-   list.splice(targetTaskIndex,1)
-}
-
-const showList = () => {
-    const todo = list.filter(item => item.status === 'To Do')
-    const done = list.filter(item => item.status === 'Done')
-    const inProgress = list.filter(item => item.status === 'In progress')
+    list.splice(targetTaskIndex, 1);
+  },
+  showList() {
+    const todo = this.list.filter((item) => item.status === "To Do");
+    const done = this.list.filter((item) => item.status === "Done");
+    const inProgress = this.list.filter(
+      (item) => item.status === "In progress"
+    );
 
     const render = (arrayOfTasks, title) => {
-        console.log(`${title}:`)
-        if(arrayOfTasks.length === 0) {
-            console.log('-')
-            return;
-        }
-        arrayOfTasks.forEach(task => console.log(`--- ${task.name}`))
-    }
+      console.log(`${title}:`);
+      if (arrayOfTasks.length === 0) {
+        console.log("-");
+        return;
+      }
+      arrayOfTasks.forEach((task) => console.log(`--- ${task.name}`));
+    };
 
-    render(todo, 'To Do');
-    render(done, 'Done'),
-    render(inProgress, 'In Progress')
-    if(done.length === 0) console.log('Nothing is Done...')
-}
+    render(todo, "To Do");
+    render(done, "Done"), render(inProgress, "In Progress");
+    if (done.length === 0) console.log("Nothing is Done...");
+  },
+};
 
-console.log(list)
-
-// removeTask('test')
-
-// console.log(list)
-showList()
+todo.showList();
