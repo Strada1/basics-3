f62352d8137fca526163f27e9a4ef7760deef411
Artofmir <you@example.com>
2023-02-13 02:30:59 +0300

    toDo list on massives-refact

--- Artofmir/todo.js
+++ Artofmir/todo.js
@@ -1,29 +1,54 @@
+const COLOR = {
+  RED: `\x1b[31m`,
+  GREEN: `\x1b[32m`,
+  YELLOW: `\x1b[33m`,
+  MAGENTA: `\x1b[35m`,
+  CYAN: `\x1b[36m`,
+  CLR: `\x1b[0m`
+};
+
+const STATUSES = {
+  TODO: 'To Do',
+  IN_PROGRESS: 'In Progress',
+  DONE: 'Done',
+};
+
+const STATUSESS = {
+  Done: `${COLOR.GREEN} Завершено: ${COLOR.CLR}`,
+  Prog: `${COLOR.YELLOW} В процессе: ${COLOR.CLR}`,
+  todo: `${COLOR.MAGENTA} Нужно сделать: ${COLOR.CLR}`,
+};
+
 let testArray = [
   {name : 'Сходить в магазин', status : 'To Do', priority : 'low'},
-  {name : 'Обработать фотки', status : 'To Do', priority : 'high'},
+  {name : 'Обработать фотки', status : 'In Progress', priority : 'high'},
   {name : 'Сделать задание Strada', status : 'In Progress', priority : 'high'},
   {name : 'Поспать', status :'Done', priority : 'low'},
   {name : 'Сходить в качалку', status : 'To Do', priority : 'low'}
 ];
 
-
+const findIndex = (indexName) => {
+  return testArray.findIndex(x => x.name == indexName); 
+};
 
 const changeStatus = (taskName, newTaskStatus, newTaskPriority) => {
 
-  let task = testArray.find(existName => existName.name == taskName);
+  idx = findIndex(taskName);   
 
-  if (task) {
-    task = {name : taskName, status : newTaskStatus, priority : newTaskPriority};
+  if (idx >= 0) {
+    testArray[idx] = {name : taskName, status : newTaskStatus, priority : newTaskPriority};
   } else {
-    console.log(`\x1b[31m Статус \x1b[0m"${taskName}"\x1b[31m не изменён - нет в списке! \x1b[0m`);
+    console.log(COLOR.RED, `Статус`, COLOR.CLR + 
+                ` "` + taskName + `" ` + 
+                COLOR.RED, `не изменён - нет в списке!`, COLOR.CLR);
   };
 };
 
 const addNewTask = (newTaskName) => {
 
-  let task = testArray.find(existName => existName.name == newTaskName);
+  idx = findIndex(newTaskName);     
 
-  if (task) {
+  if (idx >= 0) {
     console.log('Такая задача уже есть!');
   } else {
     testArray.push({name : newTaskName, status : 'To Do', priority : 'low'});
@@ -31,44 +56,42 @@ const addNewTask = (newTaskName) => {
 };
 
 const deleteTask = (taskName) => {
-let idx = testArray.findIndex(x => x.name == taskName);              
+
+  idx = findIndex(taskName);     
+
   if (idx >= 0) {
     testArray.splice(idx, 1);
-    console.log(`\x1b[31m Задача:\x1b[0m "${taskName}"\x1b[31m удалена.\x1b[0m`);
+    console.log(COLOR.RED, `Задача:`, COLOR.CLR +
+                ` "` + taskName + `" ` + 
+                COLOR.RED, `удалена.` ,COLOR.CLR);
   } else {
-    console.log(`\x1b[31m Не удалено:\x1b[0m "${taskName}",\x1b[31m нет в списке! \x1b[0m`);
+    console.log(COLOR.RED, `Не удалено:`, COLOR.CLR + 
+                ` "` + taskName + `" ` + 
+                COLOR.RED, `нет в списке!`, COLOR.CLR);
   };
 };
 
-const showList = () => {
 
-  console.log(`Задач на сегодня \x1b[32m${testArray.length}:\x1b[0m`);
-
-  const logout = (logoutTaskStatus) => {
-    for (const all of logoutTaskStatus) {
-      console.log(' - ' + all.name + ',' + '\x1b[36m','приоритет: ','\x1b[0m' + all.priority);
+  function filteredStatuses(testArray, status, newStatus) {
+    const toDo = testArray.filter(todo => todo.status === newStatus);
+  
+    console.log(status);
+    for (const all of toDo) {
+      console.log(' - ' + all.name + ',' + 
+                  COLOR.CYAN, 'приоритет: ', COLOR.CLR + 
+                  all.priority);
     };
   };
-
-  const toDo = testArray.filter(todo => todo.status === 'To Do');
   
-  console.log('\x1b[35m','Нужно сделать:', '\x1b[0m');
-
-  logout(toDo);
+  const showList = () => {
+    console.log(`Задач на сегодня ` + 
+                COLOR.GREEN + testArray.length +`:` + COLOR.CLR);
   
-  const inProgress = testArray.filter(todo => todo.status === 'In Progress');
-  
-  console.log('\x1b[33m', 'В процессе:', '\x1b[0m');
-  
-  logout(inProgress);
-  
-  const done = testArray.filter(todo => todo.status === 'Done');
-  
-  console.log('\x1b[32m', 'Завершено:', '\x1b[0m');
-
-  logout(done);
-  
+    filteredStatuses(testArray, STATUSESS.Done, STATUSES.DONE);
+    filteredStatuses(testArray, STATUSESS.Prog, STATUSES.IN_PROGRESS);
+    filteredStatuses(testArray, STATUSESS.todo, STATUSES.TODO);
   };
+  
 
 
 changeStatus ('Сделать задание Strada', 'To Do', 'low');
