7fb0d23ab751f17c92387e390f9aadb73315d282
mudrec21veka <15tegy@mail.ru>
2023-02-13 15:09:27 +0300

    add file smolskiy

---
+++ smolskiy/main.js
@@ -0,0 +1,19 @@
+//alert('HI, EVERBODY!');
+console.log('Hello Sergey');
+console.log('Haw are you?');
+console.log('Проверка git')
+
+let userName = 'Sergey Smolskiy';
+let userAge = 28;
+console.log(userName + 'is ' + userAge + ' years old');
+
+let userHeight;
+userHeight = 178;
+console.log(userHeight);
+
+let offecialNameJs = prompt('Какое официальное название JavaScript?', '');
+if (offecialNameJs == 'ECMAScript') {
+    alert('Верно!')
+} else {
+    alert('Не знаете? “ECMAScript”!');
+}
