7a66ba39caa765cbc40b9bf6f5a81373aa68d5d0
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-16 00:38:01 +0300

    Simple example of setTimeout

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-function printNumbers(from, to) {
-  let currentNumber = from
-
-  setTimeout(function timer() {
-    console.log(currentNumber)
-    if (currentNumber < to) {
-      setTimeout(timer, 1000)
-    }
-    currentNumber++
-  }, 1000)
+function helloUser(text, name) {
+  console.log(`${text}, ${name}! :)`)
 }
 
-printNumbers(1, 5)
+setTimeout(helloUser, 5000, "Привет", "чувачок")
