17691ffec1a097612a728d0c38140f7278ff08b7
andrey ryabov <araibov@mail.ru>
2023-02-17 14:25:31 +0500

    added new functions

--- #26 ToDo на массивах/script.js
+++ #26 ToDo на массивах/script.js
@@ -1,24 +1,23 @@
-const statuses = {
-    ToDo: 'ToDo',
-    InProg: 'In progress',
-    Done: 'Done'
-}
-
-const priorities = {
-    low: 'low',
-    med: 'medium',
-    high: 'hidh'
-}
-
 const list = [
-    { name: 'create a post', status: statuses.ToDo, priority: priorities.med },
-    { name: 'test', status: statuses.InProg, priority: priorities.low }
+    { name: 'create a post', status: 'In progress', priority: 'low' },
+    { name: 'test', status: 'Done', priority: 'high' }
 ];
 
 function showTaks(arr) {
     arr.forEach(e => {
-        console.log(`name : ${e.name}, status : ${e.status}, priority : ${e.priority}` );
+        console.log(`name : ${e.name}, status : ${e.status}, priority : ${e.priority}`);
     });
 }
 
+function addTask(name, status, priority) {
+    list.push({name: name, status: status, priority: priority});
+}
+
+function deleteTask(name) {
+    let index = list.findIndex(i => i.name === name);
+    list.splice(index, 1);
+}
+
+addTask('полить цветы', 'toDo', 'medium');
+deleteTask('create a post');
 showTaks(list);
