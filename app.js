fa0f5cbac01e500ea0ad78209c9a64c1795a203d
kihrynko <ki.hrynko@gmail.com>
2023-02-18 08:21:13 +0200

    solved second task

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ function buildFun(n){
 
   return res;
 }
+
+function getAverage(marks){
+  let sum = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+
+  return Math.floor(sum / marks.length);
+}
