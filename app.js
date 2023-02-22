f19a670f7c5ff304daa0ab5c65dba31cfc09c965
legendes123 <global.lolo@yandex.ru>
2023-02-23 00:03:00 +0300

    calculator

--- app.js
+++ app.js
@@ -1,48 +1,29 @@
-let buuton = document.querySelector('button')
-let buutonRunning = false
-let second = 0
-let hourse = 0
-let minute = 0
 
-function buttonTimer(){
+const buttonCalc = document.querySelectorAll('.equals')[0]
+const finalNumber = document.querySelectorAll('.finalNumber')[0]
 
-  buutonRunning = !buutonRunning  
-  if(buutonRunning){
-    startTimer()
+function calculate(){
+  const firstNumber = document.getElementById('firstNumber').value
+  const lastNumber = document.getElementById('lastNumber').value
+  const signAll = document.getElementById('select')
+  const signSelected = signAll.querySelector(`option[value="${select.value}"]`).value  
+  let result
+  switch (signSelected) {
+    case 'add':
+      result = Number(firstNumber) + Number(lastNumber);
+      break
+    case 'sub':
+      result = Number(firstNumber) - Number(lastNumber);
+      break
+    case 'mult':
+      result=  Number(firstNumber) * Number(lastNumber);
+      break
+    case 'share':
+      result =  Number(firstNumber) / Number(lastNumber);
+      break
   }
-  if(!buutonRunning){
-    stopTimer()
-  }
+  finalNumber.innerHTML = result
+
 }
 
-function startTimer(){
-  return interval = setInterval(() => countTime(), 1000)
-}
-function stopTimer(){
-  return clearInterval(interval)
-}
-function countTime(){  
-  second++
-  if(second === 60){
-    minute++
-    second = 0
-  }
-  if(minute === 60){
-    hourse++
-    minute = 0
-  }
-  finalTime()
-}
-function finalTime(){
-  if(second < 10){
-    second = second.toString().padStart(2, "0")    
-  }
-  if(minute < 10){
-    minute = minute.toString().padStart(2, "0")
-  }
-  if(hourse < 10){
-    hourse = hourse.toString().padStart(2, "0")
-  }
- console.log(`${hourse}:${minute}:${second}`)
-}
-buuton.addEventListener('click',buttonTimer)
+buttonCalc.addEventListener('click', calculate)
