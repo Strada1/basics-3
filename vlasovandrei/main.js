77ea487ea4d83669c1f97199b1b425482005f783
CallBackLater <a.a.vlasov88@gmail.com>
2023-02-22 11:50:05 +0500

    Regular timer with UI

--- vlasovandrei/main.js
+++ vlasovandrei/main.js
@@ -1,9 +1,14 @@
 let button = document.getElementById('button');
+let elHours = document.getElementById('hours');
+let elMinutes = document.getElementById('minutes');
+let elSeconds = document.getElementById('seconds');
+
 let isActive = false;
 let timerId;
-let hours = document.getElementById('hours');
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
+
+let seconds = 50;
+let minutes = 0;
+let hours = 0;
 
 function switchActive() {
 	if (isActive === false) {
@@ -13,19 +18,26 @@ function switchActive() {
 	}
 }
 
-function getTime() {
+function startStopTimer() {
 	if (switchActive()) {
 		timerId = setInterval(() => {
-			let now = new Date();
-			seconds.textContent = now.getSeconds();
-			minutes.textContent = now.getMinutes();
-			hours.textContent = now.getHours();
+			seconds++;
+			if (seconds === 60) {
+				seconds = 0;
+				minutes++;
+			} if (minutes === 60) {
+				hours++;
+				minutes = 0;
+			}
+			elSeconds.textContent = seconds;
+			elMinutes.textContent = minutes;
+			elHours.textContent = hours;
 		}, 1000);
 	} else {
 		clearInterval(timerId);
 	}
 }
 
-button.addEventListener('click', getTime);
+button.addEventListener('click', startStopTimer);
 
 	
