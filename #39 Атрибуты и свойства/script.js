3d571855667aad12c2554519ffd3e4a8b4976fc1
andrey ryabov <araibov@mail.ru>
2023-02-23 13:05:43 +0500

    добавил функцию авто изменения фона каждую секунду и реализовал кнопку для отключения этого интервала

--- #39 Атрибуты и свойства/script.js
+++ #39 Атрибуты и свойства/script.js
@@ -1,7 +1,7 @@
 const inputColor = document.querySelector('#inputColor');
 const body = document.querySelector('body');
 const change_bg = document.querySelector('#change-bg');
-const div = document.querySelector('#div');
+const disableRandomBC = document.querySelector('#disableRandomBC');
 
 inputColor.addEventListener('change', () => {
     change_bg.setAttribute('data-color', inputColor.value)
@@ -10,6 +10,23 @@ inputColor.addEventListener('change', () => {
 change_bg.addEventListener('click', () => {
     const data = change_bg.getAttribute('data-color');
     body.style.backgroundColor = data;
-    div.classList.toggle('disabled');
-    console.log(getComputedStyle(div));
 })
+
+disableRandomBC.addEventListener('click', () => {
+    clearInterval(intervalSetBC);
+})
+
+
+
+
+
+const colors = ['red', 'blue', 'green', 'orange'];
+
+function setRandomBC(colors) {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    body.style.backgroundColor = randomColor;
+}
+
+let intervalSetBC = setInterval(() => {
+    setRandomBC(colors)
+}, 1000);
