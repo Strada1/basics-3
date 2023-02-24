ab30db8a656c3cb0e4b4ac8331dee290575afe0f
Razvalnuy <megamg001@gmail.com>
2023-02-25 00:50:11 +0300

    V2 calculate

--- filesCalc/main.js
+++ filesCalc/main.js
@@ -3,7 +3,6 @@ const numB = document.getElementById('b');
 const operants = document.getElementById('operantsAll');
 const myButton = document.getElementById('myButton');
 const allResult = document.getElementById('resultSpan');
-const divTextLog = document.createElement('div');
 
 
 const ERRORS = {
@@ -17,28 +16,36 @@ function calc(){
 	const newNumB = +numB.value;
 	const newOperantsAll = operants.value;
 
-	if(numA.value === '' || numB.value === ''){
-			return document.getElementById('resultSpan').textContent = ERRORS.NULL;
-		}else if( operants.value === 'divide' && +numA.value === 0){
-			return document.getElementById('resultSpan').textContent = ERRORS.DEVIDE;
-		}else{
-			switch(newOperantsAll){
-				case 'plus':
-					  resultSum = newNumA + newNumB;
-					  break;
-					case 'minus':
-						 resultSum = newNumA - newNumB;
-						 break;
-						case 'multiply':
-							 resultSum = newNumA * newNumB;
-							break;
-							case 'divide':
-								 resultSum = newNumA / newNumB;
-								break;
-			}
-			document.getElementById('resultSpan').textContent = resultSum;
+if(numA.value === '' || numB.value === ''){
+	return document.getElementById('resultSpan').textContent = ERRORS.NULL;
+}else if( operants.value === 'divide' && +numA.value === 0){
+	return document.getElementById('resultSpan').textContent = ERRORS.DEVIDE;
+}else{
+	switch(newOperantsAll){
+		case 'plus':
+			resultSum = newNumA + newNumB;
+		break;
+		case 'minus':
+			resultSum = newNumA - newNumB;
+		break;
+		case 'multiply':
+			resultSum = newNumA * newNumB;
+		break;
+		case 'divide':
+			resultSum = newNumA / newNumB;
+		break;
+		}
+		document.getElementById('resultSpan').textContent = resultSum.toFixed(2);
 	}
+	const checkContainer = document.getElementById('cont');
+	const lastHistory = document.createElement('span');
+	lastHistory.classList.add('lastHistory');
+	lastHistory.textContent = resultSum
+	checkContainer.append(lastHistory.toFixed(2));
+	function deleteLastHistory(){
+		lastHistory.remove();
 	}
-
+	lastHistory.addEventListener('click', deleteLastHistory);
+}
 
 myButton.addEventListener('click', calc);
