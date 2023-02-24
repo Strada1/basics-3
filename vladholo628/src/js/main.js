068d4d0ce1bff24a33e8663f63fd54d90c06806d
Vlad Shamov <holovlad@yandex.ru>
2023-02-24 18:26:47 +1000

    Added history functionality

--- vladholo628/src/js/main.js
+++ vladholo628/src/js/main.js
@@ -3,8 +3,8 @@ const secondNumberInput = document.getElementById("secondNumberInput");
 const resultScreen = document.querySelector(".calculator__result");
 const submitButton = document.querySelector(".calculator__form-button");
 const operationSelect = document.querySelector(".calculator__form-select");
+const hitoryScreen = document.querySelector(".history");
 
-console.log(operationSelect.value);
 const OPERATIONS = {
   "+": "sum",
   "-": "sub",
@@ -33,12 +33,26 @@ const calc = (a, b, operation) => {
   }
 };
 
+const addToHistory = (resultValue) => {
+  const newHistoryItem = document.createElement("span");
+  newHistoryItem.classList.add("history__item");
+  newHistoryItem.textContent = resultValue;
+
+  newHistoryItem.addEventListener("click", (e) => {
+    hitoryScreen.removeChild(e.target);
+  });
+
+  hitoryScreen.appendChild(newHistoryItem);
+};
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   const firstNumber = Number(firstNumberInput.value);
   const secondNumber = Number(secondNumberInput.value);
   const selectedOperation = OPERATIONS[operationSelect.value];
   const result = calc(firstNumber, secondNumber, selectedOperation);
+
+  addToHistory(result);
 
   resultScreen.textContent = result;
 });
