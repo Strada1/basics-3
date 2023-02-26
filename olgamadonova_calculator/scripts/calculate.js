46bbd4cab10acf76b21be9ec974da2032e1b4a9b
olgadeveloper <madonovaolga@icloud.com>
2023-02-26 10:55:53 +0700

    object dictionary added

--- olgamadonova_calculator/scripts/calculate.js
+++ olgamadonova_calculator/scripts/calculate.js
@@ -3,6 +3,12 @@ const secondOperatorElement = document.querySelector('[data-second-operator]');
 const operandElement = document.querySelector('[data-operand]');
 const enterElement = document.querySelector('[data-enter]');
 const resultElement = document.querySelector('[data-result]');
+const MathOperator = {
+    Plus: 'plus',
+    Multiply: 'multiply',
+    Minus: 'minus',
+    Divide: 'divide',
+}
 
 let result = 0;
 
@@ -10,23 +16,22 @@ const enterElementHandler = () => {
     const operation = operandElement.value;
     const firstNumber = Number(firstOperatorElement.value);
     const secondNumber = Number(secondOperatorElement.value);
-//вопрос про области видимости: если объявить переменную result здесь, а не в модульной области видимости, то если я пытаюсь обратиться к ней внутри тела if {} эта переменная считывается как неинициализированная, почему так? ведь она объявлена внутри области видимости функции где я совершаю проверку, при этом внутри тела switch все в порядке, не понимаю почему такое поведение
-
+    
     if (firstOperatorElement.value === '' || secondOperatorElement.value === '') {
         resultElement.innerText = 'ожидание чисел для совершения операции';
         return;
     }
     switch (operation) {
-        case ('plus'):
+        case (MathOperator.Plus):
             result = firstNumber + secondNumber;
             break;
-        case('multiply'):
+        case(MathOperator.Multiply):
             result = firstNumber * secondNumber;
             break;
-        case ('minus'):
+        case (MathOperator.Minus):
             result = firstNumber - secondNumber;
             break;
-        case('divide'):
+        case(MathOperator.Divide):
             result = (secondNumber === 0)? 'делить на ноль нельзя': firstNumber/secondNumber;
             break;
         default:
