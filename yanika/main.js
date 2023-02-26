a1b2e3442c08f6b5595c44e4dd3b3df2af2d8f75
Yana <goyana72@gmail.com>
2023-02-27 00:43:27 +0300

    исправила нерабочую проверку на infinity и пустую строку и добавила проверку на NaN; подравняла css; добавила удаление обработчика события (но не знаю: правильно ли)

--- yanika/main.js
+++ yanika/main.js
@@ -3,36 +3,41 @@
 import {number1, number2, getElement, result, showDiv} from '/variables.js'
 
 const createOperation = () => {
-    const num1 = Number(number1.value);
-    const num2 = Number(number2.value);
+    const num1 = number1.value;
+    const num2 = number2.value;
     let oper = getElement.value;
     let res;
 
     switch (oper) {
         case 'plus':
-            res = num1 + num2;
+            res = Number(num1) + Number(num2);
             break;
         case 'minus':
-            res = num1 - num2;
+            res = Number(num1) - Number(num2);
             break;
         case 'divide':
-            res = num1 / num2;
+            res = Number(num1) / Number(num2);
             break;
         case 'multiply':
-            res = num1 * num2;
+            res = Number(num1) * Number(num2);
             break;
     }
 
-    if (res === Infinity) {res = 'Cannot divide by zero'}
-    if (num1 || num2 == '') {res = 'Enter the number'}
+    if (res == Infinity || isNaN(res)) {res = 'Cannot divide by zero'}
+    if (num1 === '' || num2 === '') {res = 'Enter the number'}
+
 
     let div = document.createElement('div')
     div.classList.add('new-div')
+    div.style.backgroundColor = 'rgb(144, 161, 146)'
     showDiv.appendChild(div)
     div.textContent = res
 }
 
 document.addEventListener('click', searchDiv => {
+    if (searchDiv.target.classList.contains('new-div')){searchDiv.target.remove()}
+})
+document.removeEventListener('click', searchDiv => {
     if (searchDiv.target.classList.contains('new-div')){searchDiv.target.remove()}
 })
 result.addEventListener('click', createOperation)
