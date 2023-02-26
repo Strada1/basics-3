0cdab133a68c252e85b1103a5d79e75d040da70f
Ruslan Rmv <ruslanrmv@gmail.com>
2023-02-27 01:33:32 +0300

    Изменение значения числа по нажатию кнопки =

--- main.js
+++ main.js
@@ -1,30 +1,9 @@
-const timerButton = document.getElementById("timer")
+const equalButton = document.getElementById("equalButton")
+const newCalculatedNumber =
+  document.getElementById("calculatedNumber").innerHTML
 
-let timerStarted = false
-let secondCounter = 0
-let timerId
-
-function timerStarStop() {
-  if (!timerStarted) {
-    printSeconds()
-  } else {
-    stopPrintSeconds()
-  }
+function newNumber() {
+  document.getElementById("calculatedNumber").innerHTML = "25"
 }
 
-function printSeconds() {
-  timerId = setInterval(() => {
-    console.log(secondCounter)
-    secondCounter++
-  }, 1000)
-
-  timerStarted = true
-}
-
-function stopPrintSeconds() {
-  clearInterval(timerId)
-  timerStarted = false
-  console.log("Paused")
-}
-
-timerButton.addEventListener("click", timerStarStop)
+equalButton.addEventListener("click", newNumber)
