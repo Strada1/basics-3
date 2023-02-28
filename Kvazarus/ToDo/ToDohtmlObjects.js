d1f34fb7d253e57ba467aaf6f4d0a1f0edb6b834
Kvazarus <kvazarus13@yandex.ru>
2023-02-28 23:19:14 +0300

    #44 Формы

---
+++ Kvazarus/ToDo/ToDohtmlObjects.js
@@ -0,0 +1,5 @@
+const forms = document.querySelectorAll('form.addTask');
+const inputsInForms = document.querySelectorAll('form.addTask input');
+const priorityDivs = document.querySelectorAll('div.wrapper > div');
+
+export const objects = {forms, inputsInForms, priorityDivs};
