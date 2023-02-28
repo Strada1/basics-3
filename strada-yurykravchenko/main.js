b4a778a6add978d09649709a3906b1203697aca3
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-03-01 02:43:47 +0300

    Add script in todo

---
+++ strada-yurykravchenko/main.js
@@ -0,0 +1,28 @@
+const buttonAddTask = document.querySelector('.button__add-task');
+const fieldAddTask = document.querySelector('.field__add-task'); //поле ввода задачи
+let highTasks = document.querySelector('.tasks-high__wrapper'); //список срочных задач
+let lowTasks = document.querySelector('.tasks-low__wrapper'); //список не срочных задач
+let task = document.querySelector('.task-wrapper');
+
+
+
+function createTask () {
+  const templateTask = document.querySelector('.template-task').content; //находим нужный фрагмент
+  const templateTaskElement = templateTask.querySelector('div') //находим нужный элемент во фрагменте
+  let textTask = templateTask.querySelector('p') //
+  let dataInputTask = fieldAddTask.value; //получаем значение из инпута
+  textTask.textContent = dataInputTask //меняем текстовый контент в нужном элементе с помощью значения из поля ввода
+  let element = templateTaskElement.cloneNode(true) //клонируем элемент с изменениями
+  let priority = prompt('Введите преоритет задачи')
+
+  if(priority === 'high') {
+    highTasks.appendChild(element)
+  } else {
+    lowTasks.appendChild(element)
+  }
+}
+
+buttonAddTask.addEventListener('click', (event) => {
+  event.preventDefault()
+  createTask()
+})
