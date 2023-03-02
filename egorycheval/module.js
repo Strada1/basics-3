4c776ce8f8c24fbf2135edb591733af36d40c577
egorychevaleksey <egory4ev38@gmail.com>
2023-03-02 18:05:32 +0800

    add module in calculator

---
+++ egorycheval/module.js
@@ -0,0 +1,7 @@
+export const firstNumber = document.getElementById('first_number');
+export const secondNumber = document.getElementById('second_number');
+export const sign = document.getElementById('sign');
+export const buttonEquals = document.getElementById('equals');
+export const valueConteiner = document.getElementById('value_conteiner');
+
+
