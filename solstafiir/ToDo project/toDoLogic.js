4ba6a0bbb006184e7d56663cbcfc46bc91925174
eddie <foxi2kiwhl@gmail.com>
2023-03-03 13:28:39 +0200

    todo with render(modules will be added later)

--- solstafiir/ToDo project/toDoLogic.js
+++ solstafiir/ToDo project/toDoLogic.js
@@ -1,3 +1,13 @@
+const PRIORITIES = {
+  HIGH: "High",
+  LOW: "Low",
+};
+
+const STATUSES = {
+  TODO: "To Do",
+  DONE: "Done",
+};
+
 const highForm = document.querySelector(".high-task-field");
 const highFormContent = document.querySelector(".add-high-task-field");
 
@@ -7,56 +17,102 @@ const lowFormContent = document.querySelector(".add-low-task-field");
 const highTaskCollector = document.querySelector(".high-task-collector");
 const lowTaskCollector = document.querySelector(".low-task-collector");
 
-function createTask(formValueContent, collection) {
-  const taskCanvas = document.createElement("div");
-  taskCanvas.className = "task";
+const list = [
+  // { id: 1, name: "wash the car", status: "To Do", priority: "High" },
+  // { id: 2, name: "create a post", status: "To Do", priority: "High" },
+  // { id: 3, name: "suck", status: "To Do", priority: "Low" },
+];
 
-  const checkBox = document.createElement("input");
-  checkBox.className = "check";
-  checkBox.type = "checkbox";
-  checkBox.addEventListener("click", () => {
-    if (checkBox.checked) {
-      taskCanvas.style.background = "lightgreen";
-      checkBox.removeEventListener();
-    } else {
-      taskCanvas.style.background = "white";
+function render() {
+  deleteTasksFromArray();
+  list.forEach((el) => {
+    const taskHTML = `<div class="task" id=${el.id} >
+    <input type="checkbox"  class="check" />
+    <div class="todo-content">
+    ${el.name}
+    </div>
+    <button class="delete-task-button">+</button>
+    </div>`;
+    switch (el.priority) {
+      case PRIORITIES.HIGH:
+        highTaskCollector.insertAdjacentHTML("beforeend", taskHTML);
+        break;
+      case PRIORITIES.LOW:
+        lowTaskCollector.insertAdjacentHTML("beforeend", taskHTML);
+        break;
+    }
+    if (el.status === STATUSES.DONE) {
+      const task = document.querySelector(`#${CSS.escape(el.id)}`);
+      task.style.background = "lightgreen";
+      const input = task.querySelector(".check");
+      input.checked = true;
     }
   });
+  console.log(list);
+}
+render();
 
-  const content = document.createElement("div");
-  content.className = "todo-content";
-  content.textContent = formValueContent;
-  if (content.textContent === "") {
-    return alert("Task field can`t be empty!");
+function addTask(contentForm, priority) {
+  const taskText = contentForm;
+  if (taskText === "") {
+    alert("Cant be empty");
+  } else {
+    list.splice(0, 0, {
+      id: Date.now(),
+      name: taskText,
+      status: STATUSES.TODO,
+      priority: priority,
+    });
   }
-
-  const deleteTaskButton = document.createElement("button");
-  deleteTaskButton.className = "delete-task-button";
-  deleteTaskButton.textContent = "+";
-
-  deleteTaskButton.addEventListener("click", () => deleteTask(taskCanvas));
-
-  collection.prepend(taskCanvas);
-  taskCanvas.prepend(content);
-  taskCanvas.prepend(checkBox);
-  taskCanvas.appendChild(deleteTaskButton);
 }
 
+// смена статуса
+function changeTaskStatus(event) {
+  if (event.target.classList.contains("check")) {
+    const id = event.target.parentElement.id;
+    list.findIndex((item) => {
+      if (item.id == id && item.status !== STATUSES.DONE) {
+        item.status = STATUSES.DONE;
+      } else if (item.id == id && item.status !== STATUSES.TODO) {
+        item.status = STATUSES.TODO;
+      }
+    });
+    render();
+  }
+}
+document.addEventListener("click", changeTaskStatus);
+
+// //удаление
+function deleteTask(event) {
+  if (event.target.classList.contains("delete-task-button")) {
+    const id = event.target.parentElement.id;
+    const index = list.findIndex((task) => task.id == id);
+    list.splice(index, 1);
+    event.target.parentElement.remove();
+    render();
+  }
+}
+document.addEventListener("click", deleteTask);
+
+//удаление существущих тасок для рендера
+function deleteTasksFromArray() {
+  const taskFromArray = document.querySelectorAll(".task");
+  if (taskFromArray) {
+    taskFromArray.forEach((taskFromArray) => taskFromArray.remove());
+  }
+}
+// //функции для добавления в поля ввысокого и низкого приоритетов
 function addHighTask(event) {
   event.preventDefault();
-  createTask(highFormContent.value, highTaskCollector);
+  addTask(highFormContent.value, PRIORITIES.HIGH);
+  render();
   event.target.reset();
 }
-
 function addLowTask(event) {
   event.preventDefault();
-  createTask(lowFormContent.value, lowTaskCollector);
+  addTask(lowFormContent.value, PRIORITIES.LOW);
+  render();
   event.target.reset();
-}
-
-function deleteTask(task) {
-  task.remove();
-  task.removeEventListener();
 }
 
 highForm.addEventListener("submit", addHighTask);
