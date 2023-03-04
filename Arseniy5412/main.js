ec676620e7242040311bd87d8d4e7be8bb0539ac
Arseniy <101417220+Arseniy5412@users.noreply.github.com>
2023-03-04 16:16:15 +0300

    Delete main.js

--- Arseniy5412/main.js
+++
@@ -1,9 +0,0 @@
-    import {divcreate, calculator} from './history.js';
-    
-    const submit = document.querySelector('.res')
-
-    submit.addEventListener('click', calculator)
-    
-    submit.addEventListener('click', divcreate);
-    
-    
