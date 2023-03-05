86d4e1de5e61c7118df95cf61b79dab14132579c
pythononfire <mypythononfire@gmail.com>
2023-03-05 13:43:51 +1000

    TODO UI

---
+++ pythononfire/JS/globalConsts.js
@@ -0,0 +1,7 @@
+export const todo = [];
+
+export const buttonAddHigh = document.querySelector('.block-addition__button--high');
+export const buttonAddLow = document.querySelector('.block-addition__button--low');
+
+export const parentDivHigh = document.querySelector('.block-tasks--high');
+export const parentDivLow = document.querySelector('.block-tasks--low');
