3e7a6792ccd33a0f92079048745513443c2928ce
dreamybo1 <idelchiksasha@gmail.com>
2023-03-06 07:15:23 +0500

    JSON 2-st task from learnJS

--- dreamybo1/JSON.js
+++ dreamybo1/JSON.js
@@ -1,8 +1,20 @@
-let user = {
-    name: "Василий Иванович",
-    age: 35
-  };
-let newUser = JSON.stringify(user);
-console.log(newUser)
-let nextUser = JSON.parse(newUser);
-console.log(nextUser)
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log(( JSON.stringify(meetup, function replacer(key, value) {
+  return (key != "" && value == meetup) ? undefined : value;
+})));
+
+
+
