9fdb3b376ac8de1b64ab7e28efd0cfee97532fed
Ruslan Rmv <ruslanrmv@gmail.com>
2023-03-06 05:14:03 +0300

    getComputedStyle

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const bgColorButton = document.getElementById("change-bg")
-const bgColor = bgColorButton.getAttribute("data-color")
-const body = document.body
+// const bgColorButton = document.getElementById("change-bg")
+// const bgColor = bgColorButton.getAttribute("data-color")
+// const body = document.body
+//
+// const changeBgColor = () => {
+//   body.style.backgroundColor = bgColor
+// }
 
-const changeBgColor = () => {
-  body.style.backgroundColor = bgColor
-}
+// bgColorButton.addEventListener("click", changeBgColor)
 
-bgColorButton.addEventListener("click", changeBgColor)
+let inputNumElement = document.querySelector(".inputNum")
+let inputNumStyles = getComputedStyle(inputNumElement)
+console.log(inputNumStyles.padding)
