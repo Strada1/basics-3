4bc72fd18f8c9d96f7219ea8dc57ea9df52a7eb0
nibezo <nibezo.cs@gmail.com>
2023-03-09 23:44:48 +0300

    Day 41 - add module

--- main.js
+++ main.js
@@ -3,46 +3,6 @@ const b = document.getElementById('b');
 const operators = document.getElementById('operators');
 const getSolutionButton = document.getElementById('getSolutionButton');
 const solution = document.getElementById('solution');
+import {calc} from './calc.js'
 
-getSolutionButton.addEventListener('click', () => {
-  // Получаем оператор
-  const operator = operators.options[operators.selectedIndex].value;
-  switch (operator) {
-    case '+':
-      let plus = document.createElement('div');
-      plus.textContent = +a.value + +b.value
-      document.body.appendChild(plus);
-      plus.addEventListener('click', (event) => {
-        event.target.remove()
-      })
-      solution.textContent = +a.value + +b.value;
-      break;
-    case '/':
-      let divide = document.createElement('div');
-      divide.textContent = +a.value / +b.value
-      document.body.appendChild(divide);
-      divide.addEventListener('click', (event) => {
-        event.target.remove()
-      })
-      solution.textContent = +a.value / +b.value;
-      break;
-    case '*':
-      let mul = document.createElement('div');
-      mul.textContent = +a.value * +b.value
-      document.body.appendChild(mul);
-      mul.addEventListener('click', (event) => {
-        event.target.remove()
-      })
-      solution.textContent = +a.value * +b.value;
-      break;
-    case '-':
-      let minus = document.createElement('div');
-      minus.textContent = +a.value - +b.value
-      document.body.appendChild(minus);
-      minus.addEventListener('click', (event) => {
-        event.target.remove()
-      })
-      solution.textContent = +a.value - b.value;
-      break;
-  }
-});
+getSolutionButton.addEventListener('click', calc);
