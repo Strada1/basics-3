aa693089c5dd5531ffb26c2278c6e31d3af62585
nibezo <nibezo.cs@gmail.com>
2023-03-09 23:33:31 +0300

    Day 40 - solve task

--- main.js
+++ main.js
@@ -7,20 +7,42 @@ const solution = document.getElementById('solution');
 getSolutionButton.addEventListener('click', () => {
   // Получаем оператор
   const operator = operators.options[operators.selectedIndex].value;
-  const result = document.createElement('div');
-  result.className = 'div'
   switch (operator) {
     case '+':
+      let plus = document.createElement('div');
+      plus.textContent = +a.value + +b.value
+      document.body.appendChild(plus);
+      plus.addEventListener('click', (event) => {
+        event.target.remove()
+      })
       solution.textContent = +a.value + +b.value;
       break;
     case '/':
+      let divide = document.createElement('div');
+      divide.textContent = +a.value / +b.value
+      document.body.appendChild(divide);
+      divide.addEventListener('click', (event) => {
+        event.target.remove()
+      })
       solution.textContent = +a.value / +b.value;
-			break;
+      break;
     case '*':
+      let mul = document.createElement('div');
+      mul.textContent = +a.value * +b.value
+      document.body.appendChild(mul);
+      mul.addEventListener('click', (event) => {
+        event.target.remove()
+      })
       solution.textContent = +a.value * +b.value;
-			break;
+      break;
     case '-':
+      let minus = document.createElement('div');
+      minus.textContent = +a.value - +b.value
+      document.body.appendChild(minus);
+      minus.addEventListener('click', (event) => {
+        event.target.remove()
+      })
       solution.textContent = +a.value - b.value;
-			break; 
+      break;
   }
 });
