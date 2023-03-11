537a63bc4a2a996d081276e595cf70bef471eea3
GeorgeSemenov <zhora.sem@yandex.ru>
2023-03-11 14:02:28 +0300

    task 46 completed

--- GeorgeSemenov/task 45 ToDo/main.js
+++ GeorgeSemenov/task 45 ToDo/main.js
@@ -6,6 +6,8 @@ import render from './render functions/render.js';
 import renderTask from './render functions/renderTask.js';
 import renderTasksContainer from './render functions/renderTasksContainer.js';
 
+// throwAndShowError();
+
 const toDoList={
   list: list,
   render: render,
@@ -17,3 +19,17 @@ const toDoList={
   parentNode: document.getElementById('display'),
 }
 toDoList.render();
+
+function throwAndShowError(){
+  window.onerror = function(msg, url, line, col, err){
+  console.log(`looks like there is error : ${err}
+
+    it says that ${msg}
+
+    caused by ${url}
+
+    at line ${line} and column ${col}`);
+}
+
+throw new Error("error message");
+}
