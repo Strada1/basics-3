c60935980b06c90479479c92e8accda2f3761f5b
Marina Frolova <mikiaef@gmail.com>
2023-03-11 14:30:46 +0300

    genderize

--- marifrolova33/constants.js
+++
@@ -1,6 +0,0 @@
-export const OPERATORS = {
-  ADD: "+",
-  MULTI: "*",
-  SUBTRACT: "-",
-  DIVIDE: "/",
-};
--- marifrolova33/main.js
+++ marifrolova33/main.js
@@ -1,74 +1,19 @@
-import { OPERATORS } from "./constants.js";
+const userNameInput = document.querySelector(".userName");
+const button = document.querySelector(".button");
+button.addEventListener("click", getName);
 
-function calc() {
-  let result;
-  let valueNumberOne = document.getElementById("numberOne").value;
-  let valueNumberTwo = document.getElementById("numberTwo").value;
-  let operator = document.getElementById("mathOperator").value;
-  const equation = document.querySelector(".equation");
+async function getName(event) {
+  event.preventDefault();
+  const serverUrl = "https://api.genderize.io";
+  const userName = userNameInput.value;
+  const url = `${serverUrl}?name=${userName}`;
 
-  if (isNaN(valueNumberOne) || isNaN(valueNumberTwo)) {
-    alert("You should use numbers!");
+  const response = await fetch(url);
+  if (response.ok) {
+    let name = await response.json();
+    console.log(`${userName} is ${name.gender}`);
   } else {
-    let numberOne = parseFloat(valueNumberOne);
-    let numberTwo = parseFloat(valueNumberTwo);
-
-    switch (operator) {
-      case OPERATORS.ADD:
-        result = numberOne + numberTwo;
-        break;
-
-      case OPERATORS.MULTI:
-        result = numberOne * numberTwo;
-
-        break;
-
-      case OPERATORS.SUBTRACT:
-        result = numberOne - numberTwo;
-        break;
-
-      case OPERATORS.DIVIDE:
-        if (numberTwo === 0) {
-          alert("division by 0");
-          result = "";
-        } else {
-          result = numberOne / numberTwo;
-          break;
-        }
-      default:
-        result = "something is wrong";
-    }
+    console.log("Gender is not found");
+    alert("Ошибка HTTP: " + response.status);
   }
-
-  if (isFinite(result)) {
-    if (Number.isInteger(result)) {
-      equation.textContent = result;
-    } else {
-      equation.textContent = result.toFixed(3);
-    }
-  } else {
-    equation.textContent = "";
-  }
-
-  //Вывод истории вычислений
-  const newDivForResult = document.createElement("div");
-  newDivForResult.classList.add("divForResult");
-  if (isFinite(result)) {
-    if (Number.isInteger(result)) {
-      newDivForResult.textContent = result;
-    } else {
-      newDivForResult.textContent = result.toFixed(3);
-    }
-  } else {
-    newDivForResult.textContent = "The result is undefined";
-  }
-  document.body.appendChild(newDivForResult);
-
-  function remove() {
-    newDivForResult.remove();
-  }
-  newDivForResult.addEventListener("click", remove);
 }
-
-let buttonEquals = document.getElementById("forEqual");
-buttonEquals.addEventListener("click", calc);
