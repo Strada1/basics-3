8dd437b969f8fd453da20a40a5b3963671f1424c
Ruslan Maroz <ich.nemoroz@gmail.com>
2023-03-13 00:16:56 +0100

    changed answer from server

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const button = document.querySelector('.sendingButton');
 const namePerson = document.querySelector('.name');
 
 button.addEventListener('click', async function(){
-    const firstName = namePerson.value;
+    const firstName = namePerson.value
     const serverUrl = 'https://api.genderize.io';
     const url = `${serverUrl}?name=${firstName}`;
     let response =  await fetch(url);
 
 if (response.ok) { // если HTTP-статус в диапазоне 200-299
   // получаем тело ответа (см. про этот метод ниже)
-  let json =  await response.text();
-  alert(json)
+  let json =  await response.json();
+  alert(`${json.name} is ${json.gender}`)
 } else {
   alert("Ошибка HTTP: " + response.status);
 }
