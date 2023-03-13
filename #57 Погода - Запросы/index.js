00c57ca5a1a73acd6f904699e266b621b613cc5e
Andrey <araibov@mail.ru>
2023-03-13 21:45:52 +0500

    окончательный коммит 13.03.2023

--- #57 Погода - Запросы/index.js
+++ #57 Погода - Запросы/index.js
@@ -2,10 +2,14 @@ import onTabClick from './src/modules/onTabClick.js';
 import searchFetch from './src/modules/searchFetch.js';
 
 
-// Функция для переключения табов по клику -- modules/onTabClick
+// Слушатель для переключения табов по клику -- modules/onTabClick
 const tabsNav = document.querySelector(".tabs__nav");
 tabsNav.addEventListener('click', onTabClick);
 
-// Функция запроса города из строки поиска -- modules/searchFetch
+// Слушатель формы для запроса города из строки поиска -- modules/searchFetch
 const searchForm = document.querySelector('.search_form');
 searchForm.addEventListener('submit', searchFetch)
+
+// TODO: Добавить слушатель формы на событие 'DOMContentLoaded':
+// При загрузке страницы брать текущий город из localStorage
+// И отображать информацию по нему
