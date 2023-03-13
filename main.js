2f3d0708c6fcebad3796c692ae177e34da76b2b0
nibezo <nibezo.cs@gmail.com>
2023-03-13 10:04:11 +0300

    feat: add tasks to JS array

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-// Delete default value from input when focus
-function clearInput(target) {
-  if (target.value == 'Добавить важные дела' || target.value == 'Добавить') {
-    target.value = '';
+// Storage all tasks here
+const tasks = [];
+
+// Check task existing
+function isTaskExist(name) {
+  for (const task of tasks) {
+    if (task.name === name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Add task into array
+function addTask(name, priority) {
+  if (isTaskExist(name) === true) {
+    console.log(
+      `Задача ${name} существует! Попробуйте добавить другую задачу. `
+    );
+    console.log('==================');
+  } else {
+    if (typeof name === 'string' && typeof priority === 'string') {
+      tasks.push({ name: name, priority: priority });
+      console.log(tasks)
+    }
   }
 }
 
 // By pressing enter or plus button add new tasks to list
 function addNewTask(levelOfTask) {
-  if (levelOfTask == 'high') {
+  if (levelOfTask == 'high') { // High level of task
     let highLevelTaskInput =
       document.getElementById('highLevelTaskInput').value;
     if (highLevelTaskInput !== '') {
+      addTask(highLevelTaskInput, 'high')
       let highLevelTasks = document.querySelector('.high-tasks');
       let newTask = document.createElement(`div`);
       newTask.innerHTML = `
@@ -23,9 +45,10 @@ function addNewTask(levelOfTask) {
       </div>`;
       highLevelTasks.appendChild(newTask);
     }
-  } else if (levelOfTask == 'low') {
+  } else if (levelOfTask == 'low') { // Low level of task
     let lowLevelTaskInput = document.getElementById('lowLevelTaskInput').value;
     if (lowLevelTaskInput !== '') {
+      addTask(lowLevelTaskInput, 'low')
       let lowLevelTasks = document.querySelector('.low-tasks');
       let newTask = document.createElement(`div`);
       newTask.innerHTML = `
@@ -38,5 +61,12 @@ function addNewTask(levelOfTask) {
   </div>`;
       lowLevelTasks.appendChild(newTask);
     }
+  }
+}
+
+// Delete default value from input when focus
+function clearInput(target) {
+  if (target.value == 'Добавить важные дела' || target.value == 'Добавить') {
+    target.value = '';
   }
 }
