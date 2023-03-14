65121343a890051f8b017301fc873026e53a3b5d
leonid-niselovsky <foduskoleg@gmail.com>
2023-03-14 11:07:31 +0500

    feat: add templates (Tue, Mar 14, 2023 11:07:31 AM)

--- leonisel/js/main.js
+++ leonisel/js/main.js
@@ -1,1 +1,34 @@
+const templateContainer = document.querySelector('.container');
+const firstTemplate = document.querySelector('#first-template');
+const secondTemplate = document.querySelector('#second-template');
+const thirdTemplate = document.querySelector('#third-template');
+const screenSwitchButtons = document.querySelectorAll('.box1-low_children');
+const cloneFirst = firstTemplate.content.cloneNode(true);
+const cloneSecond = secondTemplate.content.cloneNode(true);
+const cloneThird = thirdTemplate.content.cloneNode(true);
+
+const putFirstTemplate = templateContainer.appendChild(cloneFirst);
+
+console.log(screenSwitchButtons);
+
+for(let button of screenSwitchButtons) {
+  button.addEventListener('click', function(){
+    console.log('Hello');
+  });
+}
+
+// const putSecondTemplate = templateContainer.appendChild(cloneSecond);
+// const putThirdTemplate = templateContainer.appendChild(cloneThird);
+
+// function switchScreen(event) {
+//   console.log(event.target)
+// }
+
+// function clickScreenSwitchButtons() {
+
+//   for(let button of screenSwitchButtons) {
+//     button.addEventListener('click', switchScreen);
+//   }
+// }
+// clickScreenSwitchButtons(screenSwitchButtons);
 
