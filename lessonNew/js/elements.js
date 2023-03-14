34a4a8ccaf6769ca45e4ab1b45acb66b008c75eb
stanislav553 <stanislav.gutnov@yandex.ru>
2023-03-14 23:25:49 +0300

    second commit

---
+++ lessonNew/js/elements.js
@@ -0,0 +1,16 @@
+
+
+
+export const elements = {
+  serverUrl: 'http://api.openweathermap.org/data/2.5/weather',
+  apiKey: '60f1603b521188954fab84ff45b62949',
+  blockNow: document.querySelector('.weather__info-now'),
+  blockDetails: document.querySelector('.weather__info-details'),
+  blockForecast: document.querySelector('.forecast'),
+  searchCity: document.querySelector('.input__weather'),
+  buttonSearch: document.querySelector('.search__city'),
+  city: document.querySelector('.weather__city'),
+  tempCity: document.querySelector('.weather__temperature'),
+  cloud: document.querySelector('.hard__cloud'),
+  srcImg: document.querySelector('.hard__cloud'),
+}
--- lessonNew/js/main.js
+++ lessonNew/js/main.js
@@ -1,52 +1,43 @@
 
+import { elements } from "./elements.js";
 
-
-
-
-
-
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-const apiKey = '60f1603b521188954fab84ff45b62949';
-const blockNow = document.querySelector('.weather__info-now')
-const blockDetails = document.querySelector('.weather__info-details')
-const blockForecast = document.querySelector('.forecast')
-const searchCity = document.querySelector('.input__weather')
-const buttonSearch = document.querySelector('.search__city')
-const city = document.querySelector('.weather__city')
-const tempCity = document.querySelector('.weather__temperature')
-const cloud = document.querySelector('.hard__cloud')
 function showDetails(event) {
   if (event.target.classList.contains('button__now')) {
-    blockNow.style.display = 'block'
-    blockDetails.style.display = 'none'
-    blockForecast.style.display = 'none'
+    elements.blockNow.style.display = 'block'
+    elements.blockDetails.style.display = 'none'
+    elements.blockForecast.style.display = 'none'
   } else if (event.target.classList.contains('button__details')) {
-    blockNow.style.display = 'none'
-    blockDetails.style.display = 'block'
-    blockForecast.style.display = 'none'
+    elements.blockNow.style.display = 'none'
+    elements.blockDetails.style.display = 'block'
+    elements.blockForecast.style.display = 'none'
   } else if (event.target.classList.contains('button__forecast')) {
-    blockNow.style.display = 'none'
-    blockDetails.style.display = 'none'
-    blockForecast.style.display = 'block'
+    elements.blockNow.style.display = 'none'
+    elements.blockDetails.style.display = 'none'
+    elements.blockForecast.style.display = 'block'
   }
 }
-const srcImg = document.querySelector('.hard__cloud')
+
+function render(name, src, temp) {
+  elements.city.textContent = name;
+  elements.tempCity.textContent = `${Math.round(temp)}°`;
+  elements.srcImg.src = `https://openweathermap.org/img/wn/${src}@2x.png`;
+}
+
+
 function search(event) {
   event.preventDefault()
-  const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-  const apiKey = '60f1603b521188954fab84ff45b62949';
-  const cityName = searchCity.value
-  const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+  const cityName = elements.searchCity.value
+  const url = `${elements.serverUrl}?q=${cityName}&appid=${elements.apiKey}&units=metric`;
   fetch(url)
     .then(response => response.json())
     .then(response => {
-      city.textContent = response.name;
-      tempCity.textContent = `${response.main.temp.toFixed(0)}°`;
-      srcImg.src = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+      render(response.name, response.weather[0].icon, response.main.temp)
       return response
     })
     .catch(err => console.log(err.message))
-    .finally(searchCity.value = '')
+    .finally(elements.searchCity.value = '')
 }
+
 document.addEventListener('click', showDetails)
-buttonSearch.addEventListener('click', search)
+
+elements.buttonSearch.addEventListener('click', search)
